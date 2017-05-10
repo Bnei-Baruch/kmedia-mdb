4d11e0db88ca6058a6ccf7825ea10f5e8ba45b87
Gregory Shilin <gshilin@gmail.com>
2017-05-10 04:13:35 +0300

    more tests; using teaspoon instead of enzyme...

--- src/index.test.js
+++ src/index.test.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import renderer from 'react-test-renderer';
+import $ from 'teaspoon';
+
 import Kmedia from './components/kmedia';
+import Layout from './components/layout';
 
-const supportsHistory = 'pushState' in window.history;
+describe('Kmedia Component', () => {
+  let renderedKmedia;
+  const kmedia = () => {
+    if (!renderedKmedia) {
+      renderedKmedia = $(<Kmedia />).render();
+    }
+    return renderedKmedia;
+  };
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <Router forceRefresh={!supportsHistory} keyLength={12}>
-      <Kmedia />
-    </Router>,
-    div);
+  beforeEach(() => {
+    if (renderedKmedia) {
+      renderedKmedia.unmount();
+    }
+    renderedKmedia = undefined;
+  });
+
+  describe('render', () => {
+    const media = kmedia();
+
+    media.tap(collection =>
+      // renders without crashing
+      expect(collection.length).toBe(1)
+    );
+
+    it('always renders a `Layout`', () => {
+      expect(media.find(Layout).length).toBe(1);
+    });
+
+    it('does not receive any props', () => {
+      expect(Object.keys(media.props()).length).toBe(0);
+    });
+  });
 });
 
