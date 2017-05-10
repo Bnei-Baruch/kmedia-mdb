60555b5ed0fe2231d9182861268e6f59c2d713df
Gregory Shilin <gshilin@gmail.com>
2017-05-10 04:15:56 +0300

    attach store...

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import Kmedia from './components/kmedia';
 
-const supportsHistory = 'pushState' in window.history;
-
 ReactDOM.render(
-  <Router forceRefresh={!supportsHistory} keyLength={12}>
     <Kmedia />
-  </Router>
   , document.getElementById('root')
 );
