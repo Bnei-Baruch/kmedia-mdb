920788b88cfc572e7e4c3f066d475ebfdb90dbec
yanivel <yaniv.elimor@gmail.com>
2017-06-02 14:48:27 +0300

    DateFilter presets and selection

--- src/components/filters/DateFilter/DateFilter.js
+++ src/components/filters/DateFilter/DateFilter.js
@@ -1,44 +1,101 @@
 import React, { Component }  from 'react';
 import moment from 'moment';
 import DayPicker, { DateUtils } from 'react-day-picker';
-import { Segment, Grid, Header, Dropdown, Input } from 'semantic-ui-react';
+import { Segment, Grid, Header, Dropdown, Input, Button } from 'semantic-ui-react';
 import 'react-day-picker/lib/style.css';
 
-const options = [
-  { key: 1, text: 'Today', value: 1 },
-  { key: 2, text: 'Yesterday', value: 2 },
-  { key: 3, text: 'Last 7 Days', value: 3 },
-  { key: 4, text: 'Last 30 Days', value: 4 },
-  { key: 5, text: 'Last Month', value: 5 },
-  { key: 6, text: 'This Month', value: 6 },
-  { key: 7, text: 'Custom Range', value: 100 },
-];
+const TODAY = 1;
+const YESTERDAY = 2;
+const LAST_7_DAYS = 3;
+const LAST_30_DAYS = 4;
+const LAST_MONTH = 5;
+const THIS_MONTH = 6;
+const CUSTOM_RANGE = 100;
 
+const datePresets = {
+  TODAY: { key: 1, text: 'Today', value: TODAY },
+  YESTERDAY: { key: 2, text: 'Yesterday', value: YESTERDAY },
+  LAST_7_DAYS: { key: 3, text: 'Last 7 Days', value: LAST_7_DAYS },
+  LAST_30_DAYS: { key: 4, text: 'Last 30 Days', value: LAST_30_DAYS },
+  LAST_MONTH: { key: 5, text: 'Last Month', value: LAST_MONTH },
+  THIS_MONTH: { key: 6, text: 'This Month', value: THIS_MONTH },
+  CUSTOM_RANGE: { key: 7, text: 'Custom Range', value: CUSTOM_RANGE },
+};
+
+const datePresetsOptions = Object.keys(datePresets).map(key => datePresets[key]);
+
+const ranges = {
+  [TODAY]: () => {
+    const today = moment().toDate();
+    return ({ from: today, to: today });
+  },
+  [YESTERDAY]: () => {
+    const yesterday = moment().subtract(1, 'days').toDate();
+    return ({ from: yesterday, to: yesterday });
+  },
+  [LAST_7_DAYS]: () => ({
+    from: moment().subtract(6, 'days').toDate(),
+    to: moment().toDate()
+  }),
+  [LAST_30_DAYS]: () => ({
+    from: moment().subtract(30, 'days').toDate(),
+    to: moment().toDate()
+  }),
+  [LAST_MONTH]: () => {
+    const todayMinusMonthMoment = moment().subtract(1, 'months');
+    return ({
+      from: todayMinusMonthMoment.startOf('month').toDate(),
+      to: todayMinusMonthMoment.endOf('month').toDate()
+    });
+  },
+  [THIS_MONTH]: () => ({
+    from: moment().startOf('month').toDate(),
+    to: moment().toDate()
+  })
+};
 
 class DateFilter extends Component {
 
   state = {
     from: null,
     to: null,
-    datePreset: options[0].value
+    datePreset: null
   };
 
-  handleDayClick = day => {
-    const range = DateUtils.addDayToRange(day, this.state);
-    this.setState(range);
-  };
+  componentDidMount() {
+    this.setRange(TODAY);
+  }
 
-  handleResetClick = e => {
-    e.preventDefault();
-    this.setState({
-      from: null,
-      to: null,
-    });
+  setRange(datePreset, from, to) {
+    let range = (ranges[datePreset] ? ranges[datePreset] : ranges[TODAY])();
+    if (datePreset === CUSTOM_RANGE) {
+      range = {};
+      if (from) {
+        range.from = from;
+      }
+
+      if (to) {
+        range.to = to;
+      }
+    }
+
+    // try to show all range in calendar
+    if (!from && !to) {
+      this.datePicker.showMonth(range.from);
+    }
+
+    this.setState({ ...range, datePreset });
+  }
+
+  handleDayClick = (day) => {
+    const { from, to } = this.state;
+    const range = DateUtils.addDayToRange(day, { from, to });
+    this.setRange(CUSTOM_RANGE, range.from, range.to);
   };
 
   handleDatePresetsChange = (event, data) => {
-    this.setState({ datePreset: data.value });
-  }
+    this.setRange(data.value);
+  };
 
   render() {
     const { from, to } = this.state;
@@ -50,24 +107,32 @@ class DateFilter extends Component {
             <Grid.Column width={11}>
               <DayPicker
                 numberOfMonths={2}
-                selectedDays={[from, { from, to }]}
+                selectedDays={{ from, to }}
                 onDayClick={this.handleDayClick}
+                // eslint-disable-next-line no-return-assign
+                ref={el => this.datePicker = el}
               />
             </Grid.Column>
             <Grid.Column width={5}>
               <Header textAlign="center">Select a start index</Header>
-              <Grid divided="vertically">
+              <Grid>
                 <Grid.Row>
                   <Grid.Column width={16}>
-                    <Dropdown fluid options={options} item value={this.state.datePreset} onChange={this.handleDatePresetsChange} />
+                    <Dropdown fluid options={datePresetsOptions} item value={this.state.datePreset} onChange={this.handleDatePresetsChange} />
                   </Grid.Column>
                 </Grid.Row>
                 <Grid.Row>
                   <Grid.Column width={8}>
-                    <Input value={from} fluid />
+                    <Input value={moment(new Date(from)).format('DD-MM-YYYY')} fluid />
                   </Grid.Column>
                   <Grid.Column width={8}>
-                    <Input value={to} fluid />
+                    <Input value={moment(new Date(to)).format('DD-MM-YYYY')} fluid />
+                  </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                  <Grid.Column textAlign="right">
+                    <Button type="button">Cancel</Button>
+                    <Button type="button" primary>Apply</Button>
                   </Grid.Column>
                 </Grid.Row>
               </Grid>
