1513748c4ff610ae32687027f01d8193604a9b79
bbfsdev <bbfsdev@gmail.com>
2017-06-03 04:34:33 +0000

    Source filter basic UX done.

--- src/components/filters/SourcesFilter/SourcesFilter.js
+++ src/components/filters/SourcesFilter/SourcesFilter.js
@@ -1,6 +1,6 @@
 import React  from 'react';
 import PropTypes from 'prop-types';
-import { List, Segment } from 'semantic-ui-react';
+import { Button, Divider, List, Segment } from 'semantic-ui-react';
 import map from 'lodash/map'
 
 export default class SourceFilter extends React.Component {
@@ -19,9 +19,13 @@ export default class SourceFilter extends React.Component {
     });
   };
 
+  onCancel = () => {
+    this.setState({ selection: [] });
+  }
+
   // Return all lists of selected sources.
   createLists = (depth, sources, selection) => {
-    if (!sources || Object.keys(sources).length == 0) {
+    if (!sources || Object.keys(sources).length === 0) {
       return [];
     }
     if (selection.length > 0) {
@@ -35,21 +39,37 @@ export default class SourceFilter extends React.Component {
 
   createList = (depth, sources, selected) => {
     return (
-      <List key={selected} divided relaxed selection style={{width: "33%"}}>{
-        map(sources, (v, k) =>
-          <List.Item active={selected === k}
-                     onClick={this.onSelectionChange}
-                     key={k} data-depth={depth} value={k}>{v.name}</List.Item>)}
-      </List>
+      <div key={selected} style={{width: "33%", paddingRight: "15px", height: "250px",
+                                  display: "inline-block", overflowY: "scroll"}}>
+        <List divided relaxed selection>{
+          map(sources, (v, k) =>
+            <List.Item active={selected === k}
+                       onClick={this.onSelectionChange}
+                       key={k} data-depth={depth} value={k}>{v.name}</List.Item>)}
+        </List>
+      </div>
     );
+  }
+
+  componentDidUpdate = (prevProps, prevState) => {
+    this.listContainer.scrollLeft = this.listContainer.scrollWidth;
   }
 
   render() {
     return (
-      <Segment basic attached="bottom" className="tab active" style={{display: "flex", flex: 1, flexDirection: "row"}}>
-        {!!this.props.sources ?
-          this.createLists(0, this.props.sources, this.state.selection).map(l => l) :
-          "Loading..."}
+      <Segment basic attached="bottom" className="tab active" clearing>
+        <div ref={el => this.listContainer = el} style={{overflowY: "hidden", overflowX: "scroll", width: "100%"}}>
+          <div style={{whiteSpace: "nowrap", width: "100%"}}>
+            {!!this.props.sources ?
+              this.createLists(0, this.props.sources, this.state.selection).map(l => l) :
+              "Loading..."}
+          </div>
+        </div>
+        <Divider />
+        <div>
+          <Button floated='right' onClick={this.onApply} primary>Apply</Button>
+          <Button floated='right' onClick={this.onCancel}>Cancel</Button>
+        </div>
       </Segment>
     );
   }
@@ -64,4 +84,6 @@ SourceShape.children = SourcesType;
 SourceFilter.propTypes = {
   sources: SourcesType,
   loading: PropTypes.bool,
+  onApply: PropTypes.func,
+  selection: PropTypes.arrayOf(PropTypes.string),
 };
