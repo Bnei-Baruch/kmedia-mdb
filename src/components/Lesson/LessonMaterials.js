8cc9760036fda2c320919fc3f4a4aa54a51052fb
Edo Shor <edoshor@gmail.com>
2017-06-14 18:38:13 +0300

    Lesson materials - first version of component. WIP

--- src/components/Lesson/LessonMaterials.js
+++ src/components/Lesson/LessonMaterials.js
@@ -1,34 +1,47 @@
-import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Segment } from 'semantic-ui-react';
 
 import * as shapes from '../shapes';
+import TabsMenu from '../shared/TabsMenu';
 
-const LessonMaterials = (props) => {
-  return (
-    <Menu
-      secondary
-      pointing
-      color="blue"
-      className="index-filters"
-      style={{
-        backgroundColor: 'gray',
-        backgroundImage: 'repeating-linear-gradient(45deg, transparent, transparent 35px, rgba(255,255,255,.5) 35px, rgba(255,255,255,.5) 70px)'
-      }}
-    >
-      <Menu.Item name="item-summary">Summary</Menu.Item>
-      <Menu.Item name="item-transcription">Transcription</Menu.Item>
-      <Menu.Item name="item-sources">Sources</Menu.Item>
-      <Menu.Item name="item-sketches">Sketches</Menu.Item>
-    </Menu>
-  );
-};
+class LessonMaterials extends Component {
 
-LessonMaterials.propTypes = {
-  lesson: shapes.LessonPart,
-};
+  static propTypes = {
+    lesson: shapes.LessonPart,
+  };
 
-LessonMaterials.defaultProps = {
-  lesson: undefined,
-};
+  static defaultProps = {
+    lesson: undefined,
+  };
+
+  render() {
+    const items = [
+      {
+        name: 'summary',
+        label: 'Summary',
+        component: <Segment basic>Summary</Segment>,
+      },
+      {
+        name: 'transcription',
+        label: 'Transcription',
+        component: <Segment basic>Transcription</Segment>,
+      },
+      {
+        name: 'sources',
+        label: 'Sources',
+        component: <Segment basic>Sources</Segment>,
+      },
+      {
+        name: 'sketches',
+        label: 'Sketches',
+        component: <Segment basic>Sketches</Segment>,
+      },
+    ];
+
+    return (
+      <TabsMenu items={items} />
+    );
+  }
+}
 
 export default LessonMaterials;
---
+++ src/components/shared/TabsMenu.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Menu } from 'semantic-ui-react';
+
+class TabsMenu extends Component {
+
+  static propTypes = {
+    items: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      component: PropTypes.node.isRequired,
+    })),
+    active: PropTypes.string,
+  };
+
+  static defaultProps = {
+    items: [],
+    active: '',
+  };
+
+  constructor(props) {
+    super(props);
+
+    let { active } = props;
+    if (!active) {
+      const { items } = props;
+      if (items.length > 0) {
+        active = items[0].name;
+      }
+    }
+
+    this.state = { active };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { active } = nextProps;
+    if (active !== this.props.active) {
+      this.setState({ active });
+    }
+  }
+
+  handleActiveChange = (e, { name }) => this.setState({ active: name });
+
+  render() {
+    const { active } = this.state;
+    const { items }  = this.props;
+    const activeItem = items.find(x => x.name === active);
+
+    return (
+      <div>
+        <Menu secondary pointing color="blue">
+          {
+            items.map((item) => {
+              const { name, label } = item;
+              return (
+                <Menu.Item
+                  key={name}
+                  name={name}
+                  active={active === name}
+                  onClick={this.handleActiveChange}
+                >
+                  {label}
+                </Menu.Item>
+              );
+            })
+          }
+        </Menu>
+        {activeItem.component}
+      </div>
+    );
+  }
+}
+
+export default TabsMenu;
