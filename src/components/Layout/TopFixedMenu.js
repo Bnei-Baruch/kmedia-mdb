14296b8530960358be1907ba316d98437aac9500
Edo Shor <edoshor@gmail.com>
2017-07-17 09:36:48 +0300

    General - refactor routing to allow multiple layouts.

--- src/components/Layout/TopFixedMenu.js
+++ src/components/Layout/TopFixedMenu.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import { Menu as RMenu, Icon, Container } from 'semantic-ui-react';
+import { Container, Icon, Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
-const TopFixedMenu = ({ title, toggleVisibility }) => (
-  <RMenu fixed="top" inverted color="blue">
-    <Container>
-      <RMenu.Item as="a" icon className="ui padded grid tablet mobile only" onClick={toggleVisibility}>
-        <Icon name="sidebar" style={{ padding: 0 }} />
-      </RMenu.Item>
-      <RMenu.Item header>
-        <h3 className="main-title">
-          Kabbalah Media
-          <small>&nbsp;- {title}</small>
-        </h3>
-      </RMenu.Item>
-      <RMenu.Item as={Link} to="/home"> Features </RMenu.Item>
-      <RMenu.Item as={Link} to="/home"> Testimonials </RMenu.Item>
-      <RMenu.Item as={Link} to="/home"> Sign-in </RMenu.Item>
-    </Container>
-  </RMenu>
-);
+const TopFixedMenu = (props) => {
+  const { title, toggleVisibility } = props;
+  return (
+    <Menu fixed="top" inverted color="blue">
+      <Container>
+        <Menu.Item as="a" icon className="ui padded grid tablet mobile only" onClick={toggleVisibility}>
+          <Icon name="sidebar" style={{ padding: 0 }} />
+        </Menu.Item>
+        <Menu.Item header>
+          <h3 className="main-title">
+            Kabbalah Media
+            <small>&nbsp;- {title}</small>
+          </h3>
+        </Menu.Item>
+        <Menu.Item as={Link} to="/home"> Features </Menu.Item>
+        <Menu.Item as={Link} to="/home"> Testimonials </Menu.Item>
+        <Menu.Item as={Link} to="/home"> Sign-in </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
 
 TopFixedMenu.propTypes = {
-  title           : PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
   toggleVisibility: PropTypes.func.isRequired,
 };
 
