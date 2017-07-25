45cdcb2d0ff5e7907bd5a8788cbcfbc0677c8f03
Edo Shor <edoshor@gmail.com>
2017-07-25 16:30:29 +0300

    I18n - move locales to public folder

--- bin/serve-locals.js
+++ bin/serve-locals.js
@@ -10,7 +10,7 @@ const port = process.argv[2] || 9876;
 
 const server = http.createServer((req, res) => {
   const pathname = url.parse(req.url).pathname;
-  const filePath = path.join(__dirname, '..', pathname);
+  const filePath = path.join(__dirname, '../public', pathname);
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
   res.setHeader('Access-Control-Allow-Origin', '*');
   const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
