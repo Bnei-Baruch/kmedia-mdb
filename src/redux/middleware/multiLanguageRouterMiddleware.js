a50a4beeef4972acff9038c671a08e25221e16c7
yanivel <yaniv.elimor@gmail.com>
2017-08-23 04:46:39 +0300

    routerMiddleware is actually appliled

--- src/redux/middleware/multiLanguageRouterMiddleware.js
+++ src/redux/middleware/multiLanguageRouterMiddleware.js
@@ -25,6 +25,6 @@ export default function multiLanguageRouterMiddleware(history) {
       }
     }
 
-    return routerMiddleware(next, action);
+    return routerMiddleware()(next)(action);
   };
 }
