b93b90894f2a4b20a2908b1d9e9ca196291d04f9
Gregory Shilin <gshilin@gmail.com>
2017-09-22 06:39:58 +0300

    program - beginning

--- src/components/Programs/Full/FullProgram.js
+++ src/components/Programs/Full/FullProgram.js
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import 'moment-duration-format';
 import { Trans, translate } from 'react-i18next';
-import { Menu, Table } from 'semantic-ui-react';
-
+import { Grid, Header, Image, Menu, Table } from 'semantic-ui-react';
 
 import { formatError } from '../../../helpers/utils';
 import { ErrorSplash, FrownSplash, LoadingSplash } from '../../shared/Splash';
@@ -27,9 +26,13 @@ class FullProgram extends Component {
     err: null,
   };
 
-  getName = (fullProgram, cu) => {
+  state = {
+    activePart: 0,
+  };
+
+  getName = (fullEvent, cu) => {
     const { name, duration } = cu;
-    const ccuName            = fullProgram.ccuNames[cu.id];
+    const ccuName            = fullEvent.ccuNames[cu.id];
     const durationDisplay    = moment.duration(duration, 'seconds').format('hh:mm:ss');
     return { name, ccuName, duration: durationDisplay };
   };
@@ -46,6 +49,27 @@ class FullProgram extends Component {
     );
   };
 
+  table = (fullProgram, t) => (
+    <Menu vertical fluid>
+      <Table basic="very" compact="very" celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>{t('programs.full.ccuName')}</Table.HeaderCell>
+            <Table.HeaderCell>{t('programs.full.name')}</Table.HeaderCell>
+            <Table.HeaderCell>{t('programs.full.duration')}</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {
+            fullProgram.content_units.map(cu => (
+              this.tableRow(fullProgram, cu)
+            ))
+          }
+        </Table.Body>
+      </Table>
+    </Menu>
+  );
+
   render() {
     const { fullProgram, wip, err, t } = this.props;
 
@@ -53,31 +77,36 @@ class FullProgram extends Component {
       return <ErrorSplash text={t('messages.server-error')} subtext={formatError(err)} />;
     }
 
-    if (fullProgram) {
-      return (
-        <Menu vertical fluid>
-          <Table basic="very" compact="very" celled>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>ccuName</Table.HeaderCell>
-                <Table.HeaderCell>Name</Table.HeaderCell>
-                <Table.HeaderCell>Duration</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {
-                fullProgram.content_units.map(cu => (
-                  this.tableRow(fullProgram, cu)
-                ))
-              }
-            </Table.Body>
-          </Table>
-        </Menu>
-      );
-    }
-
     if (wip) {
       return <LoadingSplash text={t('messages.loading')} subtext={t('messages.loading-subtext')} />;
+    }
+
+    if (fullProgram && fullProgram.content_units) {
+      return (
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Image fluid shape="rounded" src="http://www.kab.co.il/images/attachments/91/276191_medium.jpg" />
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Header as="h1">
+                <Header.Content>
+                  {fullProgram.name}
+                  <Header.Subheader>
+                    {fullProgram.content_units.length}&nbsp;{t('programs.full.episodes')}
+                  </Header.Subheader>
+                </Header.Content>
+              </Header>
+              <p>This is description. I have no idea where to take it from</p>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              {this.table(fullProgram, t)}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      );
     }
 
     return (
