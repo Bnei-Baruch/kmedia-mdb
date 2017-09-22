cec3c726c4425c12e17ffb77364bd7ea61afee74
bbfsdev <bbfsdev@gmail.com>
2017-09-22 09:24:40 +0000

    Initial Event Map Page!

--- src/components/Events/Full/FullEvent.js
+++ src/components/Events/Full/FullEvent.js
@@ -3,13 +3,17 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import 'moment-duration-format';
 import { Trans, translate } from 'react-i18next';
-import { Menu, Table } from 'semantic-ui-react';
+import { Container, Embed, Grid, Header, Image, Menu, Table } from 'semantic-ui-react';
 
+import { fromToLocalized } from '../../../helpers/date';
 import { formatError } from '../../../helpers/utils';
 import { ErrorSplash, FrownSplash, LoadingSplash } from '../../shared/Splash';
 import Link from '../../Language/MultiLanguageLink';
 import NavLink from '../../Language/MultiLanguageNavLink';
 import * as shapes from '../../shapes';
+
+import placeholder from './placeholder.png';
+import EventMap from './EventMap'
 
 class FullEvent extends Component {
   static propTypes = {
@@ -45,6 +49,10 @@ class FullEvent extends Component {
     );
   };
 
+  titleDate = (fromStr, toStr) =>{
+    return fromToLocalized(moment.utc(fromStr, "YYYY-MM-DD"), moment.utc(toStr, "YYYY-MM-DD"));
+  }
+
   render() {
     const { fullEvent, wip, err, t } = this.props;
 
@@ -53,25 +61,58 @@ class FullEvent extends Component {
     }
 
     if (fullEvent) {
+      console.log('fullEvent', fullEvent);
+
+      const description = !fullEvent.description ? null : (
+        <p>{fullEvent.description}</p>
+      );
+
       return (
-        <Menu vertical fluid>
-          <Table basic="very" compact="very" celled>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>ccuName</Table.HeaderCell>
-                <Table.HeaderCell>Name</Table.HeaderCell>
-                <Table.HeaderCell>Duration</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {
-                fullEvent.content_units.map(cu => (
-                  this.tableRow(fullEvent, cu)
-                ))
-              }
-            </Table.Body>
-          </Table>
-        </Menu>
+        <Container>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={3}>
+                 <Image fluid shape='rounded' src={placeholder} />
+              </Grid.Column>
+              <Grid.Column width={8}>
+                <Header as='h1'>
+                  <Header.Content>
+                    <small className='text grey'>{this.titleDate(fullEvent.start_date, fullEvent.end_date)}</small>
+                    <br/>
+                    {fullEvent.name}
+                    <Header.Subheader>
+                      {fullEvent.city}, {fullEvent.country}
+                    </Header.Subheader>
+                  </Header.Content>
+                </Header>
+                {description}
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <EventMap address={fullEvent.full_address} />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+          <Menu vertical fluid>
+            <Table basic="very" compact="very" celled>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>ccuName</Table.HeaderCell>
+                  <Table.HeaderCell>Name</Table.HeaderCell>
+                  <Table.HeaderCell>Duration</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {
+                  fullEvent.content_units.map(cu => (
+                    this.tableRow(fullEvent, cu)
+                  ))
+                }
+              </Table.Body>
+            </Table>
+          </Menu>
+        </Container>
       );
     }
 
