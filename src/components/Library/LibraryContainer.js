8e4caf14cb264829124dc2b91a148b6f2cd6e7dc
Edo Shor <edoshor@gmail.com>
2018-01-03 09:21:17 +0200

    Library - change language selector type and fix for proper text alignment when switching language.

--- src/components/Library/LibraryContainer.js
+++ src/components/Library/LibraryContainer.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { replace } from 'react-router-redux';
 import { translate } from 'react-i18next';
-import { Accordion, Grid, Sticky, Segment, Rail } from 'semantic-ui-react';
+import { Accordion, Grid, Rail, Segment, Sticky } from 'semantic-ui-react';
 
 import { actions as sourceActions, selectors as sources } from '../../redux/modules/sources';
 import { selectors as settings } from '../../redux/modules/settings';
@@ -15,7 +15,7 @@ import { ErrorSplash, FrownSplash } from '../shared/Splash';
 import LibraryContentContainer from './LibraryContentContainer';
 
 class LibraryContainer extends Component {
-  handleContextRef = contextRef => this.setState({ contextRef })
+
   static propTypes = {
     sourceId: PropTypes.string.isRequired,
     indexMap: PropTypes.objectOf(PropTypes.shape({
@@ -50,6 +50,9 @@ class LibraryContainer extends Component {
   state = {
     lastLoadedId: null,
   };
+
+  handleContextRef = contextRef =>
+    this.setState({ contextRef });
 
   componentDidMount() {
     const { sourceId, areSourcesLoaded, replace } = this.props;
@@ -202,7 +205,7 @@ class LibraryContainer extends Component {
   };
 
   render() {
-    const { contextRef } = this.state
+    const { contextRef }                      = this.state;
     const { indexMap, sourceId, language, t } = this.props;
 
     const index = isEmpty(sourceId) ? {} : indexMap[sourceId];
@@ -227,12 +230,12 @@ class LibraryContainer extends Component {
           <Grid.Column>
             <div ref={this.handleContextRef}>
               <Segment>
-                <div className='source__content'>
+                <div className="source__content">
                   {this.header(this.properParentId(this.props.sourceId))}
                   {content}
                 </div>
-                <Rail position='left'>
-                  <Sticky context={contextRef} offset={60} className='toc'>
+                <Rail position="left">
+                  <Sticky context={contextRef} offset={60} className="toc">
                     <p>{t('sources-library.toc')}</p>
                     <Accordion fluid styled defaultActiveIndex={0} panels={this.toc(parent, true)} />
                   </Sticky>
