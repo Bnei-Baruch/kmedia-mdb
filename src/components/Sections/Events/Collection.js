6ce8e8e8c6d6aa877c78102edc2373c0500b759c
Edo Shor <edoshor@gmail.com>
2018-01-15 04:11:25 +0200

    Event playlist - scroll to selected item

--- src/components/Sections/Events/Collection.js
+++ src/components/Sections/Events/Collection.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment/moment';
-import { Header, Image, Menu } from 'semantic-ui-react';
+import { Header, Image, Menu, Ref } from 'semantic-ui-react';
 
 import { DATE_FORMAT, NO_NAME } from '../../../helpers/consts';
 import { fromToLocalized } from '../../../helpers/date';
@@ -11,12 +11,29 @@ import logo from '../../../images/event_logo.png';
 
 class MyPlaylistWidget extends PlaylistWidget {
 
+  handleSelectedRef = x => this.setState({ selectedRef: x });
+
+  componentDidUpdate(prevProps, prevState) {
+    // We want selected item to visible to the user.
+    // So we scrollIntoView if it's not.
+    if (this.state.selectedRef &&
+      (!prevState || this.state.selectedRef !== prevState.selectedRef)) {
+
+      const { top: pTop, bottom: pBottom } = this.playlistView.getBoundingClientRect();
+      const { top, bottom }                = this.state.selectedRef.getBoundingClientRect();
+
+      if ((bottom < pTop) || (top > pBottom)) {
+        this.state.selectedRef.scrollIntoView();
+      }
+    }
+  }
+
   renderHeader() {
     const { name, start_date, end_date } = this.props.playlist.collection;
 
     return (
       <Header inverted as="h2">
-        <Image circular src={logo} floated='left' />{name}
+        <Image circular src={logo} floated="left" />{name}
         <Header.Subheader>
           {fromToLocalized(moment.utc(start_date, DATE_FORMAT), moment.utc(end_date, DATE_FORMAT))}
         </Header.Subheader>
@@ -28,7 +45,7 @@ class MyPlaylistWidget extends PlaylistWidget {
     const { t, selected } = this.props;
 
     return (
-      <div key={section} className='avbox__playlist-view-section'>
+      <div key={section} className="avbox__playlist-view-section">
         <Header inverted as="h3">
           {t(`events.collection.playlist.${section}`)}&nbsp;&nbsp;
           <small>{items.length} {t(`pages.collection.items.event`)}</small>
@@ -41,14 +58,14 @@ class MyPlaylistWidget extends PlaylistWidget {
                 let prefix = '';
                 switch (section) {
                 case 'preparation':
-                case 'after_parties':
+                case 'appendices':
                   prefix = t('values.date', { date: new Date(unit.film_date) });
                   break;
                 default:
                   break;
                 }
 
-                return (
+                const mItem = (
                   <Menu.Item
                     key={unit.id}
                     name={`${offset + index}`}
@@ -56,13 +73,18 @@ class MyPlaylistWidget extends PlaylistWidget {
                     onClick={this.handleItemClick}
                   >
                     {
-                      section === 'preparation' || section === 'after_parties' ?
+                      section === 'preparation' || section === 'appendices' ?
                         <strong>{t('values.date', { date: new Date(unit.film_date) })} &nbsp;</strong> :
                         null
                     }
                     {unit.name || NO_NAME} - {formatDuration(unit.duration)}
                   </Menu.Item>
                 );
+
+                if ((offset + index) === selected) {
+                  return <Ref innerRef={this.handleSelectedRef} key={unit.id}>{mItem}</Ref>;
+                }
+                return mItem;
               }
             )
           }
@@ -75,9 +97,12 @@ class MyPlaylistWidget extends PlaylistWidget {
     const { items, groups } = this.props.playlist;
 
     return (
-      <div className='avbox__playlist-view'>
+      <div
+        ref={c => this.playlistView = c}
+        className="avbox__playlist-view"
+      >
         {
-          ['preparation', 'lessons', 'other_parts', 'after_parties']
+          ['lessons', 'other_parts', 'preparation', 'appendices']
             .map(x => {
               const offsets = groups[x];
               if (!offsets) {
