5bf59c9af9d6b112c33685855a0013ff8db72e71
Oleg Milshtein <oleg.mil@gmail.com>
2018-02-19 05:20:20 +0200

    tabs on unit - mobile fixed

--- src/components/Pages/Unit/widgets/UnitMaterials/Sketches.js
+++ src/components/Pages/Unit/widgets/UnitMaterials/Sketches.js
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
-import { Container, Divider, Segment } from 'semantic-ui-react';
+import { Container, Segment } from 'semantic-ui-react';
 
 import { RTL_LANGUAGES } from '../../../../../helpers/consts';
 import { assetUrl, imaginaryUrl, Requests } from '../../../../../helpers/Api';
@@ -212,7 +212,6 @@ class Sketches extends React.Component {
               onImageError={this.handleImageError}
             />
           </div>
-          <Divider hidden />
         </div>
       );
     }
