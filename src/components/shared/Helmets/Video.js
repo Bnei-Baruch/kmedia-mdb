7f44411f87e21eb247b33abf533dfded14722f98
orineden1 <orineden@gmail.com>
2018-04-16 03:19:05 +0300

    Helmets: add Video urls to AVUnit
    (there is a bug where the open graph structure is not in one block)

--- src/components/shared/Helmets/Video.js
+++ src/components/shared/Helmets/Video.js
@@ -8,40 +8,55 @@ class Video extends Component {
     url: PropTypes.string.isRequired,
     releaseDate: PropTypes.string,
     duration: PropTypes.number,
-    type: PropTypes.oneOf(['movie', 'tv_show', 'episode', 'other']),
-    tvShowUrl: function(props, propName, componentName) {
-      if (props.type === 'episode') {
-        if (!props['tvShowUrl']) {
-          return new Error(`${propName} should be provided for episode.`);
-        }
-      }
-    },
+    mimetype: PropTypes.string,
+    width: PropTypes.number,
+    height: PropTypes.number,
+
+    // category: PropTypes.oneOf(['movie', 'tv_show', 'episode', 'other']),
+    // tvShowUrl: function(props, propName, componentName) {
+    //   if (props.category === 'episode') {
+    //     if (!props['tvShowUrl']) {
+    //       return new Error(`${propName} should be provided for episode.`);
+    //     }
+    //   }
+    // },
     tags: PropTypes.arrayOf(PropTypes.string),
   };
 
   static defaultProps = {
-    type: 'other',
+    releaseDate: undefined,
+    duration: undefined,
+    mimetype: undefined,
+    width: undefined,
+    height: undefined,
     tags: []
   };
 
   render() {
-    const { releaseDate, duration, type, tags, tvShowUrl } = this.props;
+    const { url, releaseDate, duration, mimetype, width, height, tags } = this.props;
 
     // tv_show - to be used in the main page of tv show.
     // episode - to be used for a specific episode
     // video:series - a url to the tv show
     return (
       <Helmet>
-        <meta property="og:type" content={`video.${type}`} />
+        {/*<meta property="og:type" content={`video.${category}`} />*/}
+        <meta property="og:type" content="video.other" />
+        <meta property="og:video" content={url} />
+        <meta property="og:video:secure_url" content={url} />
 
-        {!isEmpty(tvShowUrl) ? <meta name="video:series" content={tvShowUrl} /> : null}
-
-        {/*<meta property="og:video" content="http://example.com/movie.swf" />*/}
         {/*<meta property="og:video:secure_url" content="https://secure.example.com/movie.swf" />*/}
+        {!isEmpty(mimetype) ? <meta property="og:video:type" content={mimetype} /> : null}
+        {width ? <meta property="og:video:width" content={width} /> : null}
+        {height ? <meta property="og:video:height" content={height} /> : null}
 
-        {!isEmpty(releaseDate) ? <meta name="video:release_date" content={releaseDate} /> : null}
-        {!isEmpty(duration) ? <meta name="video:duration" content={duration} /> : null}
-        {tags.map((tag, index) => <meta name="video:tag" content={tag} key={index} />)}
+
+        {/*{!isEmpty(tvShowUrl) ? <meta name="video:series" content={tvShowUrl} /> : null}*/}
+
+
+        {!isEmpty(releaseDate) ? <meta name="og:video:release_date" content={releaseDate} /> : null}
+        {duration ? <meta name="og:video:duration" content={duration} /> : null}
+        {/*{tags.map((tag, index) => <meta name="og:video:tag" content={tag} key={index} />)}*/}
       </Helmet>
     );
   }
