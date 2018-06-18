81f860af6a8ce5c99f703fc3954a336ef8e7b540
Edo Shor <edoshor@gmail.com>
2018-06-18 17:54:31 +0300

    Scroll to top on navigation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import { ConnectedRouter } from 'react-router-redux';
 import { I18nextProvider } from 'react-i18next';
 
 import routes from '../../routes';
+import ScrollToTop from '../shared/ScrollToTop/ScrollToTop';
 import '../../stylesheets/Kmedia.css';
 
 class App extends Component {
@@ -14,7 +15,9 @@ class App extends Component {
       <I18nextProvider i18n={i18n} initialI18nStore={initialI18nStore} initialLanguage={initialLanguage}>
         <Provider store={store}>
           <ConnectedRouter history={history}>
-            {renderRoutes(routes)}
+            <ScrollToTop>
+              {renderRoutes(routes)}
+            </ScrollToTop>
           </ConnectedRouter>
         </Provider>
       </I18nextProvider>
