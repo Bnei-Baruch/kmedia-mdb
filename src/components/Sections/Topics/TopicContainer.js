d621d4230b488c4b2b8f55f444688d929ac1c107
Gabi Wissotzky <wissog@gmail.com>
2018-06-27 05:35:27 +0300

    eslint changes and remove fetchTags

--- src/components/Sections/Topics/TopicContainer.js
+++ src/components/Sections/Topics/TopicContainer.js
@@ -8,10 +8,10 @@ import { TOPICS_FOR_DISPLAY } from '../../../helpers/consts';
 import { selectors } from '../../../redux/modules/tags';
 import Link from '../../Language/MultiLanguageLink';
 
-class TopicContainer extends Component{
+class TopicContainer extends Component {
 
   static propTypes = {
-    roots: PropTypes.arrayOf(PropTypes.string).isRequired,
+    roots: PropTypes.arrayOf(PropTypes.string),
     byId: PropTypes.object.isRequired,
   };
 
@@ -19,82 +19,80 @@ class TopicContainer extends Component{
     roots: []
   };
 
-  //root will be main title
-  //subroot will be subtitle
-  //the rest will be a tree - List of Lists
+  /* root will be main title
+  subroot will be subtitle
+  the rest will be a tree - List of Lists */
 
-  renderLeaf(node){
-    return(
-      <Link to={`/topics/${node.id}`}>
-        {node.label}
-      </Link>
-    );
-  }
+  renderLeaf = node => (
+    <Link to={`/topics/${node.id}`}>
+      {node.label}
+    </Link>
+  );
 
-  renderNode(node){
-    const {byId} = this.props;
+  renderNode = (node) => {
+    const { byId } = this.props;
 
-    return(
-        <div>
-          {
-            Array.isArray(node.children) && node.children.length > 0 ?
-              <List relaxed>
-                {
-                  node.children.map(id => 
+    return (
+      <div>
+        {
+          Array.isArray(node.children) && node.children.length > 0 ?
+            <List relaxed>
+              {
+                node.children.map(id =>
+                  (
                     <List.Item key={id}>
                       {this.renderNode(byId[id])}
-                    </List.Item>  
+                    </List.Item>
                   )
-                }
-              </List> :
-              this.renderLeaf(node)
-          }
-        </div>
+                )
+              }
+            </List> :
+            this.renderLeaf(node)
+        }
+      </div>
     );
   }
 
-  renderSubHeader(node){
-    return(
-      <div key={node.id} className="topics__list">
-        <Header as='h4'> 
-          {node.label}  
-        </Header>
-        { this.renderNode(node) }
-      </div>  
-    );
-  }
+  renderSubHeader = node => (
+    <div key={node.id} className="topics__list">
+      <Header as="h4">
+        { node.label }
+      </Header>
+      { this.renderNode(node) }
+    </div>
+  )
 
-  renderBranch(rootId){
+  renderBranch = (rootId) => {
     const { byId } = this.props;
     const rootNode = byId[rootId];
     const rootChildren = rootNode.children;
 
-    if (!rootChildren){
+    if (!rootChildren) {
       return null;
     }
 
     return (
       <div key={rootId} >
-        <Header as='h2'> {rootNode.label} </Header>
+        <Header as="h2"> {rootNode.label} </Header>
         {
-          rootChildren.map(id => this.renderSubHeader(byId[id])) 
+          rootChildren.map(id => this.renderSubHeader(byId[id]))
         }
       </div>
     );
   }
-  
-  render(){
-    const {roots} = this.props;
-    console.log('roots:',roots);
 
-    return(
+  render() {
+    const { roots } = this.props;
+    // console.log('roots:',roots);
+
+    return (
       <Container fluid>
         <SectionHeader section="topics" />
         <div>
-        {
-          roots.map(r => this.renderBranch(r))
-        }
-        </div>  
+          {
+            roots.map(r => this.renderBranch(r))
+          }
+        </div>
       </Container>
     );
   }
--- src/components/Sections/Topics/TopicPage.js
+++ src/components/Sections/Topics/TopicPage.js
@@ -6,86 +6,83 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import SectionHeader from '../../shared/SectionHeader';
-import {actions, selectors} from '../../../redux/modules/tags';
+import { actions, selectors } from '../../../redux/modules/tags';
 import { isEmpty } from '../../../helpers/utils';
 import TopN from './TopN';
 
 export const topNItems = 5;
 
 class TopicPage extends Component {
-
     static propTypes = {
-      sections: PropTypes.arrayOf(PropTypes.string),
+      sections: PropTypes.arrayOf(PropTypes.string).isRequired,
       getSectionUnits: PropTypes.func.isRequired,
-      getPathByID: PropTypes.func,
+      getPathByID: PropTypes.func.isRequired,
       fetchDashboard: PropTypes.func.isRequired,
-      fetchTags: PropTypes.func.isRequired,
+      // fetchTags: PropTypes.func.isRequired,
     }
 
-    componentDidMount(){
+    componentDidMount() {
       this.loadTopic(this.props);
     }
 
-    componentWillReceiveProps(nextProps){
-      if (this.props.match.params.id !== nextProps.match.params.id){
+    componentWillReceiveProps(nextProps) {
+      if (this.props.match.params.id !== nextProps.match.params.id) {
         this.loadTopic(nextProps);
       }
     }
 
-    loadTopic(nextProps){
+    loadTopic = (nextProps) => {
       const { fetchDashboard, fetchTags } = nextProps;
       const tagId = nextProps.match.params.id;
 
-      fetchTags();
+      // fetchTags();
       fetchDashboard(tagId);
     }
-        
-    render(){
-      const {sections, getSectionUnits, getPathByID} = this.props;
+
+    render() {
+      const { sections, getSectionUnits, getPathByID } = this.props;
       const tagId = this.props.match.params.id;
 
-      if (getPathByID && !isEmpty(sections)){
+      if (getPathByID && !isEmpty(sections)) {
         const tagPath = getPathByID(tagId);
 
-        return(
+        return (
           <Container>
             <SectionHeader section="topics" />
             <Grid container doubling columns={sections.length} className="homepage__iconsrow">
               {
-                sections.map(s => {
+                sections.map((s) => {
                   const sectionUnits = getSectionUnits(s);
-                  const retVal = isEmpty(sectionUnits) ? 
-                                 null :
-                                 <Grid.Column key={s}>
-                                    <TopN section={s} 
-                                          units={sectionUnits} 
-                                          N={topNItems} 
-                                          tagId={tagId} 
-                                          tagPath={tagPath}
-                                    />
-                                 </Grid.Column> 
-
-                  return retVal
+                  return isEmpty(sectionUnits) ?
+                    null :
+                    <Grid.Column key={s}>
+                      <TopN
+                        section={s}
+                        units={sectionUnits}
+                        N={topNItems}
+                        tagId={tagId}
+                        tagPath={tagPath}
+                      />
+                    </Grid.Column>;
                 })
               }
             </Grid>
           </Container>
         );
       }
-      else{
-        return <div>Topic {tagId} Not Found</div>
-      }
+
+      return <div>Topic {tagId} Not Found</div>;
     }
 }
 
 export default withRouter(connect(
-  (state) => ({
+  state => ({
     sections: selectors.getSections(state.tags),
     getSectionUnits: selectors.getSectionUnits(state.tags),
     getPathByID: selectors.getPathByID(state.tags)
   }),
   dispatch => bindActionCreators({
     fetchDashboard: actions.fetchDashboard,
-    fetchTags: actions.fetchTags
+    // fetchTags: actions.fetchTags
   }, dispatch)
 )(TopicPage));
