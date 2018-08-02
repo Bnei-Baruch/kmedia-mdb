3f60dbd27d9c19cbb00265b1f1d402a4bf546bcd
Edo Shor <edoshor@gmail.com>
2018-08-02 12:26:11 +0300

    I18n - german

--- src/helpers/i18nnext.js
+++ src/helpers/i18nnext.js
@@ -5,6 +5,7 @@ import 'moment/locale/he';
 import 'moment/locale/ru';
 import 'moment/locale/es';
 import 'moment/locale/uk';
+import 'moment/locale/de';
 
 import { DEFAULT_LANGUAGE } from './consts';
 
