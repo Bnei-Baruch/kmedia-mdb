65056af6ccf4e09dcfc9aefef4eb8a3effcce102
Gabi Wissotzky <wissog@gmail.com>
2018-08-31 15:20:15 +0300

    tried using react-to-print not working yet

--- src/components/Sections/Library/LibraryContainer.js
+++ src/components/Sections/Library/LibraryContainer.js
@@ -9,6 +9,7 @@ import classnames from 'classnames';
 import { translate } from 'react-i18next';
 import { Button, Container, Grid, Header, Input, Ref } from 'semantic-ui-react';
 import PrintProvider, { Print, NoPrint } from 'react-easy-print';
+import ReactToPrint from 'react-to-print';
 
 import { formatError, isEmpty } from '../../../helpers/utils';
 import { actions as assetsActions, selectors as assets } from '../../../redux/modules/assets';
@@ -303,8 +304,12 @@ class LibraryContainer extends Component {
     );
   };
 
-  print = () => {
-    window.print();
+  print = (contentToPrint) => {
+    // window.print();
+    // <ReactToPrint content={contentToPrint}
+    //   trigger={}
+    // />
+
   }
 
   render() {
@@ -340,87 +345,100 @@ class LibraryContainer extends Component {
     const matchString = this.matchString(parentId, t);
 
     return (
-      <PrintProvider>
-        <NoPrint>
-          <div
-            className={classnames({
-              source: true,
-              'is-readable': isReadable,
-              'toc--is-active': tocIsActive,
-              [`is-${theme}`]: true,
-              [`is-${fontType}`]: true,
-            })}
-          >
-            <div className="layout__secondary-header" ref={this.handleSecondaryHeaderRef}>
-              <Container>
-                <Grid padded centered>
-                  <Grid.Row verticalAlign="bottom">
-                    <Grid.Column mobile={16} tablet={16} computer={4} className="source__toc-header">
-                      <div className="source__header-title mobile-hidden">
-                        <Header size="small">{t('sources-library.toc')}</Header>
-                      </div>
-                      <div className="source__header-toolbar">
-                        {matchString}
-                        {this.switchSortingOrder(parentId)}
-                        <Button compact size="small" className="computer-hidden large-screen-hidden widescreen-hidden" icon="list layout" onClick={this.handleTocIsActive} />
-                      </div>
-                    </Grid.Column>
-                    <Grid.Column mobile={16} tablet={16} computer={12} className="source__content-header">
-                      <div className="source__header-title">{this.header(sourceId, fullPath)}</div>
-                      <div className="source__header-toolbar">
-                        <div id="download-button" />
-                        <LibrarySettings fontSize={this.state.fontSize} handleSettings={this.handleSettings} />
-                        <Button compact size="small" icon={isReadable ? 'compress' : 'expand'} onClick={this.handleIsReadable} />
-                        <Button compact size="small" className="computer-hidden large-screen-hidden widescreen-hidden" icon="list layout" onClick={this.handleTocIsActive} />
-                        <Button compact size="small" icon="print" onClick={this.print} />
-                      </div>
-                    </Grid.Column>
-                  </Grid.Row>
-                </Grid>
-              </Container>
-            </div>
-            <Container style={{ paddingTop: `${secondaryHeaderHeight}px` }}>
-              <Grid padded centered>
-                <Grid.Row className="is-fitted">
-                  <Grid.Column mobile={16} tablet={16} computer={4} onClick={this.handleTocIsActive}>
-                    <TOC
-                      match={matchString ? match : ''}
-                      matchApplied={this.handleFilterClear}
-                      fullPath={fullPath}
-                      rootId={parentId}
-                      contextRef={this.contextRef}
-                      getSourceById={getSourceById}
-                      apply={this.props.push}
-                      stickyOffset={secondaryHeaderHeight + (isReadable ? 0 : 60)}
-                      t={t}
+      // <PrintProvider>
+    // <NoPrint>
+      <div
+        className={classnames({
+          source: true,
+          'is-readable': isReadable,
+          'toc--is-active': tocIsActive,
+          [`is-${theme}`]: true,
+          [`is-${fontType}`]: true,
+        })}
+      >
+        <div className="layout__secondary-header" ref={this.handleSecondaryHeaderRef}>
+          <Container>
+            <Grid padded centered>
+              <Grid.Row verticalAlign="bottom">
+                <Grid.Column mobile={16} tablet={16} computer={4} className="source__toc-header">
+                  <div className="source__header-title mobile-hidden">
+                    <Header size="small">{t('sources-library.toc')}</Header>
+                  </div>
+                  <div className="source__header-toolbar">
+                    {matchString}
+                    {this.switchSortingOrder(parentId)}
+                    <Button compact size="small" className="computer-hidden large-screen-hidden widescreen-hidden" icon="list layout" onClick={this.handleTocIsActive} />
+                  </div>
+                </Grid.Column>
+                <Grid.Column mobile={16} tablet={16} computer={12} className="source__content-header">
+                  <div className="source__header-title">{this.header(sourceId, fullPath)}</div>
+                  <div className="source__header-toolbar">
+                    <div id="download-button" />
+                    <LibrarySettings fontSize={this.state.fontSize} handleSettings={this.handleSettings} />
+                    <Button compact size="small" icon={isReadable ? 'compress' : 'expand'} onClick={this.handleIsReadable} />
+                    <Button compact size="small" className="computer-hidden large-screen-hidden widescreen-hidden" icon="list layout" onClick={this.handleTocIsActive} />
+                    {/* <Button compact size="small" icon="print" onClick={this.print(content)} /> */}
+                    <ReactToPrint
+                      trigger={() => (
+                        // <a href="#">Print!</a>
+                        <Button compact size="small" icon="print" /* onClick */ />
+                      )}
+                      content={() => content}
+                      onBeforePrint={() => {
+                        console.log('before print!');
+                      }}
+                      onAfterPrint={() => {
+                        console.log('after print!');
+                      }}
                     />
-                  </Grid.Column>
-                  <Grid.Column
-                    mobile={16}
-                    tablet={16}
-                    computer={12}
-                    className={classnames({
-                      'source__content-wrapper': true,
-                      [`size${fontSize}`]: true,
-                    })}
+                  </div>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </div>
+        <Container style={{ paddingTop: `${secondaryHeaderHeight}px` }}>
+          <Grid padded centered>
+            <Grid.Row className="is-fitted">
+              <Grid.Column mobile={16} tablet={16} computer={4} onClick={this.handleTocIsActive}>
+                <TOC
+                  match={matchString ? match : ''}
+                  matchApplied={this.handleFilterClear}
+                  fullPath={fullPath}
+                  rootId={parentId}
+                  contextRef={this.contextRef}
+                  getSourceById={getSourceById}
+                  apply={this.props.push}
+                  stickyOffset={secondaryHeaderHeight + (isReadable ? 0 : 60)}
+                  t={t}
+                />
+              </Grid.Column>
+              <Grid.Column
+                mobile={16}
+                tablet={16}
+                computer={12}
+                className={classnames({
+                  'source__content-wrapper': true,
+                  [`size${fontSize}`]: true,
+                })}
+              >
+                <div ref={this.handleContextRef}>
+                  {/* <Print name="mySource" single> */}
+                  <div
+                    className="source__content"
+                    style={{ minHeight: `calc(100vh - ${secondaryHeaderHeight + (isReadable ? 0 : 60) + 14}px)` }}
                   >
-                    <div ref={this.handleContextRef}>
-                      <Print name="mySource" single>
-                        <div
-                          className="source__content"
-                          style={{ minHeight: `calc(100vh - ${secondaryHeaderHeight + (isReadable ? 0 : 60) + 14}px)` }}
-                        >
-                          {content}
-                        </div>
-                      </Print>
-                    </div>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Container>
-          </div>
-        </NoPrint>
-      </PrintProvider>
+                    {content}
+                  </div>
+                  {/* </Print> */}
+                </div>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </div>
+    // </NoPrint>
+      // </PrintProvider>
     );
   }
 }
