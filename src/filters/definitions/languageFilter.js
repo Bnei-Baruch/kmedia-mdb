b4f409f2c8cfd9707eaf845258f7a5eab1e94c38
LAPTOP-6SLSD0V3\Yuri <yurihechter@gmail.com>
2018-09-14 05:36:32 +0300

    language filter progress

---
+++ src/filters/definitions/languageFilter.js
@@ -0,0 +1,15 @@
+import { createFilterDefinition } from './util';
+
+export const languageFilter = createFilterDefinition({
+  name: 'language-filter',
+  queryKey: 'language',
+  valueToApiParam: value => {
+    filter_section_language: [value ]
+  },
+  valueToTagLabel: (value, props, store, t) => {
+    if (!value) {
+      return '';
+    }
+    return t(value);
+  }
+});
