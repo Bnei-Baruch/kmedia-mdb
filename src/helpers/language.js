2be4e7f9222f91c76f8ea5c7539b2264e85aa7ee
Gregory Shilin <gshilin@gmail.com>
2018-09-14 12:29:32 +0300

    improve performance

--- src/helpers/language.js
+++ src/helpers/language.js
@@ -8,22 +8,21 @@ import { LANG_ENGLISH } from './consts';
  * @returns the most appropriate language
  */
 export const selectSuitableLanguage = (contentLanguage, uiLanguage, languages = []) => {
-  if (languages.indexOf(contentLanguage) !== -1) {
+  if (languages.length === 0) {
+    // we don't have data for new UI language. Let's stay as we are.
+    return false;
+  } else if (languages.includes(contentLanguage)) {
     // Use content language [preferred one]
     return contentLanguage;
-  } else if (languages.indexOf(uiLanguage) !== -1) {
+  } else if (languages.includes(uiLanguage)) {
     // Or UI language
     return uiLanguage;
-  } else if (languages.indexOf(LANG_ENGLISH) !== -1) {
+  } else if (languages.includes(LANG_ENGLISH)) {
     // Or English
     return LANG_ENGLISH;
-  } else if (languages.length > 0) {
-    // Or first available language
-    // eslint-disable-next-line prefer-destructuring
-    return languages[0];
   }
-
-  // we don't have data for new UI language. Let's stay as we are.
-  return false;
+  // Or first available language
+  // eslint-disable-next-line prefer-destructuring
+  return languages[0];
 };
 
