e9401628ee8983eb2dd2a59c3e113301ffde3a6e
Gregory Shilin <gshilin@gmail.com>
2018-11-16 10:20:20 +0200

    key cannot be object

--- src/components/Sections/Library/Homepage.js
+++ src/components/Sections/Library/Homepage.js
@@ -29,7 +29,7 @@ class Homepage extends Component {
       return isEmpty(author.children) ?
         null :
         <Kabbalist
-          key={author}
+          key={author.id}
           author={author}
           getSourceById={getSourceById}
           portrait={portraits[k]}
