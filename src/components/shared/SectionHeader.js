c538d8083d1f1a73f4b5fe5cbc02917b2c4a8913
Edo Shor <edoshor@gmail.com>
2018-11-27 03:09:02 +0200

    fix for code style

--- src/components/shared/SectionHeader.js
+++ src/components/shared/SectionHeader.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
 import { translate } from 'react-i18next';
 import { Container, Grid, Header, Menu } from 'semantic-ui-react';
 
