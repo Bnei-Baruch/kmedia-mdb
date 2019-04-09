b7ba7cb009bcfbf797c45860a4b09c47a749666f
DELL-LT\guya <guy.atia@gmail.com>
2019-04-09 05:44:14 +0300

    AR-509_put_date_picker_to_modal_as_designed

--- src/components/Filters/components/Date/FastDayPicker.js
+++ src/components/Filters/components/Date/FastDayPicker.js
@@ -7,13 +7,15 @@ import scrollIntoView from 'scroll-into-view';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import Navbar from 'react-day-picker/lib/src/Navbar';
 import MomentLocaleUtils, { formatDate, parseDate } from 'react-day-picker/moment';
-import { Input, Segment } from 'semantic-ui-react';
+import { Input, Segment, Modal } from 'semantic-ui-react';
 import 'react-day-picker/lib/style.css';
 
 import { today } from '../../../../helpers/date';
 import { getLanguageDirection, getLanguageLocaleWORegion } from '../../../../helpers/i18n-utils';
 import * as shapes from '../../../shapes';
 import YearMonthForm from './YearMonthForm';
+
+import DayPicker from 'react-day-picker';
 
 class FastDayPicker extends Component {
   static propTypes = {
@@ -32,7 +34,10 @@ class FastDayPicker extends Component {
 
   state = {
     month: null,
+    open: false,
   };
+
+  inputElement = null;
 
   isMobileDevice = () => this.props.deviceInfo.device && this.props.deviceInfo.device.type === 'mobile';
 
@@ -97,7 +102,7 @@ class FastDayPicker extends Component {
 
   render() {
     const { language, value, label, onDayChange } = this.props;
-    const { month }                               = this.state;
+    const { month, open, topPos, leftPos }        = this.state;
     const selected                                = value || today().toDate();
     const selectedToString                        = moment(selected).format('YYYY-MM-DD');
     const locale                                  = getLanguageLocaleWORegion(language);
@@ -135,7 +140,7 @@ class FastDayPicker extends Component {
 
     return (
       <div>
-        <DayPickerInput
+        {/* <DayPickerInput
           overlayComponent={this.getOverlayComponent}
           component={Input}
           value={selected}
@@ -163,7 +168,67 @@ class FastDayPicker extends Component {
             captionElement: () => null,
             navbarElement: props => this.getNavBarElement(props, language)
           }}
+        /> */}
+        <Input 
+            fluid
+            label
+            size="small"
+            icon="calendar alternate outline"
+            placeholder={`${formatDate(new Date(), 'l', locale)}`}
+            value={selectedToString}
+            format="l"
+            onClick={() => 
+            {
+              const domNode = ReactDOM.findDOMNode(this.inputElement);
+              let rect = domNode.getBoundingClientRect();              
+              let topPos = rect.top - 305;
+              if (topPos < 0)
+                topPos = 0;
+              this.setState({ open: true , topPos: topPos, leftPos: rect.left });
+            }
+          } 
+          ref={(el) => {
+            this.inputElement = el;
+          }}        
         />
+ <Modal dimmer={false}
+        open={open} onClose={() => this.setState({ open: false })} 
+
+        closeOnDimmerClick={false}
+        closeOnDocumentClick={false}
+        closeOnPortalMouseLeave={false}
+        closeOnTriggerBlur={false}
+        closeOnTriggerMouseLeave={false}
+
+        style={{ 
+          width: '300px', 
+          top: topPos,
+          left: leftPos,
+          }}>
+          <Modal.Content >
+          <DayPicker
+                      locale={locale}
+                      localeUtils={MomentLocaleUtils}
+                      disabledDays={{ after: new Date() }}                  
+                      captionElement={() => null}
+                      navbarElement={props => this.getNavBarElement(props, language)}
+                      month={month}
+                      toMonth={today().toDate()}
+                      dir= {getLanguageDirection(language)}
+                      ref = {this.handleDayPickerRef}
+                      onDayChange={onDayChange}
+                      hideOnDayClick={false}                      
+
+                      closeOnDimmerClick={false}
+                      closeOnDocumentClick={false}
+                      closeOnPortalMouseLeave={false}
+                      closeOnTriggerBlur={false}
+                      closeOnTriggerMouseLeave={false}
+
+                    />
+          </Modal.Content>
+        </Modal>
+        
       </div>
     );
   }
