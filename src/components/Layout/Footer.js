43aedc6231159cdea76eacfdd49688a6cabd6cad
davgur <davgur.ru@gmail.com>
2019-04-16 05:03:40 +0300

    add rss to footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withNamespaces } from 'react-i18next';
+import { connect } from 'react-redux';
 import { Container, Grid, Header, Button } from 'semantic-ui-react';
+import { getRSSLinkByLang } from '../../helpers/utils';
+import { selectors as settings } from '../../redux/modules/settings';
 
-const Footer = ({ t }) => {
+const Footer = ({ t, language }) => {
   const year = new Date().getFullYear();
 
   return (
@@ -12,7 +15,7 @@ const Footer = ({ t }) => {
         <Grid padded inverted>
           <Grid.Row>
             <Grid.Column>
-              <Header inverted as="h5">
+              <Header inverted as="h5" floated="left">
                 {t('nav.top.header')}
                 <br />
                 <small className="text grey">
@@ -21,7 +24,11 @@ const Footer = ({ t }) => {
                   {t('nav.footer.rights')}
                 </small>
               </Header>
-              <Button icon="rss square" floated="left" color="orange" />
+              <Button
+                icon="rss square"
+                floated="right"
+                color="orange"
+                href={getRSSLinkByLang(language)} />
             </Grid.Column>
           </Grid.Row>
         </Grid>
@@ -34,4 +41,7 @@ Footer.propTypes = {
   t: PropTypes.func.isRequired,
 };
 
-export default withNamespaces()(Footer);
+export default connect(state => ({
+  language: settings.getLanguage(state.settings)
+}))(withNamespaces()(Footer));
+
