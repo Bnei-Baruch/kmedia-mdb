a37817e87c26a8eb23de84d2d0d947a739026315
guya <guy.atia@gmail.com>
2019-07-30 05:03:48 +0300

    Ar 624 this must be in context (#413)
    * DeviceInfoContext
    
    * fix ButtonDayPicker console errors
    
    * Update SearchBar.js
    
    * Update SearchBar.js
    
    * Fix search bar
    
    * Update ButtonDayPicker.js

--- src/components/Sections/Home/SearchBar.js
+++ src/components/Sections/Home/SearchBar.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { withNamespaces } from 'react-i18next';
+import {withNamespaces} from 'react-i18next';
 import {Button, Grid, Header, Input, Icon} from 'semantic-ui-react';
-
-import { selectors as device } from '../../../redux/modules/device';
 import * as shapes from '../../shapes';
-import { mapState as obMS, OmniBox, wrap } from '../../Search/OmniBox';
+import {mapState as obMS, OmniBox, wrap} from '../../Search/OmniBox';
 import ButtonDayPicker from "../../Filters/components/Date/ButtonDayPicker";
-import { BrowserRouter } from 'react-router';
 import moment from 'moment';
+import {DeviceInfoContext} from "../../../helpers/app-contexts";
 
 class MyOmniBox extends OmniBox {
-  static propTypes = {
-    deviceInfo: shapes.UserAgentParserResults.isRequired,
+  static contextType = DeviceInfoContext;
+  static propTypes   = {
     language: PropTypes.string.isRequired,
   };
 
@@ -25,41 +23,38 @@ class MyOmniBox extends OmniBox {
     window.location.href = '/' + this.props.language + '/simple-mode?date=' + moment(value).format('YYYY-MM-DD');
   };
 
-  isMobileDevice = () => this.props.deviceInfo.device && this.props.deviceInfo.device.type === 'mobile';
-
-
   renderInput() {
-    const { t, deviceInfo, language } = this.props;
-    const isMobileDevice = this.isMobileDevice();
-    return (
-      <Input
-        autoFocus={deviceInfo.device.type === undefined}  // desktop only
-        onKeyDown={this.handleSearchKeyDown}
-        action={{  onClick: this.doSearchFromClickEvent }}
-        icon={null}
-        placeholder={`${t('buttons.search')}...`}
-        style={{ width: '100%' }}
-        type="text">
-        <input />
-        <Button type='submit' className="searchButton" onClick={this.doSearchFromClickEvent}>
-          <Icon name='search' size={isMobileDevice ? 'large' : null} />
-          {!isMobileDevice ? t('buttons.search').toUpperCase() : null}
-        </Button>
-        <ButtonDayPicker
-          label={t('filters.date-filter.presets.CUSTOM_DAY')}
-          language={language}
-          deviceInfo={deviceInfo}
-          onDayChange={this.handleFromInputChange}
-        />
-      </Input>
+    const {t, language}                = this.props;
+    const {deviceInfo, isMobileDevice} = this.context;
 
+    return (
+      <div>
+        <Input
+          autoFocus={deviceInfo.device.type===undefined}  // desktop only
+          onKeyDown={this.handleSearchKeyDown}
+          className={'right action'}
+          icon={null}
+          placeholder={`${t('buttons.search')}...`}
+          style={{width: '100%'}}
+          type="text">
+          <input/>
+          <Button type='submit' className="searchButton" onClick={this.doSearchFromClickEvent}>
+            <Icon name='search' size={isMobileDevice ? 'large':null}/>
+            {!isMobileDevice ? t('buttons.search').toUpperCase():null}
+          </Button>
+          <ButtonDayPicker
+            label={t('filters.date-filter.presets.CUSTOM_DAY')}
+            language={language}
+            onDayChange={this.handleFromInputChange}
+          />
+        </Input>
+      </div>
     );
   }
 }
 
 const MyWrappedOmniBox = wrap(withNamespaces()(MyOmniBox), state => ({
-  ...obMS(state),
-  deviceInfo: device.getDeviceInfo(state.device),
+  ...obMS(state)
 }));
 
 class SearchBar extends Component {
@@ -69,17 +64,17 @@ class SearchBar extends Component {
   };
 
   render() {
-    const { t, location } = this.props;
+    const {t, location} = this.props;
 
     return (
       <Grid centered>
         <Grid.Row>
           <Grid.Column computer={12} tablet={14} mobile={16}>
-            <Header as="h1" content={t('home.search')} className="homepage__title text white" />
+            <Header as="h1" content={t('home.search')} className="homepage__title text white"/>
           </Grid.Column>
           <Grid.Column computer={12} tablet={14} mobile={16}>
             <div className="homepage__search">
-              <MyWrappedOmniBox location={location} />
+              <MyWrappedOmniBox location={location}/>
             </div>
           </Grid.Column>
         </Grid.Row>
