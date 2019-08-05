efb78dd34b0919c7da3a56746801a5193a193071
davgur <davgur.ru@gmail.com>
2019-08-05 15:23:49 +0300

    move props to function

--- src/components/Search/SearchResultIntent.js
+++ src/components/Search/SearchResultIntent.js
@@ -184,8 +184,16 @@ class SearchResultIntent extends SearchResultBase {
     );
   };
 
+  getSwipeProps = () => {
+    const isRTL = isLanguageRtl(this.props.language);
+    return {
+      onSwipedLeft: isRTL ? this.onScrollRight : this.onScrollLeft,
+      onSwipedRight: isRTL ? this.onScrollLeft : this.onScrollRight
+    };
+  };
+
   render() {
-    const { t, queryResult, hit, rank, items, unitCounter, isMobileDevice, language }   = this.props;
+    const { t, queryResult, hit, rank, items, unitCounter, isMobileDevice }             = this.props;
     const { _index: index, _type: type, _source: { mdb_uid: mdbUid, name }, highlight } = hit;
 
     const { pageNo, pageSize }            = this.state;
@@ -243,9 +251,7 @@ class SearchResultIntent extends SearchResultBase {
           </Segment>
         </Segment.Group>
         <div className="clear" />
-        <Swipeable
-          onSwipedLeft={isLanguageRtl(language) ? this.onScrollRight : this.onScrollLeft}
-          onSwipedRight={isLanguageRtl(language) ? this.onScrollLeft : this.onScrollRight}>
+        <Swipeable {...this.getSwipeProps()} >
           <Card.Group className={`${isMobileDevice() ? 'margin-top-8' : null} search__cards`} itemsPerRow={3} stackable>
             {items.slice(pageNo * pageSize, (pageNo + 1) * pageSize).map(this.renderItem)}
             {pageSize < unitCounter ? this.renderScrollLeft() : null}
