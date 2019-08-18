833ba125a507a3d0da33fe963f75aead34e92c18
Gabi Wissotzky <wissog@gmail.com>
2019-08-18 05:27:36 +0300

    added Topics filter to Articles and made it functional

--- src/components/Filters/components/TopicsFilter.js
+++ src/components/Filters/components/TopicsFilter.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { withNamespaces } from 'react-i18next';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { TOPICS_FOR_DISPLAY } from '../../../helpers/consts';
 import { isEmpty } from '../../../helpers/utils';
@@ -9,74 +8,42 @@ import { selectors } from '../../../redux/modules/tags';
 import { selectors as stats } from '../../../redux/modules/stats';
 import HierarchicalFilter from './HierarchicalFilter';
 
-class TagsFilter extends Component {
-  static propTypes = {
-    roots: PropTypes.arrayOf(PropTypes.shape({})),
-    getTagById: PropTypes.func.isRequired,
-    cuStats: PropTypes.objectOf(PropTypes.number),
-    t: PropTypes.func.isRequired,
-  };
 
-  static defaultProps = {
-    roots: [],
-    cuStats: null,
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = { tree: this.getTree(this.props) };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { roots, getTagById, cuStats } = this.props;
-
-    if ((roots !== nextProps.roots
-      && getTagById !== nextProps.getTagById)
-      || cuStats !== nextProps.cuStats) {
-      this.setState({ tree: this.getTree(nextProps) });
+const getTree = (roots, getTagById, cuStats, t) => (
+  [
+    {
+      value: 'root',
+      text: t('filters.topics-filter.all'),
+      children: roots
+        ? roots
+          .filter(x => TOPICS_FOR_DISPLAY.indexOf(x) !== -1)
+          .map(x => buildNode(x, getTagById, cuStats))
+        : null,
     }
-  }
+  ]);
 
-  getTree = (props) => {
-    const { roots, getTagById, cuStats, t } = props;
-    return [
-      {
-        value: 'root',
-        text: t('filters.topics-filter.all'),
-        children: roots
-          ? roots
-            .filter(x => TOPICS_FOR_DISPLAY.indexOf(x) !== -1)
-            .map(x => this.buildNode(x, getTagById, cuStats))
-          : null,
-      }
-    ];
+const buildNode = (id, getTagById, cuStats) => {
+  const { label, children } = getTagById(id);
+  return {
+    value: id,
+    text: label,
+    count: cuStats ? cuStats[id] : null,
+    children: children ? children.map(x => buildNode(x, getTagById, cuStats)) : null,
   };
+};
 
-  buildNode = (id, getTagById, cuStats) => {
-    const { label, children } = getTagById(id);
-    return {
-      value: id,
-      text: label,
-      count: cuStats ? cuStats[id] : null,
-      children: children ? children.map(x => this.buildNode(x, getTagById, cuStats)) : null,
-    };
-  };
+const TagsFilter = (props) => {
+  const roots  = useSelector(state => selectors.getRoots(state.tags));
+  const getTagById = useSelector(state => selectors.getTagById(state.tags));
 
-  render() {
-    const { tree } = this.state;
-    return <HierarchicalFilter name="topics-filter" tree={tree} {...this.props} />;
-  }
+  let cuStats = useSelector(state => stats.getCUStats(state.stats, props.namespace)) || { data: { tags: {} } };
+  cuStats     = isEmpty(cuStats) || isEmpty(cuStats.data) ? null : cuStats.data.tags;
+
+  const { t } = props;
+
+  const tree = getTree(roots, getTagById, cuStats, t); 
+
+  return <HierarchicalFilter name="topics-filter" tree={tree} {...props} />;
 }
 
-export default connect(
-  (state, ownProps) => {
-    let cuStats = stats.getCUStats(state.stats, ownProps.namespace) || { data: { tags: {} } };
-    cuStats     = isEmpty(cuStats) || isEmpty(cuStats.data) ? null : cuStats.data.tags;
-
-    return {
-      roots: selectors.getRoots(state.tags),
-      getTagById: selectors.getTagById(state.tags),
-      cuStats,
-    };
-  }
-)(withNamespaces()(TagsFilter));
+export default withNamespaces()(TagsFilter);
