d423a0cc5862ed4be93b62e69297d64bcd2f8742
Gregory Shilin <gshilin@gmail.com>
2019-10-03 16:03:57 +0300

    Rabash (#463)

--- src/redux/modules/sources.js
+++ src/redux/modules/sources.js
@@ -41,7 +41,7 @@ const initialState = {
   sortBy: 'AZ',
 };
 
-const NotToSort   = [BS_SHAMATI, BS_IGROT, BS_TAAS, RB_IGROT, MR_TORA, RH_ZOHAR, BS_ETZ_HAIM];
+const NotToSort   = [BS_SHAMATI, BS_IGROT, BS_TAAS, RB_IGROT, MR_TORA, RH_ZOHAR, BS_ETZ_HAIM, 'grRABASH'];
 const NotToFilter = [BS_TAAS];
 
 const sortTree = (root) => {
