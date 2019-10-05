a5219992be32ccd004be7e10ddb4d31af2bf8cd4
Gabi Wissotzky <wissog@gmail.com>
2019-10-05 12:07:13 +0300

    BlogPostContainer

--- src/components/Sections/Publications/tabs/Blog/Post/Container.js
+++ src/components/Sections/Publications/tabs/Blog/Post/Container.js
@@ -1,7 +1,5 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import { actions, selectors } from '../../../../../../redux/modules/publications';
@@ -9,75 +7,45 @@ import { selectors as settings } from '../../../../../../redux/modules/settings'
 import * as shapes from '../../../../../shapes';
 import Page from './Page';
 
-export class BlogPostContainer extends Component {
-  static propTypes = {
-    match: shapes.RouterMatch.isRequired,
-    post: shapes.BlogPost,
-    wip: shapes.WIP,
-    err: shapes.Error,
-    language: PropTypes.string.isRequired,
-    fetchPost: PropTypes.func.isRequired,
-  };
+const BlogPostContainer = ({ match }) => {
+  const wip = useSelector(state => selectors.getBlogWipPost(state.publications));
+  const err = useSelector(state => selectors.getBlogErrorPost(state.publications));
+  const language = useSelector(state => settings.getLanguage(state.settings));
+ 
+  const { blog, id } = match.params;
+  const post = useSelector(state => selectors.getBlogPost(state.publications, blog, id));
 
-  static defaultProps = {
-    post: null,
-    wip: false,
-    err: null,
-  };
+  const dispatch = useDispatch();
 
-  componentDidMount() {
-    this.askForDataIfNeeded(this.props);
+  useEffect(
+    () => {
+      dispatch(actions.fetchBlogPost(id, blog));
+    },
+    [id, blog, dispatch]
+  );
+
+  if (wip || err) {
+    return;
+  }
+  
+  if (post
+    && post.blog === blog
+    && `${post.wp_id}` === id) {
+    return;
   }
 
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    this.askForDataIfNeeded(nextProps);
-  }
-
-  askForDataIfNeeded = (props) => {
-    const { match, post, wip, err, fetchPost } = props;
-
-    // We fetch stuff if we don't have it already
-    // and a request for it is not in progress or ended with an error.
-    if (wip || err) {
-      return;
-    }
-
-    const { blog, id } = match.params;
-    if (
-      post
-      && post.blog === blog
-      && `${post.wp_id}` === id) {
-      return;
-    }
-
-    fetchPost(blog, id);
-  };
-
-  render() {
-    const { language, post, wip, err } = this.props;
-    return (
-      <Page
-        post={wip || err ? null : post}
-        language={language}
-        wip={wip}
-        err={err}
-      />
-    );
-  }
+  return (
+    <Page
+      post={wip || err ? null : post}
+      language={language}
+      wip={wip}
+      err={err}
+    />
+  );
 }
 
-const mapState = (state, ownProps) => {
-  const { blog, id } = ownProps.match.params;
-  return {
-    post: selectors.getBlogPost(state.publications, blog, id),
-    wip: selectors.getBlogWipPost(state.publications),
-    err: selectors.getBlogErrorPost(state.publications),
-    language: settings.getLanguage(state.settings),
-  };
+BlogPostContainer.propTypes = {
+  match: shapes.RouterMatch.isRequired,
 };
 
-const mapDispatch = dispatch => bindActionCreators({
-  fetchPost: actions.fetchBlogPost,
-}, dispatch);
-
-export default withRouter(connect(mapState, mapDispatch)(BlogPostContainer));
+export default withRouter(BlogPostContainer);
