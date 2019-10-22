2bd96cbb6c46a8189fcad1648312481cf54d96b1
Gabi Wissotzky <wissog@gmail.com>
2019-10-22 05:14:34 +0300

    upgraded LibraryContentContainer

--- src/components/Sections/Library/LibraryContentContainer.js
+++ src/components/Sections/Library/LibraryContentContainer.js
@@ -1,32 +1,15 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { assetUrl } from '../../../helpers/Api';
 import { isEmpty } from '../../../helpers/utils';
 import { selectSuitableLanguage } from '../../../helpers/language';
-import { getQuery, updateQuery } from '../../../helpers/url';
+import { updateQuery } from '../../../helpers/url';
 import { actions, selectors } from '../../../redux/modules/assets';
 import * as shapes from '../../shapes';
 import Library from './Library';
 import PDF from '../../shared/PDF/PDF';
-
-const fetchContent = (source, data, fetchAsset) => {
-  // In case of TAS we prefer PDF, otherwise HTML
-  if (data.pdf && PDF.isTaas(source)) {
-    // pdf.js fetch it on his own (smarter than us), we fetch it for nothing.
-    return;
-  }
-
-  let id = source;
-  if (/^gr-/.test(id)) { // Rabash Group Articles
-    const result = /^gr-(.+)/.exec(id);
-    id           = result[1];
-  }
-
-  fetchAsset(`sources/${id}/${data.html}`);
-};
 
 const getFullUrl = (pdf, data, language, source) => {
   if (pdf) {
@@ -46,131 +29,117 @@ const getFullUrl = (pdf, data, language, source) => {
   return assetUrl(`sources/${id}/${data[language].docx}`);
 };
 
-class LibraryContentContainer extends Component {
-  static propTypes = {
-    source: PropTypes.string,
-    index: shapes.DataWipErr,
-    content: shapes.DataWipErr.isRequired,
-    fetchAsset: PropTypes.func.isRequired,
-    uiLanguage: PropTypes.string.isRequired,
-    contentLanguage: PropTypes.string.isRequired,
-    langSelectorMount: PropTypes.instanceOf(PropTypes.element),
-    history: shapes.History.isRequired,
-    downloadAllowed: PropTypes.bool.isRequired,
-  };
 
-  static defaultProps = {
-    source: null,
-    index: {},
-    langSelectorMount: null,
-  };
+const getTaasPdf = (langData, source) => {
+  let pdfFile;
 
-  state = {
-    languages: [],
-    language: null,
-  };
-
-  componentDidMount() {
-    this.setStateFromProps(this.props);
+  if (langData && langData.pdf) {
+    pdfFile = `${source}/${langData.pdf}`;
   }
 
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    const { index, uiLanguage, contentLanguage } = this.props;
-    let useStateLanguages                        = false;
-    if (nextProps.index === index) {
-      if (nextProps.uiLanguage !== uiLanguage || nextProps.contentLanguage !== contentLanguage) {
-        // UI or Content language was changed
-        useStateLanguages = true;
-      } else {
-        return false;
+  return pdfFile;
+};
+
+const LibraryContentContainer = (props) => {
+  const { index: { data } = { index: { data: null } }, 
+    source = null, 
+    uiLanguage, 
+    contentLanguage, 
+    history,
+    langSelectorMount = null, 
+    downloadAllowed } = props;
+
+  const [language, setLanguage] = useState(null);
+  const [languages, setLanguages] = useState([]);
+
+  useEffect(() => {
+    if (data){
+      const languages =[...Object.keys(data)];
+      setLanguages(languages);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    const newLanguage = selectSuitableLanguage(contentLanguage, uiLanguage, languages);
+    if (newLanguage){
+      setLanguage(newLanguage);
+    }
+  }, [contentLanguage, uiLanguage, languages]);
+
+  
+  const dispatch = useDispatch();
+
+  const fetchContent = useCallback(() => {
+    if (data && language){
+      const lData = data[language];
+
+      // In case of TAS we prefer PDF, otherwise HTML
+      if (lData && lData.pdf && PDF.isTaas(source)) {
+      // pdf.js fetch it on his own (smarter than us), we fetch it for nothing.
+        return;
       }
-    }
-    return this.setStateFromProps(nextProps, useStateLanguages);
-  }
-
-  getTaasPdf = () => {
-    const { index, source, } = this.props;
-    const { language }       = this.state;
-
-    const isTaas     = PDF.isTaas(source);
-    const startsFrom = PDF.startsFrom(source);
-    let pdfFile;
-
-    if (isTaas && index && index.data) {
-      const data = index.data[language];
-      if (data && data.pdf) {
-        pdfFile = `${source}/${data.pdf}`;
+  
+      let id = source;
+      if (/^gr-/.test(id)) { // Rabash Group Articles
+        const result = /^gr-(.+)/.exec(id);
+        id           = result[1];
       }
+  
+      dispatch(actions.fetchAsset(`sources/${id}/${lData.html}`));
     }
+  }, [data, language, source, dispatch]);
 
-    return { isTaas, startsFrom, pdfFile };
-  };
-
-  setStateFromProps = (nextProps, useStateLanguages = false) => {
-    const { index: { data } = { index: { data: null } }, source, uiLanguage, contentLanguage, history } = nextProps;
-
-    if (!data) {
-      return { languages: [], language: null };
-    }
-
-    let { languages } = this.state;
-    if (!useStateLanguages && data) {
-      languages = [...Object.keys(data)];
-    }
-    
-    const newLanguage      = selectSuitableLanguage(contentLanguage, uiLanguage, languages);
-
-    if (!newLanguage) {
-      return false;
-    }
-
-    this.setState({ languages, language: newLanguage });
-
+  useEffect(() => {
     if (!isEmpty(source)) {
-      const { fetchAsset } = this.props;
-      fetchContent(source, data[newLanguage], fetchAsset);
+      fetchContent();
     }
+  }, [source, fetchContent]);
 
-    return true;
-  };
-
-  handleLanguageChanged = (e, language) => {
-    const { index: { data }, source, fetchAsset, history } = this.props;
+  // this handler passed to child component to handle language change from there
+  const handleLanguageChanged = (e, language) => {
     updateQuery(history, query => ({
       ...query,
       language,
     }));
-    this.setState({ language });
-    fetchContent(source, data[language], fetchAsset);
+
+    setLanguage(language);
   };
 
-  render() {
-    const { content, index, langSelectorMount, source, downloadAllowed } = this.props;
-    const { languages, language }                                        = this.state;
-    const { isTaas, startsFrom, pdfFile }                                = this.getTaasPdf();
+  const content = useSelector(state => selectors.getAsset(state.assets));
 
-    return (
-      <Library
-        isTaas={isTaas}
-        pdfFile={pdfFile}
-        fullUrlPath={getFullUrl(pdfFile, index.data, language, source)}
-        startsFrom={startsFrom}
-        content={index && index.data ? content : {}}
-        language={language}
-        languages={languages}
-        handleLanguageChanged={this.handleLanguageChanged}
-        langSelectorMount={langSelectorMount}
-        downloadAllowed={downloadAllowed}
-      />
-    );
+  const startsFrom = PDF.startsFrom(source);
+  const isTaas     = PDF.isTaas(source);
+  
+  let pdfFile;
+  if (data && isTaas){
+    const langData = data[language];
+    pdfFile = getTaasPdf(langData, source);
   }
+
+  return (
+    <Library
+      isTaas={isTaas}
+      pdfFile={pdfFile}
+      fullUrlPath={getFullUrl(pdfFile, data, language, source)}
+      startsFrom={startsFrom}
+      content={data ? content : {}}
+      language={language}
+      languages={languages}
+      handleLanguageChanged={handleLanguageChanged}
+      langSelectorMount={langSelectorMount}
+      downloadAllowed={downloadAllowed}
+    />
+  );
 }
 
-export default connect(
-  state => ({
-    content: selectors.getAsset(state.assets),
-  }),
-  dispatch => bindActionCreators({
-    fetchAsset: actions.fetchAsset,
-  }, dispatch)
-)(LibraryContentContainer);
+LibraryContentContainer.propTypes = {
+  source: PropTypes.string,
+  index: shapes.DataWipErr,
+  uiLanguage: PropTypes.string.isRequired,
+  contentLanguage: PropTypes.string.isRequired,
+  langSelectorMount: PropTypes.instanceOf(PropTypes.element),
+  history: shapes.History.isRequired,
+  downloadAllowed: PropTypes.bool.isRequired,
+};
+
+export default LibraryContentContainer;
