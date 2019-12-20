5396acb9fd7d62b6861185f36dd18d2a5e3e3cb1
Gabi Wissotzky <wissog@gmail.com>
2019-12-20 05:29:20 +0200

    changed to typo_suggest

--- src/components/Search/DidYouMean.js
+++ src/components/Search/DidYouMean.js
@@ -3,26 +3,26 @@ import PropTypes from 'prop-types';
 import { withNamespaces } from 'react-i18next';
 import { Container } from 'semantic-ui-react';
 
-const DidYouMean = ({suggestion, t}) => {
-  if (!suggestion){
+const DidYouMean = ({typo_suggest, t}) => {
+  if (!typo_suggest){
     return null;
   }
 
   const didYouMeanStr = t('search.didYouMean');
   const { origin, pathname } = window.location;
 
-  const to = `${origin}${pathname}?q=${suggestion}`;  
+  const to = `${origin}${pathname}?q=${typo_suggest}`;  
 
   return (
     <Container className="search__didyoumean">
       {didYouMeanStr}
-      <a href={to} className="search__link">{suggestion}</a>
+      <a href={to} className="search__link">{typo_suggest}</a>
     </Container>
   );
 };
 
 DidYouMean.propTypes = {
-  suggestion: PropTypes.string.isRequired,
+  typo_suggest: PropTypes.string.isRequired,
   t: PropTypes.func.isRequired,
 };
 
