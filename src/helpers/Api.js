f9c39314e29fa0a2ce799d4e85b348392aabe0f7
DELL-LT\guya <guy.atia@gmail.com>
2020-01-03 04:55:20 +0200

    AR-678_server_error_when_you_change_website_language_while_hebrew_blog_post_opened

--- src/helpers/Api.js
+++ src/helpers/Api.js
@@ -43,7 +43,7 @@ export class Requests {
 
         return `${key}=${Requests.encode(value)}`;
       })
-      //can happen if parameter value is empty array 
+      //can happen if parameter value is empty array
       .filter(p => p !== '')
       .join('&')}`
   );
@@ -86,7 +86,7 @@ class Api {
     Requests.get(`posts?${Requests.makeParams({ page_no, page_size, ...rest })}`)
   );
 
-  static post = (blog, id) => Requests.get(`posts/${id}/${blog}`);
+  static post = (blog, id) => Requests.get(`posts/${blog}/${id}`);
 
   static tagDashboard = ({ id, language }) => Requests.get(`tags/${id}/dashboard?${Requests.makeParams({ language })}`);
 
