25512394fbd76c97c4748aaf424e9f2468bf2202
Gabi Wissotzky <wissog@gmail.com>
2020-01-18 15:34:25 +0200

    unified MainPages to use MainTabPage

--- src/components/Sections/Publications/MainPage.js
+++ src/components/Sections/Publications/MainPage.js
@@ -1,14 +1,7 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { withNamespaces } from 'react-i18next';
-import { Menu } from 'semantic-ui-react';
-import { useLocation, useParams } from 'react-router';
+import React from 'react';
 
 import { actions } from '../../../redux/modules/publications';
-import { actions as filterActions } from '../../../redux/modules/filters';
-import NavLink from '../../Language/MultiLanguageNavLink';
-import SectionHeader from '../../shared/SectionHeader';
+import MainTabPage from '../../shared/MainTabPage';
 import Articles from './tabs/Articles/List';
 import Blog from './tabs/Blog/Container';
 import Twitter from './tabs/Twitter/Container';
@@ -21,7 +14,7 @@ export const tabs = [
   'audio-blog'
 ];
 
-const getContent = (active) => {
+const content = (active) => {
   switch (active) {
   case 'articles':
     return <Articles />;
@@ -36,49 +29,12 @@ const getContent = (active) => {
   }
 }
 
-const MainPage = ({ t }) => {
-  
-  const params   = useParams();
-  const location = useLocation();
-  const dispatch = useDispatch();
+const MainPage = () => (
+  <MainTabPage 
+    tabs={tabs} 
+    content={content} 
+    setTab={actions.setTab} 
+    section="publications" />
+);
 
-  const currentTab = params.tab || tabs[0];
-
-  // clear filters if location search parameter is changed by Menu click
-  useEffect(() => {
-    if (!location.search) {
-      const resetNamespace = tab => dispatch(filterActions.resetNamespace(tab));
-      resetNamespace(`publications-${currentTab}`);
-    }
-  }, [dispatch, location.search, currentTab]); 
-  
-  useEffect(() => {
-    const setTab = tab => dispatch(actions.setTab(tab));
-    setTab(currentTab);
-  }, [dispatch, currentTab]); 
-
-  const submenuItems = tabs.map(x => (
-    <Menu.Item
-      key={x}
-      name={x}
-      as={NavLink}
-      to={`/publications/${x}`}
-      active={currentTab === x}
-    >
-      {t(`publications.tabs.${x}`)}
-    </Menu.Item>
-  ));
-
-  return (
-    <div>
-      <SectionHeader section="publications" submenuItems={submenuItems} />
-      {getContent(currentTab)}
-    </div>
-  );
-}
-
-MainPage.propTypes = {
-  t: PropTypes.func.isRequired,
-};
-
-export default withNamespaces()(MainPage);
+export default MainPage;
