7bbbb5c4c8fd13f5fc64c3508bcebceac84cc1aa
Gabi Wissotzky <wissog@gmail.com>
2020-02-04 07:09:13 +0200

    added renderUnitHelper

--- src/components/Sections/Events/tabs/UnitList/Container.js
+++ src/components/Sections/Events/tabs/UnitList/Container.js
@@ -2,44 +2,25 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { List, Table } from 'semantic-ui-react';
 
-import { CT_FRIENDS_GATHERING, CT_MEAL, NO_NAME } from '../../../../../helpers/consts';
-import { canonicalLink } from '../../../../../helpers/links';
-import { CollectionsBreakdown } from '../../../../../helpers/mdb';
+import { CT_FRIENDS_GATHERING, CT_MEAL } from '../../../../../helpers/consts';
 import { ellipsize } from '../../../../../helpers/strings';
+import * as renderUnitHelper from '../../../../../helpers/renderUnitHelper';
 import UnitList from '../../../../Pages/UnitList/Container';
-import Link from '../../../../Language/MultiLanguageLink';
-import UnitLogo from '../../../../shared/Logo/UnitLogo';
 
 const renderUnit = (unit, t) => {
-  const breakdown = new CollectionsBreakdown(Object.values(unit.collections || {}));
+  const breakdown = renderUnitHelper.getUnitCollectionsBreakdown(unit); 
   const events    = breakdown.getEvents();
 
-  const map          = x => (
-    <List.Item key={x.id} as={Link} to={canonicalLink(x)}>
-      {x.name || NO_NAME}
-    </List.Item>
-  );
-  const relatedItems = events.map(map);
-
-  let filmDate = '';
-  if (unit.film_date) {
-    filmDate = t('values.date', { date: unit.film_date });
-  }
-
-  const link = canonicalLink(unit);
+  const relatedItems = events.map(renderUnitHelper.renderUnitNameAsListItem);
 
   return (
     <Table.Row key={unit.id} verticalAlign="top">
       <Table.Cell collapsing width={1}>
-        <Link to={link}>
-          <UnitLogo className="index__thumbnail" unitId={unit.id} fallbackImg='events' />
-        </Link>
+        { renderUnitHelper.renderUnitLogo(unit, 'events')}
       </Table.Cell>
       <Table.Cell>
-        <span className="index__date">{filmDate}</span>
-        <Link className="index__title" to={link}>
-          {unit.name || NO_NAME}
-        </Link>
+        { renderUnitHelper.renderFilmDate(unit, t) }
+        { renderUnitHelper.renderUnitNameLink(unit) }
         {
           unit.description
             ? (
---
+++ src/helpers/renderUnitHelper.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { List } from 'semantic-ui-react';
+
+import { NO_NAME } from './consts';
+import { CollectionsBreakdown } from './mdb';
+import { canonicalLink } from './links';
+import { ellipsize } from './strings';
+import Link from '../components/Language/MultiLanguageLink';
+import UnitLogo from '../components/shared/Logo/UnitLogo';
+
+export const commonRenderUnitForClips = (unit, t) => {
+  const breakdown = getUnitCollectionsBreakdown(unit);
+  const clips     = breakdown.getClips();
+
+  const relatedItems = clips
+    .map(x => renderUnitNameAsListItem(x))
+    .concat(breakdown.getAllButClips()
+      .map(x => renderUnitNameAsListItem(x)));
+
+  return {
+    link: canonicalLink(unit),
+    filmDate: getFilmDate(unit, t),
+    clips,
+    relatedItems
+  };
+};
+
+export const getUnitCollectionsBreakdown = unit =>
+  new CollectionsBreakdown(Object.values(unit.collections || {}));
+
+export const getFilmDate = (unit, t) => 
+  unit.film_date ? t('values.date', { date: unit.film_date }) : ''
+
+export const renderFilmDate = filmDate =>
+  <span className="index__date">{filmDate}</span>
+
+export const renderUnitFilmDate = (unit, t) => 
+  renderFilmDate(getFilmDate(unit, t));
+
+export const renderUnitNameAsListItem = unit => 
+  <List.Item key={unit.id} as={Link} to={canonicalLink(unit)}>
+    {unit.name || NO_NAME}
+  </List.Item>
+
+export const renderUnitNameLink = unit =>
+  <Link className="index__title" to={canonicalLink(unit)}>
+    {unit.name || NO_NAME}
+  </Link>
+
+export const renderUnitLogo = (unit, fallbackImg) => 
+  <Link to={canonicalLink(unit)}>
+    <UnitLogo className="index__thumbnail" unitId={unit.id} fallbackImg={fallbackImg} />
+  </Link>
+
+export const renderUnitCollectionLogo = (unit, fallbackImg, collectionId) =>
+  <Link to={canonicalLink(unit)}>
+    <UnitLogo
+      className="index__thumbnail"
+      unitId={unit.id}
+      collectionId={collectionId}
+      fallbackImg={fallbackImg}
+    />
+  </Link>
+
+export const renderUnitDescription = unit =>
+  unit.description
+    ? (
+      <div className="index__description mobile-hidden">
+        {ellipsize(unit.description)}
+      </div>
+    )
+    : null
+
+
+
