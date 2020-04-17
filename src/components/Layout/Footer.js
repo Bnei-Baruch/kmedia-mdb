e6a6c7dce79cccf371a5119979092d99d6298650
Gabi Wissotzky <wissog@gmail.com>
2020-04-17 07:12:02 +0300

    simplified Footer by useSelector

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { withNamespaces } from 'react-i18next';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Container, Grid, Header, Button } from 'semantic-ui-react';
 import { getRSSLinkByLang } from '../../helpers/utils';
 import { selectors as settings } from '../../redux/modules/settings';
 import { DeviceInfoContext } from "../../helpers/app-contexts";
 
-const Footer = ({ t, language }) => {
+const Footer = ({ t }) => {
+  const language = useSelector(state => settings.getLanguage(state.settings));
   const year = new Date().getFullYear();
   const { isMobileDevice } = useContext(DeviceInfoContext);
+
   return (
     <div className="layout__footer">
       <Container>
@@ -44,10 +46,7 @@ const Footer = ({ t, language }) => {
 
 Footer.propTypes = {
   t: PropTypes.func.isRequired,
-  language: PropTypes.string.isRequired
 };
 
-export default connect(state => ({
-  language: settings.getLanguage(state.settings),
-}))(withNamespaces()(Footer));
+export default withNamespaces()(Footer);
 
