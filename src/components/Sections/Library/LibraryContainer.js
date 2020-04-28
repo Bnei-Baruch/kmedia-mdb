644286eb209def56939f51dc506134c1903597f5
Gabi Wissotzky <wissog@gmail.com>
2020-04-28 05:30:37 +0300

    fix mobile scrolling bug by turning sticky active off when toc is not active

--- src/components/Sections/Library/LibraryContainer.js
+++ src/components/Sections/Library/LibraryContainer.js
@@ -432,6 +432,7 @@ class LibraryContainer extends Component {
 
     const isRtl    = isLanguageRtl(language);
     const position = isRtl ? 'left' : 'right';
+    const active = !this.context.isMobileDevice || tocIsActive;
 
     return (
       <div
@@ -499,6 +500,7 @@ class LibraryContainer extends Component {
                   contextRef={this.contextRef}
                   getSourceById={getSourceById}
                   apply={push}
+                  active={active}
                 />
               </Grid.Column>
               <Grid.Column
