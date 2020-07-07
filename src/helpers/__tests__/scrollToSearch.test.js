11d22c1b36598376c273035306ca8a8d2f7c17dc
davgur <gur28davaravut>
2020-07-07 13:42:40 +0300

    wrap to div

--- src/helpers/__tests__/scrollToSearch.test.js
+++ src/helpers/__tests__/scrollToSearch.test.js
@@ -1,37 +1,53 @@
-import { prepareScrollToSearch } from '../utils';
+import { prepareScrollToSearch, wrapSeekingPlace } from '../utils';
 import {
   data,
+  tagsPosition_data,
   expected,
   data_withTagInEnd,
   expected_withTagInEnd,
   data_withTagInMiddle,
   expected_withTagInMiddle,
   data_withP,
-  expected_withP
+  expected_withP, expected_wrapped_after, expected_wrapped_before
 } from './scrollToSearchData';
 
 describe('Scroll to search, prepareScrollToSearch:', () => {
-  const defSearchStart = 'Before I clarify';
-  const defSearchEnd   = 'readers seem to consider';
+  const srchstart = 'Before I clarify';
+  const srchend   = 'readers seem to consider';
 
   test('simple test', () => {
-    const result = prepareScrollToSearch(data, defSearchStart, defSearchEnd);
+    const result = prepareScrollToSearch(data, { srchstart, srchend });
     expect(result).toEqual(expected);
   });
 
   test('test data with html in the end', () => {
-    const result = prepareScrollToSearch(data_withTagInEnd, defSearchStart, defSearchEnd);
+    const result = prepareScrollToSearch(data_withTagInEnd, { srchstart, srchend });
     expect(result).toEqual(expected_withTagInEnd);
   });
 
   test('test data with html in the middle', () => {
-    const result = prepareScrollToSearch(data_withTagInMiddle, defSearchStart, defSearchEnd);
+    const result = prepareScrollToSearch(data_withTagInMiddle, { srchstart, srchend });
     expect(result).toEqual(expected_withTagInMiddle);
   });
 
   test('test data separated to p', () => {
     const searchStart = 'Body and Soul';
-    const result      = prepareScrollToSearch(data, searchStart, defSearchEnd);
+    const result      = prepareScrollToSearch(data, { srchstart: searchStart, srchend });
     expect(result).toEqual(expected_withP);
   });
+});
+
+describe('Scroll to search, wrapSeekingPlace:', () => {
+
+  const from = data.indexOf('and Soul');
+  const end  = 'it is important for me';
+  const to   = data.indexOf(end) + end.length;
+
+  test('simple test', () => {
+    const { before, after } = wrapSeekingPlace(data, tagsPosition_data, from, to);
+    expect(before).toEqual(expected_wrapped_before);
+    expect(after).toEqual(expected_wrapped_after);
+
+  });
+
 });
