814a24fba1b9b5dedf20cd3c8587ec8de0b3766f
Gregory Shilin <gshilin@gmail.com>
2020-07-21 09:49:50 +0300

    Ar 714 (#591)
    * update packages
    
    * add Rss feeds for collections

--- src/components/Pages/Collection/ShareForm.js
+++ src/components/Pages/Collection/ShareForm.js
@@ -23,7 +23,11 @@ const RenderShare = ({ collection, callback, t }) => {
   const { name, description, content_type } = collection;
 
   const contentType = t(`constants.content-types.${content_type}`);
-  const title       = `${contentType} "${name}": ${description}`;
+
+  let title         = `${contentType} "${name}"`;
+  if (description) {
+    title += `: ${description}`;
+  }
 
   const url      = window.location;
   const bsPixels = 46;
