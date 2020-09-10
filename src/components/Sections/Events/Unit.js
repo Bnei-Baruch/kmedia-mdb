94a07191f9904bd231cb915c7de361a6e99a06df
Gabi Wissotzky <wissog@gmail.com>
2020-09-10 06:27:36 +0300

    simplified events page implementation

--- src/components/Sections/Events/Unit.js
+++ src/components/Sections/Events/Unit.js
@@ -1,31 +1,3 @@
-import React from 'react';
-
 import { UnitContainer, wrap as wrapContainer } from '../../Pages/Unit/Container';
-import { UnitPage, wrap as wrapPage } from '../../Pages/Unit/Page';
-import Info from '../../Pages/Unit/widgets/Info/Info';
 
-class MyUnitPage extends UnitPage {
-  renderInfo() {
-    const { unit } = this.props;
-    return <Info unit={unit} section="" />;
-  }
-}
-
-const MyWrappedUnitPage = wrapPage(MyUnitPage);
-
-class MyUnitContainer extends UnitContainer {
-  render() {
-    const { language, unit, wip, err } = this.props;
-    return (
-      <MyWrappedUnitPage
-        section="events"
-        unit={wip || err ? null : unit}
-        language={language}
-        wip={wip}
-        err={err}
-      />
-    );
-  }
-}
-
-export default wrapContainer(MyUnitContainer);
+export default wrapContainer(UnitContainer);
