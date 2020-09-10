a6471113b8b8500ab1fa4785483aa01bb7c2be64
Gabi Wissotzky <wissog@gmail.com>
2020-09-10 05:37:28 +0300

    article unit fixes

--- src/components/Sections/Publications/tabs/Articles/Unit.js
+++ src/components/Sections/Publications/tabs/Articles/Unit.js
@@ -1,22 +1,54 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
+import PropTypes from 'prop-types';
 import { Container, Grid, Header } from 'semantic-ui-react';
 
 import Helmets from '../../../../shared/Helmets/index';
 import { UnitContainer, wrap as wrapContainer } from '../../../../Pages/Unit/Container';
-import { UnitPage, wrap as wrapPage } from '../../../../Pages/Unit/Page';
+import { wrap as wrapPage } from '../../../../Pages/Unit/Page';
 import TranscriptionContainer from '../../../../Pages/Unit/widgets/UnitMaterials/Transcription/TranscriptionContainer';
 import Share from "../../../Library/Share";
 import { isLanguageRtl } from "../../../../../helpers/i18n-utils";
 import MediaDownloads from '../../../../Pages/Unit/widgets/Downloads/MediaDownloads';
+import * as shapes from '../../../../shapes';
+import WipErr from '../../../../shared/WipErr/WipErr';
+import Recommended from '../../../../Pages/Unit/widgets/Recommended/Main/Recommended';
+import playerHelper from '../../../../../helpers/player';
 
-class MyUnitPage extends UnitPage {
+class MyUnitPage extends Component {
+
+  static propTypes = {
+    unit: shapes.ContentUnit,
+    wip: shapes.WIP,
+    err: shapes.Error,
+    section: PropTypes.string,
+    language: PropTypes.string.isRequired,
+    t: PropTypes.func.isRequired,
+    location: shapes.HistoryLocation,
+  };
+
+  static defaultProps = {
+    unit: null,
+    wip: false,
+    err: null,
+    section: '',
+    location: {}
+  };
+
+  constructor(props) {
+    super(props);
+    const { location } = props;
+    this.state         = { 
+      embed: playerHelper.getEmbedFromQuery(location), 
+    };
+  }
 
   // render article title instead of player
-  renderPlayer() {
+  renderHeader() {
     const { unit, t, language } = this.props;
     const isRtl = isLanguageRtl(language);
     const position = isRtl ? 'right' : 'left';
     const subText2 = t(`publications.header.subtext2`);
+
     return (
       <div className="section-header">
         <Container className="padded">
@@ -56,10 +88,6 @@ class MyUnitPage extends UnitPage {
     );
   }
 
-  renderInfo() {
-    return null;
-  }
-
   renderHelmet() {
     return (
       <Fragment>
@@ -70,11 +98,11 @@ class MyUnitPage extends UnitPage {
   }
 
   // article content
-  renderMaterials() {
+  renderArticle() {
     const { unit } = this.props;
 
     return (
-      <Grid>
+      <Grid vertically divided padded>
         <Grid.Row>
           <Grid.Column>	
             <TranscriptionContainer unit={unit} />
@@ -88,13 +116,65 @@ class MyUnitPage extends UnitPage {
       </Grid>
     );
   }
+
+  renderContent() {
+    const { unit } = this.props;
+    const { embed } = this.state;
+
+    return !embed ? (
+      <div className="unit-page">
+        {this.renderHelmet()}
+        <Container>
+          <Grid padded>
+            <Grid.Row>
+              <Grid.Column mobile={16} tablet={10} computer={10}>
+                <Grid.Row>
+                  {this.renderHeader()}
+                </Grid.Row>
+                <Grid.Row>
+                  <Grid.Column>
+                    {this.renderArticle()}
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid.Column>
+              <Grid.Column mobile={16} tablet={6} computer={6}>	
+                <Recommended unit={unit} />	
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </div>
+    ) : (
+      <div className="unit-page">
+        {this.renderHeader()}
+      </div>
+    );
+  }
+
+  render() {
+    const { unit } = this.props;
+
+    if (!unit) {
+      return null;
+    }
+
+    return this.renderContent();
+  }
 }
+
+// export const wrap = WrappedComponent => withNamespaces()(WrappedComponent);
 
 const MyWrappedUnitPage = wrapPage(MyUnitPage);
 
 class MyUnitContainer extends UnitContainer {
   render() {
-    const { language, unit, wip, err } = this.props;
+    const { language, unit, wip, err, t } = this.props;
+
+    const wipErr = WipErr({ wip, err, t });
+    if (wipErr) {
+      return wipErr;
+    }
+
     return (
       <MyWrappedUnitPage
         section="publications"
@@ -107,4 +187,4 @@ class MyUnitContainer extends UnitContainer {
   }
 }
 
-export default wrapContainer(MyUnitContainer);
+export default wrapContainer(wrapPage(MyUnitContainer));
