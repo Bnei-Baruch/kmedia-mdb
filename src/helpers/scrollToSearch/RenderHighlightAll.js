f82ad990c88b3a73b0feefffe32a40d65212aadd
davgur <davgur.ru@gmail.com>
2020-10-27 16:00:06 +0200

    fix bug with multi match from search results

--- src/helpers/scrollToSearch/RenderHighlightAll.js
+++ src/helpers/scrollToSearch/RenderHighlightAll.js
@@ -2,6 +2,29 @@ import { textToHtml, wrapSeekingPlace } from './helper';
 import { RenderBase } from './RenderBase';
 
 export class RenderHighlightAll extends RenderBase {
+  getMatches() {
+    const sMatch = this.buildMatch(this.start, this.dataCleanHtml);
+    const eMatch = this.buildMatch(this.end, this.dataCleanHtml);
+    return this.findClose(sMatch, eMatch);
+  };
+
+  findClose(sMatch, eMatch) {
+    if (sMatch.length === 0 || eMatch.length === 0)
+      return { start: null, end: null };
+    let start = null, end = null;
+    let diff  = 0;
+    for (const s of sMatch) {
+      for (const e of eMatch) {
+        const d = e.index - s.index;
+        if (d > diff) {
+          diff  = d;
+          start = s;
+          end   = e;
+        }
+      }
+    }
+    return { start, end };
+  }
 
   buildHtml() {
     if (!this.matchStart || !this.matchEnd) {
