3c7fbb5cf3a33dbe007a4ef0ca24dc671daeb503
bbfsdev <bbfsdev@gmail.com>
2020-11-17 02:02:06 +0000

    Chronicles client.

--- src/components/Pages/PlaylistCollection/Page.js
+++ src/components/Pages/PlaylistCollection/Page.js
@@ -18,11 +18,14 @@ import { DeviceInfoContext } from "../../../helpers/app-contexts";
 import { selectors as settings } from '../../../redux/modules/settings';
 import AVPlaylistPlayer from '../../AVPlayer/AVPlaylistPlayer';
 
+import { usePrevious } from '../../../helpers/utils';
+import { ClientChroniclesContext } from '../../../helpers/app-contexts';
 
 const PlaylistCollectionPage = ({ collection, nextLink = null, prevLink = null }) => {
   const location = useLocation();
   const history  = useHistory();
   const { isMobileDevice } = useContext(DeviceInfoContext);
+  const chronicles = useContext(ClientChroniclesContext);
 
   const uiLanguage = useSelector(state => settings.getLanguage(state.settings));
   const contentLanguage = useSelector(state => settings.getContentLanguage(state.settings));
@@ -32,12 +35,28 @@ const PlaylistCollectionPage = ({ collection, nextLink = null, prevLink = null }
   const [selected, setSelected] = useState(0);
   const [playlist, setPlaylist] = useState(null);
 
+  const prev = usePrevious({unit, collection});
+
   const handleSelectedChange = useCallback(nSelected => {
     if (nSelected !== selected){
       playerHelper.setActivePartInQuery(history, nSelected);
       setSelected(nSelected);
     }
   }, [history, selected]);
+
+  useEffect(() => {
+    if (prev?.unit?.id !== unit?.id) {
+      if (prev?.unit?.id) {
+        chronicles.append('collection-unit-unselected', {unit_uid: prev.unit.id});
+      }
+      if (unit?.id) {
+        chronicles.append('collection-unit-selected', {unit_uid: unit.id});
+      }
+    }
+    if (prev?.unit?.id && !unit?.id) {
+      chronicles.append('collection-unit-unselected', {unit_uid: unit.id});
+    }
+  }, [unit, prev?.unit]);
 
   const handleLanguageChange = useCallback((e, language) => {
     playerHelper.setLanguageInQuery(history, language);
--- src/helpers/app-contexts.js
+++ src/helpers/app-contexts.js
@@ -1,3 +1,5 @@
 import React from 'react';
+
 export const DeviceInfoContext = React.createContext({});
 export const SessionInfoContext = React.createContext({});
+export const ClientChroniclesContext = React.createContext({});
