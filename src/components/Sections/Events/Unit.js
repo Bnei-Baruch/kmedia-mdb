68bf1d82dc4ee8735e9936041d0e60ef0e7a61e7
Gabi Wissotzky <wissog@gmail.com>
2020-11-20 04:40:42 +0200

    removed unused files

--- src/components/Sections/Events/Unit.js
+++
@@ -1,4 +0,0 @@
-import { UnitContainer, wrap as wrapContainer } from '../../Pages/Unit/Container';
-import { wrap as wrapPage } from '../../Pages/Unit/Page';
-
-export default wrapContainer(wrapPage(UnitContainer));
