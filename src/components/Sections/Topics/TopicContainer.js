d8b3b3dbb6f28a024d07263212ecb718552d0682
Gabi Wissotzky <wissog@gmail.com>
2021-01-28 06:37:32 +0200

    Topics improvement for Series usage

--- src/components/Sections/Topics/TopicContainer.js
+++ src/components/Sections/Topics/TopicContainer.js
@@ -1,10 +1,9 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withNamespaces } from 'react-i18next';
 import produce from 'immer';
 import debounce from 'lodash/debounce';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Button, Container, Divider, Grid, Header, Input, List } from 'semantic-ui-react';
 
 import { actions as topicsActions, selectors as topicsSelectors } from '../../../redux/modules/tags';
@@ -24,121 +23,108 @@ import {
   UNIT_LESSONS_TYPE,
   UNIT_PROGRAMS_TYPE
 } from '../../../helpers/consts';
+import isEqual from 'react-fast-compare';
+
+
+const namespace = 'topics';
+const contentType    = [
+  ...UNIT_EVENTS_TYPE,
+  ...UNIT_EVENTS_TYPE,
+  ...UNIT_PROGRAMS_TYPE,
+  ...UNIT_LESSONS_TYPE,
+  ...COLLECTION_PUBLICATIONS_TYPE,
+  ...COLLECTION_EVENTS_TYPE,
+  ...COLLECTION_PROGRAMS_TYPE,
+  ...COLLECTION_LESSONS_TYPE,
+  CT_ARTICLE,
+  CT_PUBLICATION,
+];
+
+const hasChildren = node => Array.isArray(node.children) && node.children.length > 0;
+
+const sortRootsPosition = roots => {
+  const extra = roots.filter(node => !TOPICS_FOR_DISPLAY.includes(node));
+
+  return roots.length ? [...TOPICS_FOR_DISPLAY, ...extra] : roots;
+};
 
 /* root will be main title
   subroot will be subtitle
   the rest will be a tree - List of Lists */
+const TopicContainer = ({ t }) => {
+  const stats = useSelector(state => statsSelectors.getCUStats(state.stats, 'topics'), isEqual);
+  const statDataTags = isEmpty(stats) || isEmpty(stats.data) ? [] : stats.data.tags;
 
-class TopicContainer extends Component {
-  static propTypes = {
-    roots: PropTypes.arrayOf(PropTypes.string),
-    byId: PropTypes.object,
-    t: PropTypes.func.isRequired,
-    stats: PropTypes.objectOf(PropTypes.number),
-    fetchStats: PropTypes.func.isRequired
-  };
+  const roots = useSelector(state => topicsSelectors.getDisplayRoots(state.tags), isEqual) || [];
+  const byId = useSelector(state => topicsSelectors.getTags(state.tags), isEqual);
 
-  static defaultProps = {
-    roots: [],
-    stats: []
-  };
+  const [match, setMatch] = useState('');
+  const [expandedNodes, setExpandedNodes] = useState(new Set());
 
-  state = {
-    match: '',
-    expandedNodes: []
-  };
+  console.log('stats:', stats);
+  console.log('roots:', roots);
+  console.log('byID:', byId);
 
-  filteredById = {};
+  const dispatch = useDispatch();
 
-  handleFilterChange = debounce((e, data) => {
-    this.setState({ match: data.value });
+  useEffect(() => {
+    dispatch(topicsActions.fetchStats(namespace, contentType));
+  }, [dispatch]);
+
+  const handleFilterChange = debounce((e, data) => {
+    setMatch(data.value);
   }, 100);
 
-  componentDidMount() {
-    const { fetchStats } = this.props;
-    const namespace      = 'topics';
-    const contentType    = [
-      ...UNIT_EVENTS_TYPE,
-      ...UNIT_EVENTS_TYPE,
-      ...UNIT_PROGRAMS_TYPE,
-      ...UNIT_LESSONS_TYPE,
-      ...COLLECTION_PUBLICATIONS_TYPE,
-      ...COLLECTION_EVENTS_TYPE,
-      ...COLLECTION_PROGRAMS_TYPE,
-      ...COLLECTION_LESSONS_TYPE,
-      CT_ARTICLE,
-      CT_PUBLICATION,
-    ];
-
-    fetchStats(namespace, contentType);
-  }
-
-  // filter stuff
-  getRegExp = match => {
-    const escapedMatch = getEscapedRegExp(match);
-    return new RegExp(escapedMatch, 'i');
+  const handleFilterKeyDown = e => {
+    if (e.keyCode === 27) { // Esc
+      setMatch('');
+    }
   };
 
-  setVisibleState = byId => {
-    const { expandedNodes } = this.state;
+  const getAllVisibleById = () => {
     const visibleItemsCount = 3;
-    const list              = produce(byId || {}, draft => {
-      Object.keys(draft).forEach((key) => {
-        const { id, parent_id: parentId } = draft[key];
-        let visible                       = true;
-        if (parentId) {
-          visible = expandedNodes[parentId] || draft[parentId].children.indexOf(id) < visibleItemsCount;
-        }
+    const list = produce(byId || {}, draft => {
+      Object.keys(draft).forEach(key => {
+        const { id, parent_id } = draft[key];
+
+        // make node visible when its parent is in expandedNodes and its index less then visible items count
+        const visible = parent_id
+          ? expandedNodes.has(parent_id) || draft[parent_id].children.indexOf(id) < visibleItemsCount
+          : true
 
         draft[key].visible = visible;
       });
     });
 
     return list;
-  };
+  }
 
-  handleFilterKeyDown = (e) => {
-    if (e.keyCode === 27) { // Esc
-      this.handleFilterClear();
-    }
-  };
 
-  handleFilterClear = () => {
-    this.setState({ match: '' });
-  };
+  let filteredById = {};
 
-  sortRootsPosition = roots => {
-    const extra = roots.filter(node => !TOPICS_FOR_DISPLAY.includes(node));
-
-    return roots.length ? [...TOPICS_FOR_DISPLAY, ...extra] : roots;
-  };
-
-  filterTagsById = () => {
-    const { roots, byId } = this.props;
-    const { match }       = this.state;
-    const sortedRoots     = this.sortRootsPosition(roots);
+  const filterTagsById = () => {
+    const sortedRoots = sortRootsPosition(roots);
 
     if (!match) {
-      this.filteredById = this.setVisibleState(byId);
+      filteredById = getAllVisibleById();
       return sortedRoots;
     }
 
-    this.filteredById  = {};
+    filteredById  = {};
     const parentIdsArr = [];
-    const regExp       = getEscapedRegExp(match);
+    const regExp = getEscapedRegExp(match);
 
     // filter objects
-    Object.keys(byId).forEach((key) => {
-      const currentObj = byId[key];
+    Object.keys(byId).forEach(key => {
+      const { label, parent_id } = byId[key];
 
       // add object that includes the match
-      if (currentObj.label && regExp.test(currentObj.label)) {
-        currentObj.visible     = true;
-        this.filteredById[key] = currentObj;
+      if (label && regExp.test(label)) {
+        filteredById[key] = { ...byId[key], visible: true };
 
         // keep its parent_id key
-        if (currentObj.parent_id) {
-          parentIdsArr.push(currentObj.parent_id);
+        if (parent_id) {
+          parentIdsArr.push(parent_id);
         }
       }
     });
@@ -148,16 +134,16 @@ class TopicContainer extends Component {
     let i                    = 0;
     let index;
     while (i < parentIdsArr.length) {
-      const parent = byId[parentIdsArr[i]];
+      const { id, parent_id } = byId[parentIdsArr[i]];
 
       // keep displayRoot index for the order of the roots
-      if (!parent.parent_id) {
-        index = sortedRoots.indexOf(parent.id);
+      if (!parent_id) {
+        index = sortedRoots.indexOf(id);
         if (index > -1 && !displayRootIndexes.includes(index)) {
           displayRootIndexes.push(index);
         }
-      } else if (parent.parent_id && !parentIdsArr.includes(parent.parent_id)) {
-        parentIdsArr.push(parent.parent_id);
+      } else if (parent_id && !parentIdsArr.includes(parent_id)) {
+        parentIdsArr.push(parent_id);
       }
 
       i++;
@@ -167,52 +153,47 @@ class TopicContainer extends Component {
     const filteredRoots = displayRootIndexes.map(ind => sortedRoots[ind]);
 
     // add the parents to filteredById
-    parentIdsArr.forEach((parentKey) => {
-      this.filteredById[parentKey] = byId[parentKey];
+    parentIdsArr.forEach(parentKey => {
+      filteredById[parentKey] = byId[parentKey];
     });
 
     return filteredRoots;
+  }
+
+  // run filter
+  const filteredRoots = filterTagsById();
+
+  const isIncluded = id => !!filteredById[id];
+
+  const handleShowMoreClick = nodeId => {
+    setExpandedNodes(expNodes => {
+      // a new set has to be created to update the state
+      const newSet = new Set(expNodes);
+
+      if (newSet.has(nodeId)){
+        newSet.delete(nodeId)
+      } else {
+        newSet.add(nodeId)
+      }
+
+      return newSet;
+    })
   };
 
-  isIncluded = id => !!this.filteredById[id];
+  const renderLeaf = node =>
+    <Link to={`/topics/${node.id}`}>
+      {node.label}
+      {statDataTags && statDataTags[node.id] ? ` (${statDataTags[node.id]})` : ''}
+    </Link>
 
-  hasChildren = node => Array.isArray(node.children) && node.children.length > 0;
+  const renderNode = (node, grandchildrenClass = '') => {
+    const showExpandButton = node?.children?.length > 3;
+    const expanded = expandedNodes.has(node.id);
 
-  updateParentsVisibleState = parentId => {
-    const { expandedNodes } = this.state;
-    expandedNodes[parentId] = !expandedNodes[parentId];
-    this.setState({ expandedNodes });
-  };
-
-  renderLeaf = node => {
-    const { stats } = this.props;
-
-    return (
-      <Link to={`/topics/${node.id}`}>
-        {node.label}
-        {stats && stats[node.id] ? ` (${stats[node.id]})` : ''}
-      </Link>
-    );
-  };
-
-  renderChildren = children => (
-    children
-      .filter(this.isIncluded)
-      .map(id => (
-        <List.Item key={id} className={`${children.visible ? '' : 'hide-topic'}`}>
-          {this.renderNode(this.filteredById[id])}
-        </List.Item>
-      ))
-  );
-
-  renderNode = (node, grandchildrenClass = '') => {
-    const { t }             = this.props;
-    const { expandedNodes } = this.state;
-    const showExpandButton  = node.children && node.children.length > 3;
     return node ? (
       <Fragment key={`f-${node.id}`}>
         {
-          this.hasChildren(node) ? (
+          hasChildren(node) ? (
             <div key={node.id} className={`topics__card ${grandchildrenClass}`}>
               <Header as="h4" className="topics__subtitle">
                 <Link to={`/topics/${node.id}`}>
@@ -222,98 +203,78 @@ class TopicContainer extends Component {
               <List>
                 {
                   node.children
-                    .filter(this.isIncluded)
+                    .filter(isIncluded)
                     .map(id => (
-                      <List.Item key={id} className={this.filteredById[id].visible ? '' : 'hide-topic'}>
-                        {this.renderNode(this.filteredById[id], 'grandchildren')}
+                      <List.Item key={id} className={filteredById[id].visible ? '' : 'hide-topic'}>
+                        {renderNode(filteredById[id], 'grandchildren')}
                       </List.Item>
                     ))
                 }
               </List>
               <Button
                 basic
-                icon={expandedNodes[node.id] ? 'minus' : 'plus'}
+                icon={expanded ? 'minus' : 'plus'}
                 className={`topics__button ${showExpandButton ? '' : 'hide-button'}`}
                 size="mini"
-                content={t(`topics.show-${expandedNodes[node.id] ? 'less' : 'more'}`)}
-                onClick={() => this.updateParentsVisibleState(node.id)}
+                content={t(`topics.show-${expanded ? 'less' : 'more'}`)}
+                onClick={() => handleShowMoreClick(node.id)}
               />
             </div>
-          ) : this.renderLeaf(node)
+          ) : renderLeaf(node)
         }
       </Fragment>
     ) : null;
   };
 
-  renderSubHeader = node => this.hasChildren(node) ? this.renderNode(node) : null;
+  const renderSubHeader = node => hasChildren(node) ? renderNode(node) : null;
 
-  renderBranch = rootId => {
-    const rootNode = this.filteredById[rootId];
+  const renderBranch = rootId => {
+    const rootNode = filteredById[rootId];
 
-    if (!rootNode.children || rootNode.children.length === 0) {
-      return null;
-    }
-
-    return (
-      <Grid.Column key={rootId} className="topics__section">
+    return rootNode?.children?.length > 0
+      ? <Grid.Column key={rootId} className="topics__section">
         <Header as="h2" className="topics__title">
           {rootNode.label}
         </Header>
         <div className="topics__list">
           {
             rootNode.children
-              .filter(this.isIncluded)
-              .map(id => this.renderSubHeader(this.filteredById[id]))
+              .filter(isIncluded)
+              .map(id => renderSubHeader(filteredById[id]))
           }
         </div>
       </Grid.Column>
-    );
+      : null;
   };
 
-  render() {
-    const { t }         = this.props;
-    // run filter
-    const filteredRoots = this.filterTagsById();
-
-    return (
-      <div>
-        <SectionHeader section="topics" />
-        <Divider fitted />
-        <Container className="padded">
-          <Input
-            fluid
-            size="large"
-            icon="search"
-            className="search-omnibox"
-            placeholder={t('sources-library.filter')}
-            onChange={this.handleFilterChange}
-            onKeyDown={this.handleFilterKeyDown}
-          />
-        </Container>
-        <Container className="padded">
-          <Grid columns={3}>
-            <Grid.Row>
-              {filteredRoots.map(r => this.renderBranch(r))}
-            </Grid.Row>
-          </Grid>
-        </Container>
-      </div>
-    );
-  }
+  return (
+    <>
+      <SectionHeader section="topics" />
+      <Divider fitted />
+      <Container className="padded">
+        <Input
+          fluid
+          size="large"
+          icon="search"
+          className="search-omnibox"
+          placeholder={t('sources-library.filter')}
+          onChange={handleFilterChange}
+          onKeyDown={handleFilterKeyDown}
+        />
+      </Container>
+      <Container className="padded">
+        <Grid columns={3}>
+          <Grid.Row>
+            {filteredRoots.map(r => renderBranch(r))}
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </>
+  );
 }
 
-export default connect(
-  (state) => {
-    let stats = statsSelectors.getCUStats(state.stats, 'topics') || { data: { tags: {} } };
-    stats     = isEmpty(stats) || isEmpty(stats.data) ? null : stats.data.tags;
+TopicContainer.propTypes = {
+  t: PropTypes.func.isRequired,
+};
 
-    return {
-      roots: topicsSelectors.getDisplayRoots(state.tags),
-      byId: topicsSelectors.getTags(state.tags),
-      stats
-    };
-  },
-  dispatch => bindActionCreators({
-    fetchStats: topicsActions.fetchStats
-  }, dispatch)
-)(withNamespaces()(TopicContainer));
+export default withNamespaces()(TopicContainer);
