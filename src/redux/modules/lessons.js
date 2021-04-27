54cebf3dd579c9e9b8a3895cece6830881f9f847
GW125 <32199248+wissog@users.noreply.github.com>
2021-04-27 03:42:05 +0300

    Selected series (#704)
    * removed some braces
    
    * Topics improvement for Series usage
    
    * code improve
    
    * add fields to the tree
    
    * simplified coding
    
    * yarn.lock
    
    * locales
    
    * css
    
    * Topics code improve
    
    * new Display of Selected Series - like Topics
    
    * Removed VOLUME types from all the sources
    
    * improve render button
    
    * add By Sources caption
    
    * Kabbalists order and display changed
    
    * some improvements
    
    * better coding
    
    * expand first level of topic - expanded level <= 3
    
    * css scrolling display fix
    
    * expand all levels excluding the leaves
    
    * small code fixes

--- src/redux/modules/lessons.js
+++ src/redux/modules/lessons.js
@@ -2,14 +2,15 @@ import { createAction } from 'redux-actions';
 import groupBy from 'lodash/groupBy';
 import mapValues from 'lodash/mapValues';
 
-import { isEmpty, strCmp } from '../../helpers/utils';
+import { isEmpty, isNotEmptyArray, strCmp, getEscapedRegExp } from '../../helpers/utils';
+import { SRC_VOLUME } from '../../helpers/consts';
 import { selectors as mdb } from './mdb';
 import { handleActions, types as settings } from './settings';
 import { selectors as sources } from './sources';
+import { selectors as tags } from './tags';
 import { types as ssr } from './ssr';
 
 /* Types */
-
 const SET_TAB = 'Lessons/SET_TAB';
 
 const RECEIVE_LECTURES         = 'Lessons/RECEIVE_LECTURES';
@@ -27,7 +28,6 @@ export const types = {
 };
 
 /* Actions */
-
 const setTab = createAction(SET_TAB);
 
 const receiveLectures       = createAction(RECEIVE_LECTURES);
@@ -118,81 +118,187 @@ export const reducer = handleActions({
 const getWip            = state => state.wip;
 const getErrors         = state => state.errors;
 const getLecturesByType = state => state.lecturesByType;
+const getSeriesIDs      = state => state.seriesIDs;
 
-const $$sortTree = (node) => {
+const $$sortTree = node => {
   if (isEmpty(node)) {
     return [];
   }
 
-  // leaf nodes has array of items
+  // leaf nodes has array of children
   // we sort them by start_date
-  if (Array.isArray(node.items)) {
-    node.items.sort((a, b) => strCmp(a.start_date, b.start_date));
+  if (Array.isArray(node.children) && node.children.length > 0) {
+    node.children.sort((a, b) => strCmp(a.start_date, b.start_date));
     return node;
   }
 
-  // non-leaf nodes are reshaped to {name, items}
+  // non-leaf nodes are reshaped to {name, children}
   // instead of {name, item, item, item...}
-  const l = Object.keys(node)
-    .filter(x => x !== 'name')
+  const children = Object.keys(node)
+    .filter(x => x !== 'name' && x !== 'id' && x !== 'parent_id' && x !== 'type')
     .map(x => node[x]);
-  l.sort(x => x.name);
+
+  children.sort(x => x.name);
+
+  const { id, parent_id, name, type } = node;
 
   return {
-    name: node.name,
-    items: l.map($$sortTree),
+    id,
+    name,
+    parent_id,
+    type,
+    children: children.map($$sortTree),
   };
 };
 
-const getSeriesBySource = (state, mdbState, sourcesState) => {
-  const srcPathById = sources.getPathByID(sourcesState);
+const getTree = (state, match) => {
+  const srcPathById = sources.getPathByID(state.sources);
+  const tagPathById = tags.getPathByID(state.tags);
 
   // sources might not have been loaded by now
-  if (!srcPathById) {
+  if (!srcPathById || !tagPathById) {
     return [];
   }
 
-  // construct the folder-like tree
-  const tree = state.seriesIDs.reduce((acc, val) => {
-    const series = mdb.getCollectionById(mdbState, val);
+  const regExp = match && getEscapedRegExp(match);
 
-    // mdb might not have been loaded by now
-    if (!series) {
-      return acc;
+  const allSeries = state.lessons.seriesIDs
+    .map(serId => mdb.getCollectionById(state.mdb, serId))
+    .filter(series => !!series);
+
+  const byTag = allSeries.reduce((acc, series) => {
+    const { tag_id, name } = series;
+
+    if (isNotEmptyArray(tag_id)){
+      const tagPaths = tag_id.map(tagId => tagPathById(tagId));
+
+      for (let i = 0; i < tagPaths.length; i++) {
+        // check filter
+        const exists = match
+          ? tagPaths[i].map(x => x.label).find(n => regExp.test(n)) || regExp.test(name)
+          : true;
+
+        if (exists) {
+          addToTagsTree(acc, tagPaths[i], series);
+        }
+      }
     }
-
-    const { source_id: sourceID } = series;
-    if (!sourceID) {
-      return acc;
-    }
-
-    const path = srcPathById(sourceID);
-    if (!Array.isArray(path) || path.length === 0) {
-      return acc;
-    }
-
-    // mkdir -p path[:]
-    let dir = acc;
-    for (let i = 0; i < path.length; i++) {
-      dir[path[i].id] = dir[path[i].id] || {};
-      dir             = dir[path[i].id];
-
-      dir.name = path[i].name;
-    }
-
-    // mv series path.items
-    dir.items = dir.items || [];
-    dir.items.push(series);
 
     return acc;
   }, {});
 
-  return $$sortTree(tree).items;
+  // construct the folder-like tree
+  const bySourceTree = allSeries.reduce((acc, series) => {
+    const { source_id, name } = series;
+    if (!source_id) {
+      return acc;
+    }
+
+    const path = srcPathById(source_id);
+    if (!Array.isArray(path) || path.length === 0) {
+      return acc;
+    }
+
+    // check filter
+    const exists = match
+      ? path.map(x => x.name).find(n => regExp.test(n)) || regExp.test(name)
+      : true;
+
+    if (exists) {
+      addToSourcesTree(acc, path, series);
+    }
+
+    return acc;
+  }, {});
+
+  return { bySourceTree, byTag };
+}
+
+const addToTagsTree = (acc, path, series) => {
+  let dir = acc;
+  for (let i = 0; i < path.length; i++) {
+    const { id, label, parent_id, type } = path[i];
+
+    dir[id] = dir[id] || {};
+    dir = dir[id];
+
+    dir.id = id;
+    dir.parent_id = parent_id;
+    dir.name = label;
+    dir.type = type;
+  }
+
+  dir.children = dir.children || [];
+  dir.children.push(series);
+}
+
+const addToSourcesTree = (acc, path, series) => {
+  const hasVolume = path.some(p => p.type === SRC_VOLUME);
+
+  let volumeId, volumeParentId;
+  let dir = acc;
+  for (let i = 0; i < path.length; i++) {
+    let { id, name, parent_id, type } = path[i];
+
+    if (hasVolume) {
+      // save volume data for later and skip the path part
+      if (type === SRC_VOLUME){
+        volumeId = id;
+        volumeParentId = parent_id;
+        continue;
+      }
+
+      // for volume child - set the parent to be grandparent, e.g. skip the volume in the tree
+      if (parent_id === volumeId) {
+        parent_id = volumeParentId;
+      }
+    }
+
+    dir[id] = dir[id] || {};
+    dir = dir[id];
+
+    dir.id = id;
+    dir.parent_id = parent_id;
+    dir.name = name;
+    dir.type = type;
+  }
+  // mv series path.children
+  dir.children = dir.children || [];
+  dir.children.push(series);
+};
+
+const getSeriesTree = (state, match) => {
+  const { bySourceTree, byTag } = getTree(state, match);
+
+  if (isEmpty(bySourceTree) && isEmpty(byTag)){
+    return null;
+  }
+
+  const roots         = sources.getRoots(state.sources);
+  const getSourceById = sources.getSourceById(state.sources);
+  const authors = roots.map(getSourceById);
+
+  const fullTree = {}
+
+  Object.keys(bySourceTree).filter(key => key !== 'vk').forEach(key => {
+    bySourceTree[key].name = authors.find(a => a.id === key).full_name;
+    fullTree[key] = bySourceTree[key];
+  });
+
+  fullTree['byTopics'] = { 'id': 'byTopics', 'name': 'By Topics' };
+
+  Object.keys(byTag).forEach(key => {
+    fullTree['byTopics'][key] = byTag[key]
+  });
+
+  const sortedTree = $$sortTree(fullTree);
+  return sortedTree.children;
 };
 
 export const selectors = {
   getWip,
   getErrors,
   getLecturesByType,
-  getSeriesBySource,
+  getSeriesIDs,
+  getSeriesTree,
 };
