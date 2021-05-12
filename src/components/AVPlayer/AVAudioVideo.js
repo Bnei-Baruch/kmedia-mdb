2f53b9ca9b1e48145d0379053844a78744ab8f5d
Gabi Wissotzky <wissog@gmail.com>
2021-05-12 04:54:06 +0300

    more linting fixes

--- src/components/AVPlayer/AVAudioVideo.js
+++ src/components/AVPlayer/AVAudioVideo.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import TimedPopup from '../shared/TimedPopup';
