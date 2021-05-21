39d13a89f1226213e9e46294ec8b78d6c91dd05d
Gabi Wissotzky <wissog@gmail.com>
2021-05-21 05:15:14 +0300

    changed kabbalist title to h3

--- src/components/Sections/Lessons/tabs/Series/Container.js
+++ src/components/Sections/Lessons/tabs/Series/Container.js
@@ -100,7 +100,7 @@ const SeriesContainer = ({ t }) => {
 
   const renderKabbalist = node => {
     if (node?.children?.length > 0) {
-      const initLevel = 2;
+      const initLevel = 3;
 
       return (
         <div key={`${node.id}#${initLevel}`} >
@@ -110,7 +110,7 @@ const SeriesContainer = ({ t }) => {
           <div className="topics__list">
             <List>
               {
-                node.children.map(n => renderNode(n, initLevel + 1))
+                node.children.map(n => renderNode(n, initLevel))
               }
             </List>
           </div>
