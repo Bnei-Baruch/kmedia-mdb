8440c1991946cbd7aabf0b670a3b33e69118bcb2
Gabi Wissotzky <wissog@gmail.com>
2021-06-18 05:04:48 +0300

    added GetTempUnitIds for mockup

--- src/components/Sections/Personal/History.js
+++ src/components/Sections/Personal/History.js
@@ -1,24 +1,22 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { actions, selectors } from '../../../redux/modules/home';
-import { selectors as mdb } from '../../../redux/modules/mdb';
-import Template from './Template';
+import React from 'react';
+import Template, { GetTempUnitIds } from './Template';
 
 const History = () => {
-  const dispatch  = useDispatch();
+  // const dispatch  = useDispatch();
 
-  useEffect(() => {
-    dispatch(actions.fetchData(true));
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(actions.fetchData(true));
+  // }, [dispatch]);
 
 
-  const latestUnitsFn     = latestUnitIDs => state => Array.isArray(latestUnitIDs)
-    ? latestUnitIDs.map(x => mdb.getDenormContentUnit(state.mdb, x))
-    : [];
+  // const latestUnitsFn     = latestUnitIDs => state => Array.isArray(latestUnitIDs)
+  //   ? latestUnitIDs.map(x => mdb.getDenormContentUnit(state.mdb, x))
+  //   : [];
 
-  const latestUnitIDs = useSelector(state => selectors.getLatestUnits(state.home));
-  const latestUnits   = useSelector(latestUnitsFn(latestUnitIDs));
+  // const latestUnitIDs = useSelector(state => selectors.getLatestUnits(state.home));
+  // const latestUnits   = useSelector(latestUnitsFn(latestUnitIDs));
+
+  const latestUnits = GetTempUnitIds();
 
   return (
     <Template units={latestUnits} title={"History"} />
--- src/components/Sections/Personal/Playlists.js
+++ src/components/Sections/Personal/Playlists.js
@@ -1,24 +1,8 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { actions, selectors } from '../../../redux/modules/home';
-import { selectors as mdb } from '../../../redux/modules/mdb';
-import Template from './Template';
+import React from 'react';
+import Template, { GetTempUnitIds } from './Template';
 
 const Playlists = () => {
-  const dispatch  = useDispatch();
-
-  useEffect(() => {
-    dispatch(actions.fetchData(true));
-  }, [dispatch]);
-
-
-  const latestUnitsFn     = latestUnitIDs => state => Array.isArray(latestUnitIDs)
-    ? latestUnitIDs.map(x => mdb.getDenormContentUnit(state.mdb, x))
-    : [];
-
-  const latestUnitIDs = useSelector(state => selectors.getLatestUnits(state.home));
-  const latestUnits   = useSelector(latestUnitsFn(latestUnitIDs));
+  const latestUnits = GetTempUnitIds();
 
   return (
     <Template units={latestUnits} title={"Playlists"} />
