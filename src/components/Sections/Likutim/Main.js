006757c77bdaa179ca44f0fbf59035402124015d
Gabi Wissotzky <wissog@gmail.com>
2021-09-01 05:53:44 +0300

    added filters

--- src/components/Sections/Likutim/Main.js
+++ src/components/Sections/Likutim/Main.js
@@ -1,14 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { withNamespaces } from 'react-i18next';
-import { List, Card, Grid, Divider } from 'semantic-ui-react';
+import { List, Card, Grid, Divider, Container } from 'semantic-ui-react';
 
 import { actions, selectors } from '../../../redux/modules/likutim';
 import { selectors as settings } from '../../../redux/modules/settings';
 import { canonicalLink } from '../../../helpers/links';
+import { noop } from '../../../helpers/utils';
 
 import Link from '../../Language/MultiLanguageLink';
 import SectionHeader from '../../shared/SectionHeader';
+import Filters from '../../Filters/Filters';
+import filterComponents from '../../../components/Filters/components';
+
+const filters = [
+  {
+    name: 'topics-filter',
+    component: filterComponents.TopicsFilter,
+  },
+  {
+    name: 'date-filter',
+    component: filterComponents.DateFilter,
+  },
+  {
+    name: 'language-filter',
+    component: filterComponents.LanguageFilter,
+  },
+];
 
 const Main = ({ t }) => {
   const wip = useSelector(state => selectors.getWip(state.likutim));
@@ -26,13 +44,17 @@ const Main = ({ t }) => {
 
   useEffect(() => {
     if (!wip && !err && !dataLoaded) {
-      console.log('fetch likutim:')
       dispatch(actions.fetchLikutim());
       setDataLoaded(true);
     }
   }, [dispatch, err, wip, dataLoaded])
 
-  console.log('likutim:', language, likutim);
+  // console.log('likutim:', language, likutim);
+
+  // reload data on filter change
+  const handleFiltersChanged = () => {
+    setDataLoaded(false);
+  };
 
   const sortedLikutim = likutim.sort((l1, l2) => l1.name < l2.name ? -1 : 1);
   const firstLetters = sortedLikutim
@@ -43,32 +65,46 @@ const Main = ({ t }) => {
     <div>
       <SectionHeader section="likutim" />
       <Divider fitted />
-      <Grid container padded>
-        <Card.Group stackable itemsPerRow={4}>
-          {
-            firstLetters.map(f =>
-              <Card key={f}>
-                <Card.Content>
-                  <Card.Header as='h2' textAlign='center'>
-                    {f}
-                  </Card.Header>
-                  <List relaxed>
-                    {
-                      sortedLikutim.filter(sl => sl.name[0] === f).map(lu =>
-                        <List.Item key={lu.id}>
-                          <List.Header>
-                            <Link to={canonicalLink(lu)}>{`${lu.name} | ${t('values.date', { date: lu.film_date })}`}</Link>
-                          </List.Header>
-                        </List.Item>
-                      )
-                    }
-                  </List>
-                </Card.Content>
-              </Card>
-            )
-          }
-        </Card.Group>
-      </Grid>
+      <Filters
+        namespace="likutim"
+        filters={filters}
+        onChange={handleFiltersChanged}
+        onHydrated={noop}
+      />
+      <Container className="padded">
+        <Grid>
+          <Grid.Column>
+            <Card.Group stackable itemsPerRow={4}>
+              {
+                firstLetters.map(f =>
+                  <Card key={f}>
+                    <Card.Content>
+                      <Card.Header as='h2' textAlign='center'>
+                        {f}
+                      </Card.Header>
+                      <List relaxed>
+                        {
+                          sortedLikutim
+                            .filter(sl => sl.name[0] === f)
+                            .map(lu =>
+                              <List.Item key={lu.id}>
+                                <List.Header>
+                                  <Link to={canonicalLink(lu)}>
+                                    {`${lu.name} | ${t('values.date', { date: lu.film_date })}`}
+                                  </Link>
+                                </List.Header>
+                              </List.Item>
+                            )
+                        }
+                      </List>
+                    </Card.Content>
+                  </Card>
+                )
+              }
+            </Card.Group>
+          </Grid.Column>
+        </Grid>
+      </Container>
     </div>
   )
 }
--- src/sagas/likutim.js
+++ src/sagas/likutim.js
@@ -1,18 +1,20 @@
 import { call, put, takeLatest, select } from 'redux-saga/effects';
 
 import Api from '../helpers/Api';
-// import { IsCollectionContentType } from '../helpers/consts';
 import { CT_LIKUTIM } from '../helpers/consts';
 import { actions, types } from '../redux/modules/likutim';
 import { actions as mdbActions } from '../redux/modules/mdb';
 import { selectors as settings } from '../redux/modules/settings';
-
+import { selectors as filterSelectors } from '../redux/modules/filters';
+import { filtersTransformer } from '../filters';
 
 function* fetchLikutim() {
   try {
+    const filters      = yield select(state => filterSelectors.getFilters(state.filters, 'likutim'));
+    const filterParams = filtersTransformer.toApiParams(filters) || {};
+
     const language = yield select(state => settings.getContentLanguage(state.settings));
-    const { data } = yield call(Api.units, { content_type: CT_LIKUTIM, language });
-    console.log('likutim data:', language, data);
+    const { data } = yield call(Api.units, { content_type: CT_LIKUTIM, language, ...filterParams });
 
     yield put(mdbActions.receiveContentUnits(data.content_units));
     yield put(actions.fetchLikutimSuccess(data))
