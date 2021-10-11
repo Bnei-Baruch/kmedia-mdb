62e29bf02f5be772fcb747144d8b169e90da3ce9
davgur <davgur.ru@gmail.com>
2021-10-11 03:47:17 +0300

    Ar 840 from likutim to related lesson associations not fetched (#781)
    * add default language
    
    * Fetch associations
    
    * Fetch associations
    
    * Fetch associations
    
    * Show related files from all types - women lesson, virtual lesson and more
    
    * lint fix
    
    * reuse code
    
    * add scroll to search
    
    * order imports
    
    * Fix recommend label 'new'. (#782)
    
    * Bug play next lesson on playlist (#780)
    
    * restore removed code
    
    * fix refresh with keycloack
    
    * fetch CU if no collection
    
    * show wright language
    
    * show wright language
    
    Co-authored-by: bbfsdev <bbfsdev@gmail.com>

---
+++ src/components/shared/ScrollToSearch.js
@@ -0,0 +1,92 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import { DeviceInfoContext, SessionInfoContext } from '../../helpers/app-contexts';
+import { getQuery } from '../../helpers/url';
+import { buildSearchLinkFromSelection, DOM_ROOT_ID, prepareScrollToSearch } from '../../helpers/scrollToSearch/helper';
+import ShareBar from './ShareSelected';
+import { SCROLL_SEARCH_ID } from '../../helpers/consts';
+
+const ScrollToSearch = ({ data, language, urlParams='' }) => {
+
+  const { enableShareText: { isShareTextEnabled, setEnableShareText } } = useContext(SessionInfoContext);
+  const { isMobileDevice }                                              = useContext(DeviceInfoContext);
+
+  const [searchUrl, setSearchUrl]   = useState();
+  const [searchText, setSearchText] = useState();
+
+  const location                             = useLocation();
+  const { srchstart, srchend, highlightAll } = getQuery(location);
+  const search                               = { srchstart, srchend };
+
+  useEffect(() => {
+    const element = document.getElementById(SCROLL_SEARCH_ID);
+    if (element === null) {
+      return;
+    }
+
+    setTimeout(() => element.scrollIntoView(), 0);
+  }, [data]);
+
+  useEffect(() => {
+    const handleOnMouseUp = e => {
+      if (isMobileDevice || !isShareTextEnabled) {
+        return false;
+      }
+
+      const { url, text } = buildSearchLinkFromSelection(language);
+      if (url) {
+        setSearchText(text);
+        setSearchUrl(`${url}&${urlParams}`);
+      }
+
+      return false;
+    };
+
+    document.addEventListener('mouseup', handleOnMouseUp);
+    return () => document.removeEventListener('mouseup', handleOnMouseUp);
+  }, [isMobileDevice, isShareTextEnabled, language]);
+
+  if (!data) {
+    return null;
+  }
+
+  const disableShareBar = () => setEnableShareText(false);
+
+  const renderShareBar = () => {
+    if (isMobileDevice || !searchUrl)
+      return null;
+
+    return <ShareBar url={searchUrl} text={searchText} disable={disableShareBar} />;
+  };
+
+  const handleOnMouseDown = e => {
+    if (isMobileDevice || !isShareTextEnabled) {
+      return false;
+    }
+
+    setSearchUrl(null);
+    return false;
+  };
+
+  return (
+    <div className="search-on-page--container">
+      {isShareTextEnabled && renderShareBar()}
+      {
+        <div
+          id={DOM_ROOT_ID}
+          onMouseDown={handleOnMouseDown}
+          className="source__content"
+          dangerouslySetInnerHTML={{ __html: prepareScrollToSearch(data, search, highlightAll === 'true') }}
+        />
+      }
+    </div>
+  );
+};
+
+ScrollToSearch.propTypes = {
+  data: PropTypes.any,
+};
+
+export default ScrollToSearch;
