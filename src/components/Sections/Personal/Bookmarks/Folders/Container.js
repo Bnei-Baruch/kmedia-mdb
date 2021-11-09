1026ce1e298d5c4ac83aee494402ea825c795c65
davgur <davgur.ru@gmail.com>
2021-11-09 10:57:07 +0200

    lint

--- src/components/Sections/Personal/Bookmarks/Folders/Container.js
+++ src/components/Sections/Personal/Bookmarks/Folders/Container.js
@@ -2,7 +2,7 @@ import React, { useCallback, useContext, useEffect } from 'react';
 import { withRouter } from 'react-router';
 import { withNamespaces } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
-import { ListItem } from 'semantic-ui-react';
+import { ListItem, List } from 'semantic-ui-react';
 
 const Container = ({ location, t }) => (
   <List>
