6f9b382b01aeaf1e2e8070622b884fc398e60823
davgur <davgur.ru@gmail.com>
2021-11-14 11:37:47 +0200

    fix css my lists

--- src/components/Sections/Personal/Main/PlaylistItem.js
+++ src/components/Sections/Personal/Main/PlaylistItem.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Card, Header, Icon, Table } from 'semantic-ui-react';
+import { Card, Container, Header, Icon, Table } from 'semantic-ui-react';
 
 import { selectors as mdb } from '../../../../redux/modules/mdb';
 import { MY_NAMESPACE_PLAYLISTS } from '../../../../helpers/consts';
 import { isLanguageRtl } from '../../../../helpers/i18n-utils';
 import { canonicalLink } from '../../../../helpers/links';
-import { imageByUnit } from '../../../../helpers/utils';
+import { formatDuration, imageByUnit } from '../../../../helpers/utils';
 import UnitLogo from '../../../shared/Logo/UnitLogo';
 import Link from '../../../Language/MultiLanguageLink';
 import PlaylistPlayIcon from '../../../../images/icons/PlaylistPlay';
 import { getMyItemKey } from '../../../../helpers/my';
+import clsx from 'clsx';
 
 export const PlaylistItem = ({ item, t, language, asList = false }) => {
   const unit = useSelector(state => mdb.getDenormContentUnit(state.mdb, item.content_unit_uid)) || {};
@@ -37,6 +38,31 @@ export const PlaylistItem = ({ item, t, language, asList = false }) => {
   );
 
   const renderAsList = () => (
+    <Container
+      as={Link}
+      to={href}
+      key={key}
+      className={clsx('cu_item cu_item_list no-thumbnail')}
+    >
+      <div className="my_playlist_item">
+        <div className="over_layer">
+          <Header as={'h3'}>{item.total_items || 0}</Header>
+          <Icon name="list" size="large" />
+        </div>
+        <UnitLogo unitId={item.poster_unit_uid} width={165} fallbackImg={canonicalSection} />
+      </div>
+      <div className={`cu_item_info ${isRtl ? 'rtl' : 'ltr'}`}>
+
+        <Header as="h4" className="weight-normal no-margin-top">
+          {item.name}
+          <Header.Subheader>{`${item.total_items || 0} ${t('personal.videosOnList')}`}</Header.Subheader>
+        </Header>
+      </div>
+    </Container>
+
+  );
+
+  const renderAsList_ = () => (
     <Table.Row className="cu_item cu_item_list no-thumbnail" verticalAlign="top" key={key}>
       <Table.Cell width={7} className={'no-padding-top'}>
         <Link to={href}>
@@ -45,7 +71,7 @@ export const PlaylistItem = ({ item, t, language, asList = false }) => {
               <Header as={'h3'}>{item.count || 0}</Header>
               <Icon name="list" size="large" />
             </div>
-            <UnitLogo unitId={item.content_unit_uid} width={150} fallbackImg={canonicalSection} />
+            <UnitLogo unitId={item.poster_unit_uid} width={150} fallbackImg={canonicalSection} />
           </div>
         </Link>
       </Table.Cell>
--- src/components/Sections/Personal/Playlist/Page.js
+++ src/components/Sections/Personal/Playlist/Page.js
@@ -36,7 +36,6 @@ const Page = ({ t }) => {
     id && dispatch(actions.fetchOne(MY_NAMESPACE_PLAYLISTS, { id }));
   }, [id, language, user, dispatch]);
 
-
   const needToLogin = NeedToLogin({ t });
   if (needToLogin) return needToLogin;
 
@@ -50,17 +49,11 @@ const Page = ({ t }) => {
   const items         = [...playlist.items || []];
   items.sort((a, b) => b.position - a.position);
 
-  const removeItem = (e, piID) => {
-    e.preventDefault();
-    e.stopPropagation();
-    dispatch(actions.remove(MY_NAMESPACE_PLAYLISTS, { id, ids: [piID], changeItems: true }));
-  };
+  const removeItem = piID => dispatch(actions.remove(MY_NAMESPACE_PLAYLISTS, { id, ids: [piID], changeItems: true }));
 
   const onAlertCloseHandler = () => dispatch(actions.setDeleted(MY_NAMESPACE_PLAYLISTS, false));
 
-  const changeItemPosition = (e, i, up) => {
-    e.preventDefault();
-    e.stopPropagation();
+  const changeItemPosition = (i, up) => {
     const currentItem = items[i];
     const nextItem    = up ? items[i - 1] : items[i + 1];
     let cp, np;
@@ -76,6 +69,11 @@ const Page = ({ t }) => {
     dispatch(actions.edit(MY_NAMESPACE_PLAYLISTS, { id, items: _items, changeItems: true }));
   };
 
+  const stopBubbling = e => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
   const renderItem = (x, i) => (
     <ContentItemContainer
       id={x.content_unit_uid}
@@ -83,13 +81,13 @@ const Page = ({ t }) => {
       link={`${link}?ap=${i}`}
       asList
     >
-      <div className="my_playlist_actions">
+      <div className="my_playlist_actions" onClick={stopBubbling}>
         <Button
           basic
           icon="long arrow alternate up"
           className="no-shadow"
           disabled={i === 0}
-          onClick={e => changeItemPosition(e, i, true)}
+          onClick={() => changeItemPosition(i, true)}
         />
         <Popup
           basic
@@ -99,7 +97,7 @@ const Page = ({ t }) => {
               basic
               icon="remove circle"
               className="no-shadow"
-              onClick={e => removeItem(e, x.id)}
+              onClick={() => removeItem(x.id)}
             />
           }>
         </Popup>
@@ -108,7 +106,7 @@ const Page = ({ t }) => {
           icon="long arrow alternate down"
           className="no-shadow"
           disabled={i === items.length - 1}
-          onClick={e => changeItemPosition(e, i, false)}
+          onClick={() => changeItemPosition(i, false)}
         />
       </div>
     </ContentItemContainer>
