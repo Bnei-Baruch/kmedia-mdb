1f4f47b951cf54b3acc6d1cc949cbe86f1043582
davgur <davgur.ru@gmail.com>
2021-11-18 14:33:44 +0200

    lint

--- src/components/Sections/Personal/Bookmarks/Bookmarks/List.js
+++ src/components/Sections/Personal/Bookmarks/Bookmarks/List.js
@@ -54,10 +54,10 @@ const BookmarkList = ({ t }) => {
     <List divided relaxed celled>
       {
         items.map(x => <BookmarksItem
-            bookmark={x}
-            getPathByID={getPathByID}
-            key={`${MY_NAMESPACE_BOOKMARKS}_${x.id}`}
-          />
+          bookmark={x}
+          getPathByID={getPathByID}
+          key={`${MY_NAMESPACE_BOOKMARKS}_${x.id}`}
+        />
         )
       }
     </List>
