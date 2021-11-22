dfcb125f055c825e59306f2aadbb9e3b2194ed0f
davgur <davgur.ru@gmail.com>
2021-11-22 14:26:19 +0200

    lint

--- src/components/shared/DocToolbar/NoteBtn.js
+++ src/components/shared/DocToolbar/NoteBtn.js
@@ -3,15 +3,12 @@ import PropTypes from 'prop-types';
 import { withNamespaces } from 'react-i18next';
 import { Button, MenuItem } from 'semantic-ui-react';
 
-const NoteBtn = ({ t, url }) => {
-
-  return (
-    <MenuItem>
-      <Button circular icon="sticky note" />
+const NoteBtn = ({ t, url }) => (
+  <MenuItem>
+    <Button circular icon="sticky note" />
       note
-    </MenuItem>
-  );
-};
+  </MenuItem>
+);
 
 NoteBtn.propTypes = {
   t: PropTypes.func.isRequired,
