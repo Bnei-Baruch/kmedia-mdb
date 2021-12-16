c9a311a5a2a4a32c46218d50efb422f72b17b352
davgur <davgur.ru@gmail.com>
2021-12-16 15:37:57 +0200

    lint

--- src/components/shared/DocToolbar/Toolbar.js
+++ src/components/shared/DocToolbar/Toolbar.js
@@ -1,15 +1,15 @@
-import React, {useRef, useState} from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import {withNamespaces} from 'react-i18next';
-import {Button, Header, Icon, Label, Menu, MenuItem, Popup} from 'semantic-ui-react';
-import {useSelector} from 'react-redux';
+import { withNamespaces } from 'react-i18next';
+import { Button, Header, Icon, Label, Menu, MenuItem, Popup } from 'semantic-ui-react';
+import { useSelector } from 'react-redux';
 
-import {selectors} from '../../../redux/modules/auth';
+import { selectors } from '../../../redux/modules/auth';
 import ShareBtn from './ShareBtn';
 import CopyBtn from './CopyBtn';
 import BookmarkBtn from './BookmarkBtn';
 
-const DocToolbar = ({t, url, text, source, position, disable}) => {
+const DocToolbar = ({ t, url, text, source, position, disable }) => {
   const [open, setOpen] = useState(!!url);
   const user = useSelector(state => selectors.getUser(state.auth));
   const contextRef = useRef();
@@ -22,7 +22,7 @@ const DocToolbar = ({t, url, text, source, position, disable}) => {
     <div
       className="search-on-doc--bar-position"
       ref={contextRef}
-      style={{top: `${position.y}px`, left: `${position.x}px`}}
+      style={{ top: `${position.y}px`, left: `${position.x}px` }}
     >
       <div className="search-on-doc--toolbar">
         <Label
@@ -46,9 +46,9 @@ const DocToolbar = ({t, url, text, source, position, disable}) => {
         <Menu compact inverted>
           <ShareBtn url={'url'}/>
           <CopyBtn icon="copy" name={t('share-text.copy-text')} text={text}
-                   popup={t('messages.text-copied-to-clipboard')}/>
+            popup={t('messages.text-copied-to-clipboard')}/>
           <CopyBtn icon="linkify" name={t('share-text.copy-link')} text={url}
-                   popup={t('messages.link-copied-to-clipboard')}/>
+            popup={t('messages.link-copied-to-clipboard')}/>
           {source && user && <BookmarkBtn source={source} close={handleToggle}/>}
           {/*<NoteBtn />*/}
           {/*user&&<TagBtn />*/}
