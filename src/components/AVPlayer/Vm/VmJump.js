31203d40303eca7bedd7f2c84663f92c9aa6db05
Gabi Wissotzky <wissog@gmail.com>
2022-01-02 04:24:10 +0200

    fixes

--- src/components/AVPlayer/Vm/VmJump.js
+++ src/components/AVPlayer/Vm/VmJump.js
@@ -2,18 +2,9 @@ import { useRef } from 'react';
 import { Control, usePlayerContext } from '@vime/react';
 import { Icon } from 'semantic-ui-react';
 
-const onJump = (amount, currentTime, duration, setCurrentTime) => {
-  let jumpTo = currentTime + amount;
-  // Make sure we don't exceed the duration boundaries
-  jumpTo     = Math.max(0, Math.min(jumpTo, duration));
-
-  setCurrentTime(jumpTo);
-};
-
 export const VmJump = () => {
   const ref                           = useRef(null);
   const [currentTime, setCurrentTime] = usePlayerContext(ref, 'currentTime', 0);
-  const [duration]                    = usePlayerContext(ref, 'duration', -1);
   const [isPlaybackReady]             = usePlayerContext(ref, 'playbackReady', false);
 
   return (
@@ -22,11 +13,11 @@ export const VmJump = () => {
       disabled={!isPlaybackReady}
       style={{ '--vm-control-scale': 0.5, margin: '0 -2rem', }}
     >
-      <span onClick={() => onJump(-5, currentTime, duration, setCurrentTime)}>
+      <span onClick={() => setCurrentTime(currentTime - 5)}>
         -5s<Icon name="backward" />
       </span>
       {' '}
-      <span onClick={() => onJump(+5, currentTime, duration, setCurrentTime)}>
+      <span onClick={() => setCurrentTime(currentTime + 5)}>
         <Icon name="forward" />+5s
       </span>
     </Control>
