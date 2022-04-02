1e36b8e0c2220d14799d2002b9d98001fe17d46a
Gabi Wissotzky <wissog@gmail.com>
2022-04-02 06:54:57 +0300

    added randomize button

--- src/components/Pages/PlaylistCollection/widgets/Playlist/Playlist.js
+++ src/components/Pages/PlaylistCollection/widgets/Playlist/Playlist.js
@@ -1,27 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import isEqual from 'react-fast-compare';
 
 import { DeviceInfoContext } from '../../../../../helpers/app-contexts';
 import ContentItemContainer from '../../../../shared/ContentItem/ContentItemContainer';
-import { Header } from 'semantic-ui-react';
+import { Button, Header } from 'semantic-ui-react';
 import { withNamespaces } from 'react-i18next';
 
 const PlaylistWidget = ({ playlist, selected = 0, link, t }) => {
   const { isMobileDevice } = useContext(DeviceInfoContext);
-
   const { collection, items, name } = playlist;
-  const unitsToDisplay              = items.filter(item => !!item.unit).map(item => item.unit);
+
+  const [unitsToDisplay, setUnitsToDisplay] = useState(items.filter(item => !!item.unit).map(item => item.unit))
+
+  const randomize = () => {
+    const newUnitsToDisplay = [...unitsToDisplay];
+    // create an array of randoms and sort units by it
+    const randomArr = newUnitsToDisplay.map(() => Math.random() * newUnitsToDisplay.length);
+    newUnitsToDisplay.sort((a, b) => {
+      const ai = newUnitsToDisplay.indexOf(a);
+      const bi = newUnitsToDisplay.indexOf(b);
+
+      return randomArr[ai] - randomArr[bi];
+    })
+
+    setUnitsToDisplay(newUnitsToDisplay);
+  }
 
   return (
     <div id="avbox_playlist" className="avbox__playlist-view">
       {
         !isMobileDevice &&
-        <Header
-          as="h3"
-          className={'avbox__playlist-header h3'}
-          content={name || t(`playlist.title-by-type.${collection.content_type}`)}
-        />
+        <Header as="h3" className={'avbox__playlist-header h3'}>
+          {name || t(`playlist.title-by-type.${collection.content_type}`)}
+          <Button style={{ padding: 'inherit', fontSize: '1em' }} icon='random' circular positive onClick={() => randomize()}></Button>
+        </Header>
       }
       {
         unitsToDisplay.map((unit, i) => (
@@ -48,8 +61,7 @@ PlaylistWidget.propTypes = {
 
 const areEqual = (prevProps, nextProps) => (
   nextProps.selected === prevProps.selected
-  && nextProps.nextLink === prevProps.nextLink
-  && nextProps.prevLink === prevProps.prevLink
+  && nextProps.link === prevProps.link
   && isEqual(nextProps.playlist, prevProps.playlist)
 );
 
