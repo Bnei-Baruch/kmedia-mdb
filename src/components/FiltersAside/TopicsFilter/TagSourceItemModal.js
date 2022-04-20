f026dc8fc345cb49a7cb3ea3d34cf6f9684780b7
davgur <davgur.ru@gmail.com>
2022-04-20 13:22:06 +0300

    filter modal as table

--- src/components/FiltersAside/TopicsFilter/TagSourceItemModal.js
+++ src/components/FiltersAside/TopicsFilter/TagSourceItemModal.js
@@ -1,24 +1,32 @@
-import { Button, Card, Icon, Input, Modal } from 'semantic-ui-react';
-import { FN_TOPICS_MULTI } from '../../../helpers/consts';
 import React, { useState } from 'react';
-import { withNamespaces } from 'react-i18next';
 import { useSelector } from 'react-redux';
+import { withNamespaces } from 'react-i18next';
+import { Button, Icon, Input, Modal, Table } from 'semantic-ui-react';
+import clsx from 'clsx';
+
+import { FN_TOPICS_MULTI } from '../../../helpers/consts';
 import { selectors as settings } from '../../../redux/modules/settings';
 import { getLanguageDirection } from '../../../helpers/i18n-utils';
 import TagSourceItem from './TagSourceItem';
-import clsx from 'clsx';
+
+const ITEMS_PER_ROW = 3;
+const buildRowArr   = (n) => {
+  const abs = n % ITEMS_PER_ROW;
+  const len = (n - abs) / ITEMS_PER_ROW + ((abs === 0) ? 0 : 1);
+  return Array(len).fill(0);
+};
 
 const TagSourceItemModal = props => {
   const {
-    baseItems,
-    filterName,
-    parent,
-    open,
-    onClose,
-    getById,
-    getPath,
-    t
-  } = props;
+          baseItems,
+          filterName,
+          parent,
+          open,
+          onClose,
+          getById,
+          getPath,
+          t
+        } = props;
 
   const [query, setQuery] = useState('');
 
@@ -40,14 +48,23 @@ const TagSourceItemModal = props => {
       .filter(x => x?.[field] && reg.test(x[field]));
   }
 
-  const renderAsCard = item => (
-    <Card className="tree_item_modal_content">
-      <TagSourceItem {...props} id={item.id} deep={-1} />
-    </Card>
+  const renderRow = (x, i) => (
+    <Table.Row>
+      {children.slice(i * ITEMS_PER_ROW, (i + 1) * ITEMS_PER_ROW).map(renderItem)}
+    </Table.Row>
   );
 
-  const chAsCard = children.filter(x => x?.children.length > 0);
-  const chAsLeaf = children.filter(x => !(x?.children.length > 0));
+  const renderItem = item => {
+    if (!item) return <Table.Cell />;
+
+    return (
+      <Table.Cell className={clsx('tree_item_modal_content', { 'item single_item': !(item.children.length > 0) })}>
+        <TagSourceItem {...props} id={item.id} deep={-1} />
+      </Table.Cell>
+    );
+  };
+
+  const rows = buildRowArr(children.length);
 
   return (
     <Modal
@@ -58,29 +75,21 @@ const TagSourceItemModal = props => {
       onClose={onClose}
       closeIcon={<Icon name="times circle outline" />}
     >
-      <Modal.Header className="no-border">
+      <Modal.Header className="no-border nowrap">
         {parent[field]}
         <Input
           className="search-input"
-          placeholder={t('filters.aside-filter.search-input')}
+          placeholder={t(`filters.aside-filter.search-input-${isTag ? 'topic' : 'source'}`)}
           onChange={handleSetQuery}
           value={query}
         />
       </Modal.Header>
       <Modal.Content scrolling>
-        <Card.Group itemsPerRow={3} >
+        <Table collapsing celled={false} basic>
           {
-            chAsLeaf.length > 0 && (chAsLeaf.map(x => (
-              <Card fluid={false} className="tree_item_modal_content item single_item">
-                <TagSourceItem {...props} id={x.id} deep={-1} />
-              </Card>
-            ))
-            )
+            rows.map(renderRow)
           }
-          {
-            chAsCard.map(renderAsCard)
-          }
-        </Card.Group>
+        </Table>
       </Modal.Content>
       <Modal.Actions>
         <Button primary content={t('buttons.close')} onClick={onClose} />
