0ed4b41d2c0bdde1ce5733153d03e76073bb6d92
davgur <davgur.ru@gmail.com>
2022-05-02 06:08:32 +0300

    temp 9

--- src/components/Pages/Unit/widgets/Recommended/Main/Recommended.js
+++ src/components/Pages/Unit/widgets/Recommended/Main/Recommended.js
@@ -124,16 +124,6 @@ const Recommended = ({ unit, t, filterOutUnits = [], displayTitle = true }) => {
   }, [dispatch, err, unitId, unitTags, unitCollections, filterOutUnits, prevUnitId, activeVariant, unitContentType, unitSources]);
 
   const recommendedUnitsTypes = [];
-  if (activeVariant === AB_RECOMMEND_NEW) {
-    recommendedUnitsTypes.push(RANDOM_PROGRAMS);
-    unitTags.forEach(tag => recommendedUnitsTypes.push(sameTopic(tag)));
-    unitSources.forEach(source => recommendedUnitsTypes.push(sameSource(source)));
-    unitSourceCollections.forEach(source => recommendedUnitsTypes.push(sameSourceCollection(source.id)));
-    unitCollections.forEach(collection => recommendedUnitsTypes.push(sameCollection(collection.id)));
-    recommendedUnitsTypes.push(SERIES);
-  } else if (activeVariant === AB_RECOMMEND_RANDOM) {
-    recommendedUnitsTypes.push(RANDOM_UNITS);
-  }
 
   if (activeVariant !== AB_RECOMMEND_RANDOM) {
     recommendedUnitsTypes.push(DEFAULT);
@@ -141,6 +131,132 @@ const Recommended = ({ unit, t, filterOutUnits = [], displayTitle = true }) => {
 
   const recommendedUnits = useRecommendedUnits(recommendedUnitsTypes);
 
+  const renderRecommended = [];
+  if (activeVariant === AB_RECOMMEND_NEW) {
+    renderRecommended.push(
+      <DisplayRecommended
+        key={SERIES}
+        unit={unit}
+        t={t}
+        recommendedUnits={recommendedUnits[SERIES]}
+        title={<span>{makeLandingPageLink(t, SGLP_LESSON_SERIES)}</span>}
+        displayTitle={displayTitle}
+        feedName={SERIES}
+        showLabels={false} />
+    );
+    unitCollections.forEach(collection => {
+      if (recommendedUnits[sameCollection(collection.id)].length !== 0) {
+        renderRecommended.push(
+          <DisplayRecommended
+            key={sameCollection(collection.id)}
+            unit={unit}
+            t={t}
+            recommendedUnits={recommendedUnits[sameCollection(collection.id)]}
+            title={<span>{t(`materials.recommended.same-collection`)} {makeCollectionLink(collection, t)}</span>}
+            displayTitle={displayTitle}
+            viewLimit={3}
+            feedName={sameCollection(collection.id)}
+            showLabels={true} />);
+      }
+    });
+    unitTags.forEach(tag => {
+      if (recommendedUnits[sameTopic(tag)].length !== 0) {
+        renderRecommended.push(
+          <DisplayRecommended
+            key={sameTopic(tag)}
+            unit={unit}
+            t={t}
+            recommendedUnits={recommendedUnits[sameTopic(tag)]}
+            title={<span>{t('materials.recommended.same-topic')}: {makeTagLink(tag, getTagById)}</span>}
+            displayTitle={displayTitle}
+            viewLimit={3}
+            feedName={sameTopic(tag)}
+            showLabels={false} />);
+      }
+    });
+    unitSources.forEach(source => {
+      if (recommendedUnits[sameSource(source)].length !== 0) {
+        renderRecommended.push(
+          <DisplayRecommended
+            key={sameSource(source)}
+            unit={unit}
+            t={t}
+            recommendedUnits={recommendedUnits[sameSource(source)]}
+            title={<span>{t('materials.recommended.same-source')}: {makeSourceLink(source, getSourceById)}</span>}
+            displayTitle={displayTitle}
+            viewLimit={3}
+            feedName={sameSource(source)}
+            showLabels={false} />);
+      }
+    });
+    unitSourceCollections.forEach(source => {
+      if (recommendedUnits[sameSourceCollection(source.id)].length !== 0) {
+        renderRecommended.push(
+          <DisplayRecommended
+            key={sameSourceCollection(source.id)}
+            unit={unit}
+            t={t}
+            recommendedUnits={recommendedUnits[sameSourceCollection(source.id)]}
+            title={<span>{t('materials.recommended.same-source')}: {makeSourceLink(source.id, getSourceById)}</span>}
+            displayTitle={displayTitle}
+            viewLimit={3}
+            feedName={sameSourceCollection(source.id)}
+            showLabels={false} />);
+      }
+    });
+    if (recommendedUnits[RANDOM_PROGRAMS].length !== 0) {
+      renderRecommended.push(
+        <DisplayRecommended
+          key={RANDOM_PROGRAMS}
+          unit={unit}
+          t={t}
+          recommendedUnits={recommendedUnits[RANDOM_PROGRAMS]}
+          title={<span>{makeLandingPageLink(t, SGLP_PRORGRAMS)}</span>}
+          displayTitle={displayTitle}
+          viewLimit={3}
+          feedName={RANDOM_PROGRAMS}
+          showLabels={false} />
+      );
+    }
+  } else if (activeVariant === AB_RECOMMEND_RANDOM) {
+    if (recommendedUnits[RANDOM_UNITS].length !== 0) {
+      renderRecommended.push(
+        <DisplayRecommended
+          key={RANDOM_UNITS}
+          unit={unit}
+          t={t}
+          recommendedUnits={recommendedUnits[RANDOM_UNITS]}
+          title={t(`materials.recommended.${DEFAULT}`)}
+          displayTitle={displayTitle}
+          viewLimit={3}
+          feedName={RANDOM_UNITS}
+          showLabels={false} />
+      );
+    }
+  }
+
+  if (activeVariant !== AB_RECOMMEND_RANDOM && recommendedUnits[DEFAULT].length !== 0) {
+    renderRecommended.push(
+      <DisplayRecommended
+        key={DEFAULT}
+        unit={unit}
+        t={t}
+        recommendedUnits={recommendedUnits[DEFAULT]}
+        title={t(`materials.recommended.${DEFAULT}`)}
+        displayTitle={displayTitle}
+        viewLimit={activeVariant === AB_RECOMMEND_NEW ? 4 : 0}
+        feedName={DEFAULT}
+        showLabels={false} />);
+  }
+
+  const wipErr = WipErr({ wip, err, t });
+  if (wipErr) {
+    return wipErr;
+  }
+
+  if (renderRecommended.length) {
+    return renderRecommended;
+  }
 
   return null;
 }
