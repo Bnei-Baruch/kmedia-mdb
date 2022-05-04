6993697e860b9ed8a846e708109fd4f032969f26
davgur <davgur.ru@gmail.com>
2022-05-04 12:43:21 +0300

    lint

--- src/components/Pages/Unit/widgets/Recommended/Main/DisplayRecommended.js
+++ src/components/Pages/Unit/widgets/Recommended/Main/DisplayRecommended.js
@@ -63,8 +63,8 @@ const RecommendedPlaylist = ({ recommendForUnit, units, selected, t, chronicles,
             <div>
               <small className="text">
                 <Popup content={`${watchingNow(unit.id, index)} ${t('materials.recommended.watching-now')}`}
-                       trigger={
-                         <span>{`${watchingNowToString(watchingNow(unit.id, index))} ${t('materials.recommended.watching-now')}`}</span>}
+                  trigger={
+                    <span>{`${watchingNowToString(watchingNow(unit.id, index))} ${t('materials.recommended.watching-now')}`}</span>}
                 />
               </small>
             </div>
@@ -127,26 +127,26 @@ const RecommendedPlaylist = ({ recommendForUnit, units, selected, t, chronicles,
                     label={unitLabels[index]}
                     size={'small'} />
                   : (unit.content_type && IsUnitContentType(unit.content_type) ?
-                      <ContentItemContainer
+                    <ContentItemContainer
+                      id={unit.id}
+                      key={unit.id}
+                      asList
+                      label={unitLabels[index]}
+                      size={'small'} /> :
+                    (unit.type ?  /* TODO: Improve the distinction between source and topic. */
+                      <SourceItemContainer
                         id={unit.id}
                         key={unit.id}
                         asList
                         label={unitLabels[index]}
                         size={'small'} /> :
-                      (unit.type ?  /* TODO: Improve the distinction between source and topic. */
-                          <SourceItemContainer
-                            id={unit.id}
-                            key={unit.id}
-                            asList
-                            label={unitLabels[index]}
-                            size={'small'} /> :
-                          <TagItemContainer
-                            id={unit.id}
-                            key={unit.id}
-                            asList
-                            label={unitLabels[index]}
-                            size={'small'} />
-                      )
+                      <TagItemContainer
+                        id={unit.id}
+                        key={unit.id}
+                        asList
+                        label={unitLabels[index]}
+                        size={'small'} />
+                    )
                   )
                 }
               </List.Item>
