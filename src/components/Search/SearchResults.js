87a36abdf51474af90667e52d02480cadbfd900b
davgur <davgur.ru@gmail.com>
2022-05-12 11:56:58 +0300

    Add topics, languages, facets filters to search.

--- src/components/Search/SearchResults.js
+++ src/components/Search/SearchResults.js
@@ -1,25 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Trans, withNamespaces } from 'react-i18next';
-import { useSelector } from 'react-redux';
-import { Container, Divider, Grid } from 'semantic-ui-react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router';
+import { Button, Container, Divider, Grid, Modal } from 'semantic-ui-react';
 
 import {
   SEARCH_GRAMMAR_HIT_TYPES,
   SEARCH_INTENT_HIT_TYPE_SERIES_BY_SOURCE,
   SEARCH_INTENT_HIT_TYPE_SERIES_BY_TAG,
-  SEARCH_INTENT_HIT_TYPES
+  SEARCH_INTENT_HIT_TYPES,
+  BLOGS,
 } from '../../helpers/consts';
 import { isEmpty } from '../../helpers/utils';
 import { getQuery } from '../../helpers/url';
-import { ClientChroniclesContext } from '../../helpers/app-contexts';
+import { ClientChroniclesContext, DeviceInfoContext } from '../../helpers/app-contexts';
+import { getLanguageDirection } from '../../helpers/i18n-utils';
+import { actions, selectors } from '../../redux/modules/search';
+import { selectors as mdbSelectors } from '../../redux/modules/mdb';
 import { selectors as settings } from '../../redux/modules/settings';
 import { selectors as filterSelectors } from '../../redux/modules/filters';
 import { selectors as sourcesSelectors } from '../../redux/modules/sources';
 import { selectors as tagsSelectors } from '../../redux/modules/tags';
+import { selectors as publicationSelectors } from '../../redux/modules/publications';
 import { filtersTransformer } from '../../filters';
-import * as shapes from '../shapes';
 import WipErr from '../shared/WipErr/WipErr';
+import SectionHeader from '../shared/SectionHeader';
 import Pagination from '../Pagination/Pagination';
 import ResultsPageHeader from '../Pagination/ResultsPageHeader';
 import SearchResultCU from './SearchResultCU';
@@ -31,37 +37,95 @@ import SearchResultSource from './SearchResultSource';
 import SearchResultPost from './SearchResultPost';
 import DidYouMean from './DidYouMean';
 import SearchResultSeries from './SearchResultSeries';
+import Filters from './Filters';
+import FilterLabels from '../FiltersAside/FilterLabels';
 
-const SearchResults = props => {
+const cuMapFromState = (state, results) => (
+  results && results.hits && Array.isArray(results.hits.hits)
+    ? results.hits.hits.reduce((acc, val) => {
+      if (val._source.result_type === 'units') {
+        const cuID = val._source.mdb_uid;
+        const cu   = mdbSelectors.getDenormContentUnit(state.mdb, cuID);
+        if (cu) {
+          acc[cuID] = cu;
+        }
+      }
+
+      return acc;
+    }, {})
+    : {}
+);
+
+const postMapFromState = (state, results) => (
+  results && results.hits && Array.isArray(results.hits.hits)
+    ? results.hits.hits.reduce((acc, val) => {
+      if (val._source.result_type === 'posts') {
+        const ids     = val._source.mdb_uid.split('-');
+        const blogObj = BLOGS.find(b => b.id === parseInt(ids[0], 10));
+        const p       = publicationSelectors.getBlogPost(state.publications, blogObj.name, ids[1]);
+        if (p) {
+          acc[val._source.mdb_uid] = p;
+        }
+      }
+
+      return acc;
+    }, {})
+    : {}
+);
+
+const cMapFromState = (state, results) => (
+  results && results.hits && Array.isArray(results.hits.hits)
+    ? results.hits.hits.reduce((acc, val) => {
+      if (val._source.result_type === 'collections') {
+        const cID = val._source.mdb_uid;
+        const c   = mdbSelectors.getDenormCollection(state.mdb, cID);
+        if (c && c.content_units?.length) {
+          acc[cID] = c;
+        }
+      }
+
+      return acc;
+    }, {})
+    : {}
+);
+
+const SearchResults = ({ t }) => {
+  const [openFilters, setOpenFilters] = useState(false);  // For mobile only.
+  const closeFilters   = () => setOpenFilters(false);
+
+  const queryResult = useSelector(state => selectors.getQueryResult(state.search));
+  const searchResults     = queryResult.search_result;
+
+  const cMap = useSelector(state => cMapFromState(state, searchResults));
+  const cuMap = useSelector(state => cuMapFromState(state, searchResults));
+  const postMap = useSelector(state => postMapFromState(state, searchResults));
+
+  const wip = useSelector(state => selectors.getWip(state.search));
+  const err = useSelector(state => selectors.getError(state.search));
+
+  const pageNo = useSelector(state => selectors.getPageNo(state.search));
+  const pageSize = useSelector(state => settings.getPageSize(state.settings));
+  const language = useSelector(state => settings.getLanguage(state.settings));
+
+  const location = useLocation();
+  const dispatch = useDispatch();
+
   /* Requested by Mizrahi
     const [showNote, setShowNote] = useState(true);
    */
-  const filters          = useSelector(state => filterSelectors.getFilters(state.filters, 'search'));
-  const areSourcesLoaded = useSelector(state => sourcesSelectors.areSourcesLoaded(state.sources));
-  const getTagById       = useSelector(state => tagsSelectors.getTagById(state.tags));
-  const getSourcePath    = useSelector(state => sourcesSelectors.getSourceById(state.sources));
-  const getSourceById    = useSelector(state => sourcesSelectors.getSourceById(state.sources));
-  const contentLanguage  = useSelector(state => settings.getContentLanguage(state.settings));
-  const chronicles       = useContext(ClientChroniclesContext);
+  const filters            = useSelector(state => filterSelectors.getFilters(state.filters, 'search'));
+  const areSourcesLoaded   = useSelector(state => sourcesSelectors.areSourcesLoaded(state.sources));
+  const getTagById         = useSelector(state => tagsSelectors.getTagById(state.tags));
+  const getSourcePath      = useSelector(state => sourcesSelectors.getSourceById(state.sources));
+  const getSourceById      = useSelector(state => sourcesSelectors.getSourceById(state.sources));
+  const contentLanguage    = useSelector(state => settings.getContentLanguage(state.settings));
+  const chronicles         = useContext(ClientChroniclesContext);
+  const { isMobileDevice } = useContext(DeviceInfoContext);
+  const dir                = getLanguageDirection(language);
 
-  const
-    {
-      queryResult,
-      cMap,
-      cuMap,
-      postMap,
-      wip,
-      err,
-      hitType,
-      pageNo,
-      pageSize,
-      language,
-      t,
-      handlePageChange,
-      location
-    } = props;
-
-  const filterByHitType = hit => hitType ? hit.type === hitType : true;
+  const handlePageChange = page => {
+    dispatch(actions.setPage(page));
+  };
 
   const searchLanguageByIndex = (index, def) => index.split('_')[2] ?? def;
 
@@ -81,7 +145,7 @@ const SearchResults = props => {
 
     searchLanguage = searchLanguageByIndex(_index, searchLanguage);
     const newProps = {
-      ...props, filters, getTagById, getSourceById, contentLanguage, getSourcePath, searchLanguage,
+      queryResult, t, location, filters, getTagById, getSourceById, contentLanguage, getSourcePath, searchLanguage,
       hit, rank, key, chronicles,
     };
 
@@ -151,7 +215,7 @@ const SearchResults = props => {
   };
    */
 
-  const wipErr = WipErr({ wip: wip || !areSourcesLoaded, err, t });
+  const wipErr = WipErr({ wip: !areSourcesLoaded, err, t });
   if (wipErr) {
     return wipErr;
   }
@@ -173,84 +237,100 @@ const SearchResults = props => {
   // Elastic too slow and might fails on more than 1k results.
   const totalForPagination                    = Math.min(1000, total);
 
-  if (total === 0) {
-    return (
-      <div>
-        {typo_suggest && <DidYouMean typo_suggest={typo_suggest} />}
+  const renderFilters = () => (<Filters namespace={'search'} />);
+  const renderSearchResults = () => (
+    <>
+      {/* Requested by Mizrahi renderTopNote() */}
+      {typo_suggest && <DidYouMean typo_suggest={typo_suggest} />}
+
+      <FilterLabels namespace={'search'} />
+
+      {total === 0 ?
         <Trans i18nKey="search.results.no-results">
           Your search for
           <strong style={{ fontStyle: 'italic' }}>{{ query }}</strong>
           found no results.
         </Trans>
-      </div>
-    );
-  }
+        :
+        <>
+          <div className="searchResult_content">
+            {wip ?
+              WipErr({ wip, err: null, t })
+              :
+              <>
+                <ResultsPageHeader pageNo={pageNo} total={total} pageSize={pageSize} t={t} />
+                {hits.map((h, rank) => renderHit(h, rank, searchLanguage))}
+              </>
+            }
+          </div>
+          <Divider fitted />
+          <Container className="padded pagination-wrapper" textAlign="center">
+            <Pagination
+              pageNo={pageNo}
+              pageSize={pageSize}
+              total={totalForPagination}
+              language={language}
+              onChange={handlePageChange}
+            />
+          </Container>
+        </>
+      }
+    </>);
 
   return (
-    <Grid>
-      <Grid.Column key="1" computer={16} tablet={16} mobile={16}>
-        {/* Requested by Mizrahi renderTopNote() */}
-        {typo_suggest && <DidYouMean typo_suggest={typo_suggest} />}
-
-        <div className="searchResult_content">
-          <ResultsPageHeader pageNo={pageNo} total={total} pageSize={pageSize} t={t} />
-          {hits.filter(filterByHitType).map((h, rank) => renderHit(h, rank, searchLanguage))}
-        </div>
-        <Divider fitted />
-
-        <Container className="padded pagination-wrapper" textAlign="center">
-          <Pagination
-            pageNo={pageNo}
-            pageSize={pageSize}
-            total={totalForPagination}
-            language={language}
-            onChange={handlePageChange}
-          />
-        </Container>
-      </Grid.Column>
-      {/* <Grid.Column key="2" /> */}
-    </Grid>
+    <div>
+      <>
+        { isMobileDevice &&
+          <>
+            <Button className="search_mobile_filter" basic icon="filter" floated={'right'} onClick={() => setOpenFilters(true)} />
+            <Modal
+              closeIcon
+              open={openFilters}
+              onClose={closeFilters}
+              dir={dir}
+              className={dir}
+            >
+              <Modal.Content className="filters-aside-wrapper" scrolling>
+                {renderFilters()}
+              </Modal.Content>
+              <Modal.Actions>
+                <Button primary content={t('buttons.close')} onClick={closeFilters} />
+              </Modal.Actions>
+            </Modal>
+          </>
+        }
+        <SectionHeader section="search" />
+      </>
+      <Container className="padded">
+        { isMobileDevice ?
+          <Grid divided>
+            <Grid.Column>
+              {renderSearchResults()}
+            </Grid.Column>
+          </Grid>
+          :
+          <Grid divided>
+            <Grid.Column width="4" className="filters-aside-wrapper">
+              {renderFilters()}
+            </Grid.Column>
+            <Grid.Column width="12">
+              {renderSearchResults()}
+            </Grid.Column>
+          </Grid>
+        }
+      </Container>
+    </div>
   );
 };
 
 SearchResults.propTypes = {
-  getSourcePath: PropTypes.func,
-  queryResult: PropTypes.shape({
-    intents: PropTypes.arrayOf(PropTypes.shape({
-      language: PropTypes.string,
-      type: PropTypes.string,
-      value: PropTypes.shape({}),
-    })),
-    search_result: PropTypes.shape({})
-  }),
-  cMap: PropTypes.objectOf(shapes.Collection),
-  cuMap: PropTypes.objectOf(shapes.ContentUnit),
-  twitterMap: PropTypes.objectOf(shapes.Tweet),
-  pageNo: PropTypes.number.isRequired,
-  pageSize: PropTypes.number.isRequired,
-  language: PropTypes.string.isRequired,
-  wip: shapes.WIP,
-  err: shapes.Error,
   t: PropTypes.func.isRequired,
-  handlePageChange: PropTypes.func.isRequired,
-  location: shapes.HistoryLocation.isRequired,
-  click: PropTypes.func.isRequired,
-  postMap: PropTypes.objectOf(PropTypes.shape({
-    blog: PropTypes.string,
-    content: PropTypes.string,
-    created_at: PropTypes.string,
-    title: PropTypes.string,
-    url: PropTypes.string,
-    wp_id: PropTypes.number,
-  })).isRequired,
-  hitType: PropTypes.shape({}),
 };
 
 SearchResults.defaultProps = {
   queryResult: null,
   cMap: {},
   cuMap: {},
-  twitterMap: {},
   wip: false,
   err: null,
   getSourcePath: undefined
