84b537d4872c213f769f29d820d225073ce6da35
davgur <davgur.ru@gmail.com>
2022-05-25 17:49:32 +0300

    add wip

--- src/components/Sections/Program/ProgramPage.js
+++ src/components/Sections/Program/ProgramPage.js
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { Container, Divider, Grid } from 'semantic-ui-react';
 import { PAGE_NS_PROGRAMS } from '../../../helpers/consts';
-import { usePrevious } from '../../../helpers/utils';
+import { isEmpty, usePrevious } from '../../../helpers/utils';
 import { selectors as filters } from '../../../redux/modules/filters';
 import { actions, selectors as lists } from '../../../redux/modules/lists';
 
@@ -25,20 +25,15 @@ const ProgramPage = ({ t }) => {
   const { id: cid } = useParams();
   const namespace   = `${PAGE_NS_PROGRAMS}_${cid}`;
 
-  const collection                              = useSelector(state => mdb.getDenormCollection(state.mdb, cid));
-  const { collections: { [cid]: wip = false } } = useSelector(state => mdb.getWip(state.mdb));
-  const { collections: { [cid]: err = null } }  = useSelector(state => mdb.getErrors(state.mdb));
+  const collection = useSelector(state => mdb.getDenormCollection(state.mdb, cid));
 
-  const { items, total } = useSelector(state => lists.getNamespaceState(state.lists, namespace)) || {};
-  const language         = useSelector(state => settings.getLanguage(state.settings));
-  const pageSize         = useSelector(state => settings.getPageSize(state.settings));
-  const selected         = useSelector(state => filters.getFilters(state.filters, namespace), isEqual);
-  const prevSel          = usePrevious(selected);
+  const { items, total, wip, err } = useSelector(state => lists.getNamespaceState(state.lists, namespace)) || {};
+  const language                   = useSelector(state => settings.getLanguage(state.settings));
+  const pageSize                   = useSelector(state => settings.getPageSize(state.settings));
+  const selected                   = useSelector(state => filters.getFilters(state.filters, namespace), isEqual);
+  const prevSel                    = usePrevious(selected);
 
-  useEffect(() => {
-    dispatch(mdbActions.fetchCollection(cid));
-  }, []);
-
+  const dispatch = useDispatch();
   useEffect(() => {
     let page_no = pageNo > 1 ? pageNo : 1;
     if (page_no !== 1 && prevSel !== selected) page_no = 1;
@@ -48,46 +43,39 @@ const ProgramPage = ({ t }) => {
 
   const onPageChange = n => setPageNo(n);
 
-  const dispatch = useDispatch();
+  const wipErr = WipErr({ wip, err, t });
 
-  useEffect(() => {
-    dispatch(mdbActions.fetchCollection(cid));
-  }, []);
-
-  const wipErr = WipErr(wip, err, t);
-  if (wipErr) return wipErr;
-
-  return (
-    <div className="collection-page">
-      <PageHeader collection={collection} namespace={namespace} title="programs-collection" />
-      <Container className="padded" fluid>
-        <Divider />
-        <Grid divided>
-          <Grid.Column width="4" className="filters-aside-wrapper">
-            <Filters
-              namespace={namespace}
-              baseParams={{ collection: [cid] }}
-            />
-          </Grid.Column>
-          <Grid.Column width="12">
-            <ResultsPageHeader pageNo={pageNo} total={total} pageSize={pageSize} />
-            <FilterLabels namespace={namespace} />
-            {items?.map((id, i) => <ItemOfList id={id} ccu={collection} key={i} />)}
-            <Divider fitted />
-            <Container className="padded pagination-wrapper" textAlign="center">
-              {total > 0 && <Pagination
-                pageNo={pageNo}
-                pageSize={pageSize}
-                total={total}
-                language={language}
-                onChange={onPageChange}
-              />}
-            </Container>
-          </Grid.Column>
-        </Grid>
-      </Container>
-    </div>
-  );
+  return (<>
+    <PageHeader collection={collection} namespace={namespace} title="programs-collection" />
+    <Container className="padded" fluid>
+      <Divider />
+      <Grid divided>
+        <Grid.Column width="4" className="filters-aside-wrapper">
+          <Filters
+            namespace={namespace}
+            baseParams={{ collection: [cid] }}
+          />
+        </Grid.Column>
+        <Grid.Column width="12">
+          <ResultsPageHeader pageNo={pageNo} total={total} pageSize={pageSize} />
+          <FilterLabels namespace={namespace} />
+          {
+            wipErr || items?.map((id, i) => <ItemOfList id={id} ccu={collection} key={i} />)
+          }
+          <Divider fitted />
+          <Container className="padded pagination-wrapper" textAlign="center">
+            {total > 0 && <Pagination
+              pageNo={pageNo}
+              pageSize={pageSize}
+              total={total}
+              language={language}
+              onChange={onPageChange}
+            />}
+          </Container>
+        </Grid.Column>
+      </Grid>
+    </Container>
+  </>);
 };
 
 export default withNamespaces()(ProgramPage);
