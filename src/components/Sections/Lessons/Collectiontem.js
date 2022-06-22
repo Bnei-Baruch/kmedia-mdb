7c3f02ca431ef6a1b2d6900e941a6a0ee4cd72ae
davgur <davgur.ru@gmail.com>
2022-06-22 15:03:43 +0300

    fixe dates

--- src/components/Sections/Lessons/Collectiontem.js
+++ src/components/Sections/Lessons/Collectiontem.js
@@ -1,7 +1,8 @@
+import clsx from 'clsx';
 import React from 'react';
 import { withNamespaces } from 'react-i18next';
 import { useSelector } from 'react-redux';
-import { List } from 'semantic-ui-react';
+import { Header, List } from 'semantic-ui-react';
 
 import { fromToLocalized } from '../../../helpers/date';
 import { canonicalLink } from '../../../helpers/links';
@@ -25,12 +26,9 @@ const CollectionItem = ({ id, t }) => {
   return (<List.Item key={id} className="media_item">
     <UnitLogoWithDuration duration={cus[0].duration} unitId={cus[0].id} width={144} />
     <div className="media_item__content">
-      <Link to={canonicalLink(c)}>
-        {name}
-        <span className="display-iblock margin-left-8 margin-right-8">{t('values.date', { date: film_date })}</span>
-      </Link>
+      <Header as={Link} to={canonicalLink(c)} content={name} />
       <div>{t(`constants.content-types.${content_type}`)}</div>
-      <div className="description">
+      <div className={clsx('description', { 'is_single': !(description?.length > 1) })}>
         {description.map((d, i) => (<span key={i}>{d}</span>))}
       </div>
     </div>
--- src/components/Sections/Lessons/UnitItem.js
+++ src/components/Sections/Lessons/UnitItem.js
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React from 'react';
 import { withNamespaces } from 'react-i18next';
 import { useSelector } from 'react-redux';
@@ -45,7 +46,7 @@ const UnitItem = ({ id, t }) => {
             </div>
           )
         }
-        <div className="description">
+        <div className={clsx('description', { 'is_single': !(description?.length > 1) })}>
           {description.map((d, i) => (<span key={i}>{d}</span>))}
         </div>
       </div>
