3e08edcf0609437328e643e574c9cc836d43d59b
davgur <davgur.ru@gmail.com>
2022-06-22 15:08:18 +0300

    lint

--- src/components/Sections/Lessons/Collectiontem.js
+++ src/components/Sections/Lessons/Collectiontem.js
@@ -23,16 +23,18 @@ const CollectionItem = ({ id, t }) => {
     description.push(fromToLocalized(start_date, end_date));
   }
 
-  return (<List.Item key={id} className="media_item">
-    <UnitLogoWithDuration duration={cus[0].duration} unitId={cus[0].id} width={144} />
-    <div className="media_item__content">
-      <Header as={Link} to={canonicalLink(c)} content={name} />
-      <div>{t(`constants.content-types.${content_type}`)}</div>
-      <div className={clsx('description', { 'is_single': !(description?.length > 1) })}>
-        {description.map((d, i) => (<span key={i}>{d}</span>))}
+  return (
+    <List.Item key={id} className="media_item">
+      <UnitLogoWithDuration duration={cus[0].duration} unitId={cus[0].id} width={144} />
+      <div className="media_item__content">
+        <Header as={Link} to={canonicalLink(c)} content={name} />
+        <div>{t(`constants.content-types.${content_type}`)}</div>
+        <div className={clsx('description', { 'is_single': !(description?.length > 1) })}>
+          {description.map((d, i) => (<span key={i}>{d}</span>))}
+        </div>
       </div>
-    </div>
-  </List.Item>);
+    </List.Item>
+  );
 };
 
 export default withNamespaces()(CollectionItem);
