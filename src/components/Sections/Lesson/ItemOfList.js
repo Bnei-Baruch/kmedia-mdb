0968a7f287d0c3f01dd9112934bc7a785883827a
davgur <davgur.ru@gmail.com>
2022-06-23 13:05:33 +0300

    fix crash on switch lang

--- src/components/Sections/Lesson/ItemOfList.js
+++ src/components/Sections/Lesson/ItemOfList.js
@@ -16,6 +16,9 @@ const ItemOfList = ({ id, ccu, t }) => {
   const views = useSelector(state => recommended.getViews(id, state.recommended));
   const cu    = useSelector(state => mdb.getDenormContentUnit(state.mdb, id));
 
+  if (!cu)
+    return null;
+
   const link = canonicalLink(cu);
 
   const description = [];
