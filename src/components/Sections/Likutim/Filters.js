8bb342da1674decb867cee122ac93ea3c8056a97
davgur <davgur.ru@gmail.com>
2022-07-03 11:06:37 +0300

    remove not need

--- src/components/Sections/Likutim/Filters.js
+++ src/components/Sections/Likutim/Filters.js
@@ -3,12 +3,10 @@ import React, { useEffect, useState } from 'react';
 import { withNamespaces } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { Container, Header } from 'semantic-ui-react';
-import { CT_VIRTUAL_LESSONS, FN_TOPICS_MULTI, PAGE_NS_LESSONS } from '../../../helpers/consts';
+import { FN_TOPICS_MULTI } from '../../../helpers/consts';
 
 import { selectors as filters } from '../../../redux/modules/filters';
 import { actions, selectors } from '../../../redux/modules/filtersAside';
-import { actions as prepareActions } from '../../../redux/modules/preparePage';
-import { selectors as settings } from '../../../redux/modules/settings';
 import FiltersHydrator from '../../Filters/FiltersHydrator';
 import CuNameFilter from '../../FiltersAside/CuNameFilter';
 import DateFilter from '../../FiltersAside/DateFilter';
@@ -18,7 +16,6 @@ import TagSourceFilter from '../../FiltersAside/TopicsFilter/TagSourceFilter';
 const Filters = ({ namespace, baseParams, t }) => {
   const [isHydrated, setIsHydrated] = useState(false);
 
-  const language     = useSelector(state => settings.getLanguage(state.settings));
   const isReady      = useSelector(state => selectors.isReady(state.filtersAside, namespace));
   const { wip, err } = useSelector(state => selectors.getWipErr(state.filtersAside, namespace));
   const selected     = useSelector(state => filters.getFilters(state.filters, namespace), isEqual);
@@ -26,33 +23,14 @@ const Filters = ({ namespace, baseParams, t }) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(prepareActions.fetchCollections(PAGE_NS_LESSONS, { content_type: [CT_VIRTUAL_LESSONS] }));
-  }, [language, dispatch]);
-
-  useEffect(() => {
     if (!isReady && !wip && !err) {
-      dispatch(actions.fetchStats(namespace, {
-        ...baseParams,
-        with_collections: true,
-        with_persons: true,
-        with_media: true,
-        with_original_languages: true,
-      }, { isPrepare: true, countC: true }));
+      dispatch(actions.fetchStats(namespace, baseParams, { isPrepare: true, }));
     }
   }, [dispatch, isReady, baseParams]);
 
   useEffect(() => {
     if (isHydrated && isReady) {
-      dispatch(actions.fetchStats(namespace, {
-        ...baseParams,
-        with_collections: true,
-        with_persons: true,
-        with_media: true,
-        with_original_languages: true,
-      }, {
-        isPrepare: false,
-        countC: true
-      }));
+      dispatch(actions.fetchStats(namespace, baseParams, { isPrepare: false, }));
     }
   }, [dispatch, isHydrated, isReady, selected, baseParams]);
 
