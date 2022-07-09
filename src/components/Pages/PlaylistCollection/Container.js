b2bac62598d44ee92038756a5b8a438be77b792d
Gabi Wissotzky <wissog@gmail.com>
2022-07-09 06:16:24 +0300

    replace url to include unit to avoid refresh when unit is selected

--- src/components/Pages/PlaylistCollection/Container.js
+++ src/components/Pages/PlaylistCollection/Container.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import React, { useEffect, useMemo, useState, useCallback } from 'react';
 import { withNamespaces } from 'react-i18next';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 
 import { COLLECTION_DAILY_LESSONS, DATE_FORMAT } from '../../../helpers/consts';
 import { canonicalLink } from '../../../helpers/links';
@@ -11,35 +11,39 @@ import playerHelper from '../../../helpers/player';
 import { isEmpty } from '../../../helpers/utils';
 
 import { actions, selectors } from '../../../redux/modules/mdb';
-import { actions as recommended } from '../../../redux/modules/recommended';
+import { selectors as settings } from '../../../redux/modules/settings';
 import WipErr from '../../shared/WipErr/WipErr';
 import Page from './Page';
 
 const PlaylistCollectionContainer = ({ cId, t, cuId }) => {
-  const collection         = useSelector(state => selectors.getDenormCollectionWUnits(state.mdb, cId), shallowEqual);
-  const wipMap             = useSelector(state => selectors.getWip(state.mdb), shallowEqual);
-  const errorMap           = useSelector(state => selectors.getErrors(state.mdb), shallowEqual);
-  const cWindow            = useSelector(state => selectors.getWindow(state.mdb), shallowEqual);
-  const collections        = useSelector(state => cWindow?.data?.map(id => selectors.getDenormCollection(state.mdb, id)).filter(c => !!c), shallowEqual);
-  const location           = useLocation();
+  const collection   = useSelector(state => selectors.getDenormCollectionWUnits(state.mdb, cId), shallowEqual);
+  const wipMap       = useSelector(state => selectors.getWip(state.mdb), shallowEqual);
+  const errorMap     = useSelector(state => selectors.getErrors(state.mdb), shallowEqual);
+  const cWindow      = useSelector(state => selectors.getWindow(state.mdb), shallowEqual);
+  const collections  = useSelector(state => cWindow?.data?.map(id => selectors.getDenormCollection(state.mdb, id)).filter(c => !!c), shallowEqual);
+  const uiLanguage   = useSelector(state => settings.getLanguage(state.settings));
+  const location     = useLocation();
+  const history      = useHistory();
 
   const [nextLink, setNextLink] = useState(null);
   const [prevLink, setPrevLink] = useState(null);
 
   const { cuIDs, content_units, content_type, film_date } = collection || false;
 
+  // select a unit if not given from the link
   cuId = cuId || cuIDs?.[playerHelper.getActivePartFromQuery(location)];
 
-  const dispatch = useDispatch();
+  useEffect(() => {
+    const unit = content_units?.find(u => u.id === cuId);
+    if (unit) {
+      // if a unit not given in the link - replace it here to avoid refresh of the page after user selects
+      const shareUrl = canonicalLink(unit, null, collection);
 
-  const fetchWindow = useCallback(() => {
-    const filmDate = moment.utc(film_date);
-    dispatch(actions.fetchWindow({
-      cId,
-      start_date: filmDate.subtract(5, 'days').format(DATE_FORMAT),
-      end_date: filmDate.add(10, 'days').format(DATE_FORMAT)
-    }));
-  }, [dispatch, film_date, cId]);
+      if (!location.pathname.includes(shareUrl)) {
+        history.replace(`/${uiLanguage}${shareUrl}`)
+      }
+    }
+  }, [collection, content_units, cuId, history, location, uiLanguage])
 
   const createPrevNextLinks = useCallback(curIndex => {
     const prevCollection = curIndex < collections.length - 1 ? collections[curIndex + 1] : null;
