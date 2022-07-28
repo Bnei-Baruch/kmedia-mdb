fb40e79d72532fbb5c174832642f90f5c44a1453
davgur <davgur.ru@gmail.com>
2022-07-28 16:41:28 +0300

    add player

--- src/components/AVPlayer/AvSeekBar.js
+++ src/components/AVPlayer/AvSeekBar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const AvSeekBar = ({ duration, start = 0, end = Infinity }) => {
-  if (!duration) return  null
+  if(!duration) return  null
   const left  = `${100 * start / duration}%`;
   const width = `${100 * (end - start) / duration}%`;
   return (
     <div className="seekbar" tabIndex="0">
-      <div
-        className="is-slice"
-        style={{ left, width }}
-      />
+        <div
+          className="is-slice"
+          style={{ left, width }}
+        />
     </div>
   );
 };
