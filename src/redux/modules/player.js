24561c742606685eeb338513d6a9ca002964a8d1
davgur <davgur.ru@gmail.com>
2022-07-28 16:41:28 +0300

    add player

--- src/redux/modules/player.js
+++ src/redux/modules/player.js
@@ -1,70 +1,69 @@
 import { createAction } from 'redux-actions';
 import { handleActions } from './settings';
-import { JWPLAYER_ID, PLAYER_OVER_MODES } from '../../helpers/consts';
+import { JWPLAYER_ID } from '../../helpers/consts';
 
-const PLAYER_READY  = 'Player/READY';
-const PLAYER_REMOVE = 'Player/REMOVE';
-const PLAYER_PLAY   = 'Player/PLAY';
-const PLAYER_PAUSE  = 'Player/PAUSE';
-const PLAYER_RATE   = 'Player/RATE';
+const PLAYER_READY    = 'Player/READY';
+const PLAYER_ACTIVE   = 'Player/PLAYER_ACTIVE';
+const PLAYER_INACTIVE = 'Player/PLAYER_INACTIVE';
 
-const PLAYER_SET_FILE           = 'Player/SET_FILE';
+const PLAYER_PLAY               = 'Player/PLAY';
+const PLAYER_PAUSE              = 'Player/PAUSE';
+const PLAYER_RATE               = 'Player/RATE';
+const PLAYER_MUTE               = 'Player/MUTE';
+const PLAYER_VOLUME             = 'Player/VOLUME';
 const PLAYER_SET_OVER_MODE      = 'Player/SET_OVER_MODE';
 const PLAYER_CONTINUE_PLAY_FROM = 'Player/CONTINUE_PLAY_FROM';
-const PLAYER_NEW_PLAYLIST_ITEM  = 'Player/NEW_PLAYLIST_ITEM';
 
-const SET_SHARE_START_END = 'Player/SET_SHARE_START_END';
+const PLAYER_SHARE_START = 'Player/PLAYER_SHARE_START';
+const PLAYER_SHARE_STOP  = 'Player/PLAYER_SHARE_STOP';
 
 export const types = {
+  PLAYER_READY,
+
   PLAYER_PLAY,
-  PLAYER_REMOVE
+  PLAYER_MUTE,
+  PLAYER_VOLUME,
 };
 
 // Actions
-const playerReady  = createAction(PLAYER_READY);
-const playerRemove = createAction(PLAYER_REMOVE);
-const setFile      = createAction(PLAYER_SET_FILE);
+const playerReady    = createAction(PLAYER_READY);
+const playerActive   = createAction(PLAYER_ACTIVE);
+const playerInactive = createAction(PLAYER_INACTIVE);
 
-const playerPlay  = createAction(PLAYER_PLAY);
-const playerPause = createAction(PLAYER_PAUSE);
-const playerRate  = createAction(PLAYER_RATE);
+const playerPlay   = createAction(PLAYER_PLAY);
+const playerPause  = createAction(PLAYER_PAUSE);
+const playerRate   = createAction(PLAYER_RATE);
+const playerMute   = createAction(PLAYER_MUTE);
+const playerVolume = createAction(PLAYER_VOLUME);
+const setOverMode  = createAction(PLAYER_SET_OVER_MODE);
+const continuePlay = createAction(PLAYER_CONTINUE_PLAY_FROM);
 
-const setOverMode     = createAction(PLAYER_SET_OVER_MODE);
-const continuePlay    = createAction(PLAYER_CONTINUE_PLAY_FROM);
-const newPlaylistItem = createAction(PLAYER_NEW_PLAYLIST_ITEM);
-
-const setShareStartEnd = createAction(SET_SHARE_START_END);
+const playerShareStart = createAction(PLAYER_SHARE_START);
+const playerShareStop  = createAction(PLAYER_SHARE_STOP);
 
 export const actions = {
-  setFile,
-  setOverMode,
-  setShareStartEnd,
+  playerReady,
+  playerActive,
 
   playerPlay,
   playerPause,
+  playerRate,
+  playerMute,
+  playerVolume,
+  setOverMode,
   continuePlay,
+
+  playerShareStart,
+  playerShareStop
 };
 
 /* Reducer */
-const initialState = {
-  overMode: null,
-  continuePlay: { pos: -1, isPlayed: false },
-  isReady: false,
-  file: null,
-  shareStartEnd: { start: 0, end: Infinity },
-};
+const initialState = { overMode: null, continuePlay: { pos: -1, isPlayed: false } };
 
-const onReady = draft => draft.ready = true;
-
-const onRemove = draft => {
-  draft.continuePlay = initialState.continuePlay;
-  draft.overMode     = null;
-  draft.ready        = false;
-};
-
-const onNewPlaylistItem = (draft, e) => {
-  const p = window.jwplayer(JWPLAYER_ID);
+const onReady = (draft, e) => {
+  console.log('onReady', e);
   if (draft.continuePlay && draft.continuePlay.pos !== -1) {
+    const p = window.jwplayer(JWPLAYER_ID);
     p.stop().seek(draft.continuePlay.pos);
     draft.continuePlay.isPlayed ? p.play() : p.pause();
 
@@ -72,62 +71,84 @@ const onNewPlaylistItem = (draft, e) => {
     draft.overMode     = null;
   }
 };
-const onSetFile         = (draft, payload) => draft.file = payload;
 
-const onPlay = (draft, payload) => draft.played = payload.newstate === 'playing';
+const onInactive = (draft, payload) => {
+  console.log('onInactive', payload);
+  draft.controls = false;
+};
+const onActive   = (draft, payload) => {
+  console.log('onActive', payload);
+  draft.controls = true;
+};
 
-const onPause = draft => draft.played = false;
+const onPlay = (draft, payload) => {
+  console.log('onPlay', payload);
+  draft.played = payload;
+};
+
+const onPause = (draft, payload) => draft.played = false;
 
 const onRate = (draft, payload) => draft.rate = payload.playbackRate;
+
+const onMute = (draft, payload) => {
+  console.log('onMute', payload);
+};
+
+const onVolume = (draft, payload) => {
+  console.log('onVolume', payload);
+};
 
 const onSetOverMode = (draft, payload) => draft.overMode = payload;
 
 const onContinuePlay = (draft, payload = -1) => {
-  const p            = window.jwplayer(JWPLAYER_ID);
-  const pos          = p.getPosition();
-  const isPlayed     = p.getState() === 'playing';
+  const p        = window.jwplayer(JWPLAYER_ID);
+  const pos      = p.getPosition();
+  const isPlayed = p.getState() === 'playing';
+  console.log('onContinuePlay', pos, isPlayed);
   draft.continuePlay = { pos, isPlayed };
 };
 
 export const reducer = handleActions({
   [PLAYER_READY]: onReady,
-  [PLAYER_REMOVE]: onRemove,
-  [PLAYER_SET_FILE]: onSetFile,
+  [PLAYER_ACTIVE]: onActive,
+  [PLAYER_INACTIVE]: onInactive,
 
   [PLAYER_PLAY]: onPlay,
   [PLAYER_PAUSE]: onPause,
   [PLAYER_RATE]: onRate,
+  [PLAYER_MUTE]: onMute,
+  [PLAYER_VOLUME]: onVolume,
 
   [PLAYER_SET_OVER_MODE]: onSetOverMode,
   [PLAYER_CONTINUE_PLAY_FROM]: onContinuePlay,
-  [PLAYER_NEW_PLAYLIST_ITEM]: onNewPlaylistItem,
 
-  [SET_SHARE_START_END]: (draft, payload) => {
-    draft.shareStartEnd = payload;
-  },
+  [PLAYER_SHARE_START]: (draft, payload) => draft.start = payload,
+  [PLAYER_SHARE_STOP]: (draft, payload) => draft.stop = payload,
 }, initialState);
 
-const isReady          = state => state.ready;
-const isPlay           = state => state.played;
-const getFile          = state => state.file;
-const getOverMode      = state => state.overMode || PLAYER_OVER_MODES.none;
-const getRate          = state => state.rate || 1;
-const getShareStartEnd = state => state.shareStartEnd;
+const isControls   = state => state.controls;
+const isAudio      = state => state.isAudio;
+const isPlay       = state => state.played;
+const getOverMode  = state => state.overMode;
+const getRate      = state => state.rate || 1;
+const getStartStop = ({ start, stop }) => ({ start, stop });
 
 export const selectors = {
-  isReady,
+  isControls,
+  isAudio,
   isPlay,
-  getFile,
   getOverMode,
   getRate,
-  getShareStartEnd,
+  getStartStop,
 };
 
 export const PLAYER_ACTIONS_BY_EVENT = {
   'ready': playerReady,
-  'remove': playerRemove,
+  'userInactive': playerInactive,
+  'userActive': playerActive,
   'play': playerPlay,
   'playbackRateChanged': playerRate,
   'pause': playerPause,
-  'playlistItem': newPlaylistItem,
+  'mute': playerMute,
+  'volume': playerVolume
 };
