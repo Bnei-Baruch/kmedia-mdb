3bfe3e7246592f2940b30e3ee16dd9002f496679
Gabi Wissotzky <wissog@gmail.com>
2022-09-12 05:34:22 +0300

    Added Playlist items

--- src/components/shared/UnitLogoWithDuration.js
+++ src/components/shared/UnitLogoWithDuration.js
@@ -6,7 +6,6 @@ import { MY_NAMESPACE_HISTORY } from '../../helpers/consts';
 import { formatDuration } from '../../helpers/utils';
 import UnitLogo from './Logo/UnitLogo';
 import { getProgress } from './ContentItem/helper';
-import { PAGE_SIZE } from './../Sections/Personal/History/Page';
 
 const widthBySize = { 'normal': '140px' };
 
@@ -14,16 +13,12 @@ const UnitLogoWithDuration = ({ unit, size = 'normal', width = 144, ...propz }) 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(myActions.fetch(MY_NAMESPACE_HISTORY, { page_no: 1, page_size: PAGE_SIZE }));
+    dispatch(myActions.fetch(MY_NAMESPACE_HISTORY, { cu_uids: [unit.id] }));
   }, [dispatch, unit.id]);
 
   const historyItems = useSelector(state => my.getList(state.my, MY_NAMESPACE_HISTORY)) || [];
-
-  let playTime;
   const historyUnit = historyItems.find(x => x.content_unit_uid === unit.id);
-  if (historyUnit) {
-    playTime = historyUnit.data.current_time;
-  }
+  const playTime = historyUnit?.data.current_time;
 
   return (
     <div className="with_duration" style={{ minWidth: widthBySize[size] }}>
