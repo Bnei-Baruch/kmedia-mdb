e1b5e10617ff8b14188c17f1195841c4b7cea414
davgur <davgur.ru@gmail.com>
2022-09-19 15:54:20 +0300

    set time min-wight

--- src/components/Player/Settings/AudiVideoBtns.js
+++ src/components/Player/Settings/AudiVideoBtns.js
@@ -1,40 +1,24 @@
 import { Button, Header, Icon } from 'semantic-ui-react';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectors, actions } from '../../../redux/modules/player';
+import { actions } from '../../../redux/modules/player';
 import { selectors as playlistSelectors, actions as playlistActions } from '../../../redux/modules/playlist';
 import { withNamespaces } from 'react-i18next';
 import PlayerLanguages from './PlayerLanguages';
-import { LANGUAGES, PLAYER_OVER_MODES, MT_VIDEO, MT_AUDIO } from '../../../helpers/consts';
+import { LANGUAGES, MT_VIDEO, MT_AUDIO } from '../../../helpers/consts';
+import { updateQuery } from '../../../helpers/url';
+import { useHistory } from 'react-router-dom';
 
-const PLAYER_SPEEDS = [0.75, 1, 1.25, 1.5, 2];
-const Settings      = ({ t }) => {
+const AudioVideoBtns = ({ file, t }) => {
+  const history       = useHistory();
+  const { mediaType } = useSelector(state => playlistSelectors.getInfo(state.playlist));
+  const dispatch      = useDispatch();
 
-  const rate = useSelector(state => selectors.getRate(state.player));
-  const file = useSelector(state => selectors.getFile(state.player));
-  const item = useSelector(state => playlistSelectors.getPlayed(state.playlist));
-  const info = useSelector(state => playlistSelectors.getInfo(state.playlist));
-
-  const dispatch = useDispatch();
-
-  if (!item || !info || !file) return null;
-
-  const { qualityByLang }                = item;
-  const { quality, language, mediaType } = info;
-
-  const handleSetSpeed = x => window.jwplayer().setPlaybackRate(x);
-
-  const handleSetQuality = x => {
-    dispatch(actions.continuePlay());
-    dispatch(playlistActions.setQuality(x));
-  };
 
   const handleSetMediaType = x => {
     dispatch(actions.continuePlay());
     dispatch(playlistActions.setMediaType(x));
   };
-
-  const handleOpenLangs = () => dispatch(actions.setOverMode(PLAYER_OVER_MODES.languages));
 
   return (
     <div className="settings">
@@ -46,7 +30,6 @@ const Settings      = ({ t }) => {
               inverted
               onClick={handleSetMediaType}
               value={MT_VIDEO}
-              key={MT_VIDEO}
               content="Video"
               active={mediaType === MT_VIDEO}
             />
@@ -54,7 +37,6 @@ const Settings      = ({ t }) => {
               inverted
               onClick={handleSetMediaType}
               name={MT_AUDIO}
-              key={MT_AUDIO}
               content="Audio"
               active={mediaType === MT_AUDIO}
             />
@@ -73,7 +55,6 @@ const Settings      = ({ t }) => {
                     content={content}
                     onClick={() => handleSetSpeed(x)}
                     active={x === rate}
-                    key={x}
                   />
                 );
               })
@@ -84,13 +65,12 @@ const Settings      = ({ t }) => {
           <Header size="tiny">Quality</Header>
           <Button.Group size="mini" inverted>
             {
-              qualityByLang[language].map((x, i) => (
+              qualityByLang[language].map(x => (
                 <Button
                   inverted
                   content={x}
                   onClick={() => handleSetQuality(x)}
                   active={x === quality}
-                  key={`${x}_${i}`}
                 />
               ))
             }
@@ -111,4 +91,4 @@ const Settings      = ({ t }) => {
   );
 };
 
-export default withNamespaces()(Settings);
+export default withNamespaces()(AudioVideoBtns);
