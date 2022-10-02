3b54f8a6beac39c9773cff25a116a86b6e8ef321
davgur <davgur.ru@gmail.com>
2022-10-02 12:20:14 +0300

    single media

--- src/sagas/playlist.js
+++ src/sagas/playlist.js
@@ -1,19 +1,13 @@
 import { takeEvery, select, put, call } from 'redux-saga/effects';
-import i18n from 'i18next';
 
-import { selectors as authSelectors } from '../redux/modules/auth';
+import playerHelper from '../helpers/player';
 import { selectors as settings } from '../redux/modules/settings';
-import { selectors as my, selectors } from '../redux/modules/my';
 import { types, actions } from '../redux/modules/playlist';
 import { selectors as mdb } from '../redux/modules/mdb';
-import { MY_NAMESPACE_PLAYLISTS, MY_NAMESPACE_REACTIONS, IsCollectionContentType } from '../helpers/consts';
-import { canonicalCollection } from '../helpers/utils';
-import { getMyItemKey } from '../helpers/my';
-import playerHelper from '../helpers/player';
-import { assetUrl } from '../helpers/Api';
 import { fetchCollection, fetchUnit, fetchUnitsByIDs } from './mdb';
 import { fetchViewsByUIDs } from './recommended';
-import { fetchOne, fetch as fetchMy } from './my';
+import { canonicalCollection } from '../helpers/utils';
+import { assetUrl } from '../helpers/Api';
 
 function* build(action) {
   const { cId } = action.payload;
@@ -21,11 +15,11 @@ function* build(action) {
 
   const { location } = yield select(state => state.router);
 
-  const fetched = yield select(state => mdb.getFullCollectionFetched(state.mdb));
-  if (!fetched?.[cId]) {
-    yield call(fetchCollection, { payload: cId });
-  }
   let c = yield select(state => mdb.getDenormCollection(state.mdb, cId));
+  if (!c) {
+    yield put(fetchCollection({ id: cId }));
+    c = yield select(state => mdb.getDenormCollection(state.mdb, cId));
+  }
 
   if (!cuId) {
     cuId = c.cuIDs?.[playerHelper.getActivePartFromQuery(location)];
@@ -75,68 +69,11 @@ function* singleMediaBuild(action) {
 
   const mediaType   = playerHelper.getMediaTypeFromQuery(location);
   const language    = playerHelper.getLanguageFromQuery(location, siteLang || contentLang);
-  const preImageUrl = !!c ? assetUrl(`logos/collections/${c.id}.jpg`) : null;
+  const preImageUrl = c ? assetUrl(`logos/collections/${c.id}.jpg`) : null;
   const item        = playerHelper.playableItem(cu, preImageUrl);
 
   yield put(actions.buildSuccess({ items: [item], language, mediaType, cuId, cId: c.id, isSingleMedia: true }));
   yield fetchViewsByUIDs([cuId]);
-}
-
-function* myPlaylistBuild(action) {
-  const token = yield select(state => authSelectors.getToken(state.auth));
-  if (!token) return;
-
-  const { pId } = action.payload;
-
-  const { items: data, name } = (pId === MY_NAMESPACE_REACTIONS) ?
-    yield fetchMyReactions() : yield fetchMyPlaylist(pId);
-
-  const content_units = yield select(state => data?.map(x => mdb.getDenormContentUnit(state.mdb, x.content_unit_uid)).filter(x => !!x)) || [];
-
-  const siteLang    = yield select(state => settings.getLanguage(state.settings));
-  const contentLang = yield select(state => settings.getContentLanguage(state.settings));
-
-  const { location } = yield select(state => state.router);
-  const mediaType    = playerHelper.getMediaTypeFromQuery(location);
-  const language     = playerHelper.getLanguageFromQuery(location, siteLang || contentLang);
-  const ap           = playerHelper.getActivePartFromQuery(location);
-  const items        = content_units.map(cu => playerHelper.playableItem(cu));
-  const cuId         = items[ap]?.id || items[0].id;
-  const baseLink     = `/${siteLang}/${MY_NAMESPACE_PLAYLISTS}/${pId}`;
-
-  yield put(actions.buildSuccess({ items, cuId, name, language, mediaType, pId, baseLink }));
-
-  const cuUIDs = content_units.map(c => c.id);
-  yield fetchViewsByUIDs(cuUIDs);
-}
-
-function* fetchMyPlaylist(id) {
-  const { key }    = getMyItemKey(MY_NAMESPACE_PLAYLISTS, { id });
-  let playlistData = yield select(state => my.getItemByKey(state.my, MY_NAMESPACE_PLAYLISTS, key));
-
-  if (!playlistData) {
-    yield call(fetchOne, { payload: { namespace: MY_NAMESPACE_PLAYLISTS, id } });
-    playlistData = yield select(state => my.getItemByKey(state.my, MY_NAMESPACE_PLAYLISTS, key));
-  }
-  return playlistData;
-}
-
-function* fetchMyReactions() {
-  yield call(fetchMy, {
-    payload: {
-      namespace: MY_NAMESPACE_REACTIONS,
-      params: { page_no: 1, page_size: 100, with_files: true }
-    }
-  });
-  const data = yield select(state => selectors.getList(state.my, MY_NAMESPACE_REACTIONS));
-  const id   = data.filter(x => !IsCollectionContentType(x.subject_type))
-    .map(x => x.subject_uid);
-
-  yield call(fetchUnitsByIDs, { payload: { id, with_files: true } });
-  const name  = i18n.t('personal.reactions');
-  const items = id.map(x => ({ content_unit_uid: x }));
-  return { items, name };
-
 }
 
 function* watchBuild() {
@@ -147,8 +84,4 @@ function* watchSingleMediaBuild() {
   yield takeEvery(types.SINGLE_MEDIA_BUILD, singleMediaBuild);
 }
 
-function* watchMyPlaylistBuild() {
-  yield takeEvery(types.MY_PLAYLIST_BUILD, myPlaylistBuild);
-}
-
-export const sagas = [watchBuild, watchSingleMediaBuild, watchMyPlaylistBuild];
+export const sagas = [watchBuild, watchSingleMediaBuild];
