7884c63a70ad74e9dc40793de5f71f38c77e367d
davgur <davgur.ru@gmail.com>
2022-10-20 14:13:23 +0300

    add background play pause

---
+++ src/components/Player/Controls/PlayPauseBg.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { selectors } from '../../../redux/modules/player';
+import { JWPLAYER_ID } from '../../../helpers/consts';
+import { stopBubbling } from '../../../helpers/utils';
+
+const PlayPauseBg = () => {
+  const isPlay      = useSelector(state => selectors.isPlay(state.player));
+  const handleClick = e => {
+    const p = window.jwplayer(JWPLAYER_ID);
+    isPlay ? p.pause() : p.play();
+    stopBubbling(e);
+  };
+  return (
+    <div className="controls__bg_play" onClick={handleClick}></div>
+  );
+};
+
+export default PlayPauseBg;
