0a729af796b0b9bcc163225ae5f66afc1ba73e7c
davgur <davgur.ru@gmail.com>
2022-10-31 10:35:35 +0200

    Fix css (#959)
    * restore css classes
    
    * add links to icon

--- src/components/Sections/Lessons/DailyLessonItem.js
+++ src/components/Sections/Lessons/DailyLessonItem.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import { withNamespaces } from 'react-i18next';
-import { useSelector } from 'react-redux';
+import { useSelector, shallowEqual } from 'react-redux';
 import { Header, List } from 'semantic-ui-react';
-import { canonicalLink, getCuByCcuSkipPreparation } from '../../../helpers/links';
+import { canonicalLink } from '../../../helpers/links';
 
 import { selectors as mdb } from '../../../redux/modules/mdb';
+import { selectors as my } from '../../../redux/modules/my';
 import Link from '../../Language/MultiLanguageLink';
-import UnitLogoWithDuration from '../../shared/UnitLogoWithDuration';
+import UnitLogoWithDuration, { getLogoUnit } from '../../shared/UnitLogoWithDuration';
+import { MY_NAMESPACE_HISTORY } from '../../../helpers/consts';
 
 const DailyLessonItem = ({ id, t }) => {
-  const ccu = useSelector(state => mdb.getDenormCollection(state.mdb, id));
+  const c            = useSelector(state => mdb.getDenormCollection(state.mdb, id));
+  const historyItems = useSelector(state => my.getList(state.my, MY_NAMESPACE_HISTORY), shallowEqual) || [];
 
-  const { number, film_date, content_units = [] } = ccu || {};
+  const { number, film_date, content_units = [] } = c || {};
 
-  if (!ccu || content_units.length === 0) return null;
-  const logoUnit = content_units.find(x => x.id === getCuByCcuSkipPreparation(ccu));
+  if (!c || content_units.length === 0) return null;
+
+  const logoUnit = getLogoUnit(content_units, historyItems);
+  const link     = canonicalLink(logoUnit);
+
   return (
     <List.Item key={id} className="media_item daily_lesson">
-      <div style={{ minWidth: '140px' }}>
+      <Link to={link} style={{ minWidth: '140px' }}>
         <UnitLogoWithDuration unit={logoUnit} />
-      </div>
+      </Link>
       <div className="media_item__content">
-        <Header as={Link} to={canonicalLink(ccu)}>
+        <Header as={Link} to={link}>
           {t('constants.content-types.DAILY_LESSON')}
           <small>
             <span className="display-iblock margin-left-8 margin-right-8">{t('values.date', { date: film_date })}</span>
