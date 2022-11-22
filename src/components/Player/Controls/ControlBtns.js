8d7fe8612881efebaf05ff55f169ef888c945911
davgur <davgur.ru@gmail.com>
2022-11-22 15:08:45 +0200

    add mobile version

--- src/components/Player/Controls/ControlBtns.js
+++ src/components/Player/Controls/ControlBtns.js
@@ -1,43 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import fscreen from 'fscreen';
 import { withNamespaces } from 'react-i18next';
 import { Popup, Icon } from 'semantic-ui-react';
 
-import { actions, selectors as player } from '../../../redux/modules/player';
+import { actions, selectors } from '../../../redux/modules/player';
 import { PLAYER_OVER_MODES } from '../../../helpers/consts';
 import { stopBubbling } from '../../../helpers/utils';
 
 export const FullscreenBtn = withNamespaces()(({ openOnFull, t }) => {
-  const [on, setOn] = useState(false);
+  const dispatch     = useDispatch();
+  const isFullScreen = useSelector(state => selectors.isFullScreen(state.player));
 
   const handleFullScreen = () => {
-    if (!on) {
+    if (!isFullScreen) {
       openOnFull();
-      setOn(true);
+      dispatch(actions.setFullScreen(true));
     } else {
       fscreen.exitFullscreen();
-      setOn(false);
+      dispatch(actions.setFullScreen(false));
     }
   };
   return (
     <Popup content={t('player.controls.fullscreen')} inverted size="mini" position="top right" trigger={
       <div className="controls__fullscreen" onClick={handleFullScreen}>
-        <Icon fitted name={on ? 'compress' : 'expand'} />
+        <Icon fitted name={isFullScreen ? 'compress' : 'expand'} />
       </div>
     } />
   );
 });
 
 export const ShareBtn = withNamespaces()(({ t }) => {
-  const mode = useSelector(state => player.getOverMode(state.player));
-
+  const mode     = useSelector(state => selectors.getOverMode(state.player));
   const dispatch = useDispatch();
 
   const handleOpen = e => {
+    stopBubbling(e);
     const newMode = (mode === PLAYER_OVER_MODES.share) ? PLAYER_OVER_MODES.none : PLAYER_OVER_MODES.share;
     dispatch(actions.setOverMode(newMode));
-    stopBubbling(e);
   };
 
   return (
@@ -50,14 +50,14 @@ export const ShareBtn = withNamespaces()(({ t }) => {
 });
 
 export const SettingsBtn = withNamespaces()(({ t }) => {
-  const mode = useSelector(state => player.getOverMode(state.player));
+  const mode = useSelector(state => selectors.getOverMode(state.player));
 
   const dispatch = useDispatch();
 
   const handleOpen = e => {
+    stopBubbling(e);
     const newMode = (mode === PLAYER_OVER_MODES.settings) ? PLAYER_OVER_MODES.none : PLAYER_OVER_MODES.settings;
     dispatch(actions.setOverMode(newMode));
-    stopBubbling(e);
   };
 
   return (
