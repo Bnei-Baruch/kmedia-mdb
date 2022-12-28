9f6cf709c251288cfa082d4167a98c4e997639e1
GW125 <32199248+wissog@users.noreply.github.com>
2022-12-28 14:24:49 +0200

    separated file AudioPlayer into a component and embedded it in Sources and Library (#967)
    * separated file AudioPlayer into a component and embedded it in Sources and Library
    
    * css & code improve
    
    * Audio alignment fix
    
    * use button for audio
    
    * no multiple sources removed dropdown
    
    * sources audio design fixes
    
    * transcription margins fix
    
    * css lint

---
+++ src/components/shared/AudioPlayer.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import { withNamespaces } from 'react-i18next';
+
+import PlayAudioIcon from '../../images/icons/PlayAudio';
+import { physicalFile } from '../../helpers/utils';
+import { Button } from 'semantic-ui-react';
+
+
+const AudioPlayer = ({ mp3, t }) => {
+  const [playing, setPlaying]       = useState(false);
+  const [audioInfo, setAudioInfo]   = useState(null);
+
+  useEffect(() => {
+    const clearAudioInfo = () => {
+      if (audioInfo !== null) {
+        setAudioInfo(null);
+        setPlaying(false);
+      }
+    };
+
+    if (!mp3) {
+      clearAudioInfo();
+    } else {
+      const newAudioInfo = { url: physicalFile(mp3, true), name: mp3.name };
+      if (audioInfo?.url !== newAudioInfo.url) {
+        setAudioInfo(newAudioInfo);
+        setPlaying(false);
+      }
+    }
+  }, [audioInfo, mp3]);
+
+  return audioInfo &&
+    <span className="library-audio-player">
+      {playing
+        ? <audio controls src={audioInfo?.url} autoPlay={true} preload="metadata" />
+        :
+        <Button compact onClick={() => setPlaying(true)}>
+          { t('sources-library.play-audio-file') }
+          <PlayAudioIcon className="playAudioIcon" />
+        </Button>
+      }
+    </span>;
+}
+
+export default withNamespaces()(AudioPlayer);
