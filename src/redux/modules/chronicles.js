583a4ac6a2260bc75ba8ce980e1ccb8bae8180b6
davgur <davgur.ru@gmail.com>
2023-01-08 10:11:01 +0200

    remove not used

--- src/redux/modules/chronicles.js
+++ src/redux/modules/chronicles.js
@@ -38,7 +38,7 @@ export const reducer = handleActions({
 
   [playerTypes.PLAYER_PLAY]: draft => ({ ...draft, event: 'player-play' }),
   [playerTypes.PLAYER_PAUSE]: draft => ({ ...draft, event: 'player-stop' }),
-  [playerTypes.PLAYER_DESTROY_PLUGIN]: draft => ({ ...draft, event: 'player-stop' }),
+  [playerTypes.PLAYER_REMOVE]: draft => ({ ...draft, event: 'player-stop' }),
   [playerTypes.PLAYER_MUTE_UNMUTE]: draft => ({ ...draft, event: 'mute-unmute' }),
 
 }, initialState);
