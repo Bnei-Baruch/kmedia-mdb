e0c1e7c1c2958f91e15469aca8edc62922335d17
davgur <davgur.ru@gmail.com>
2023-02-19 16:30:57 +0200

    Fix player bugs (#987)
    * fix start from history
    
    * fix start from history
    
    * mark fetched history even not login
    
    * fix muted

--- src/redux/modules/player.js
+++ src/redux/modules/player.js
@@ -70,7 +70,6 @@ const initialState = {
   shareStartEnd: { start: 0, end: Infinity },
   isFullScreen: false,
   wip: false,
-  isMuted: localStorage.getItem(LOCALSTORAGE_MUTE) === 'true'
 };
 
 const onReady = draft => {
