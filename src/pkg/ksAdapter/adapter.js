9f7605ba85ab5c7d203c3c4370887c8502151027
davgur <davgur.ru@gmail.com>
2023-05-24 11:11:42 +0300

    KC fixes

--- src/pkg/ksAdapter/adapter.js
+++ src/pkg/ksAdapter/adapter.js
@@ -11,7 +11,9 @@ export const KC_UPDATE_USER  = 'KC_UPDATE_USER';
 export const KC_UPDATE_TOKEN = 'KC_UPDATE_TOKEN';
 
 export const login = () => {
-  keycloak.login({ redirectUri: `${window.location.href}?authorised=true` })
+  const url = new URL(window.location.href);
+  url.searchParams.set('authorised', 'true');
+  keycloak.login({ redirectUri: url.href })
     .then(r => {
       updateUser(r);
     })
