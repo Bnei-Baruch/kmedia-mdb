c29e554f08d9414a26077389bbe747dc7308b767
davgur <davgur.ru@gmail.com>
2023-06-18 14:23:20 +0300

    if unit have prev collection - use it

--- src/components/Pages/WithPlayer/BuildPlaylistByUnit.js
+++ src/components/Pages/WithPlayer/BuildPlaylistByUnit.js
@@ -11,11 +11,13 @@ const BuildPlaylistByUnit = ({ cts }) => {
 
   const { id: prevCuId, cId: prevCId, wip } = useSelector(state => selectors.getInfo(state.playlist));
 
-  const unit        = useSelector(state => mdb.getDenormContentUnit(state.mdb, id));
-  const fetched     = useSelector(state => mdb.getFullUnitFetched(state.mdb))[id];
-  const wipCU       = useSelector(state => mdb.getWip(state.mdb).units)[id];
-  const errCU       = useSelector(state => mdb.getErrors(state.mdb).units)[id];
-  const { id: cId } = (unit && Object.values(unit.collections).find(c => cts.includes(c.content_type))) || false;
+  const unit    = useSelector(state => mdb.getDenormContentUnit(state.mdb, id));
+  const fetched = useSelector(state => mdb.getFullUnitFetched(state.mdb))[id];
+  const wipCU   = useSelector(state => mdb.getWip(state.mdb).units)[id];
+  const errCU   = useSelector(state => mdb.getErrors(state.mdb).units)[id];
+
+  const cs          = unit && Object.values(unit.collections) || [];
+  const { id: cId } = cs.find(c => c.id === prevCId) || cs.find(c => cts.includes(c.content_type));
 
   const dispatch = useDispatch();
 
