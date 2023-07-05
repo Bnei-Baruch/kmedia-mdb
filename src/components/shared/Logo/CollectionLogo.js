723bff474ea036ec5e2a59247b59549cbf2a8bef
davgur <davgur.ru@gmail.com>
2023-07-05 13:03:54 +0300

    use prev as fallback

--- src/components/shared/Logo/CollectionLogo.js
+++ src/components/shared/Logo/CollectionLogo.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Requests, cLogoUrl } from '../../../helpers/Api';
+import { Requests, cLogoUrl, assetUrl } from '../../../helpers/Api';
 import FallbackImage from '../FallbackImage';
 
 const CollectionLogo = props => {
   const { collectionId = null, ...rest } = props;
 
-  const src = Requests.imaginary('thumbnail', {
-    url: cLogoUrl(`${collectionId}.jpg`),
-    width: 250,
-    stripmeta: true
-  });
+  const srcParams = { url: cLogoUrl(`${collectionId}.jpg`), width: 250, stripmeta: true };
+  const src       = Requests.imaginary('thumbnail', srcParams);
+  srcParams.url   = assetUrl(`logos/collections/${collectionId}.jpg`);
+  const fallbacks = [Requests.imaginary('thumbnail', srcParams)];
 
   return (
     <FallbackImage
@@ -19,6 +18,7 @@ const CollectionLogo = props => {
       shape="rounded"
       className="collection-logo ui fluid image"
       src={src}
+      fallbackImage={fallbacks}
       {...rest}
     />
   );
