132f10f6b68bc67ae76265277a603674ceb46519
davgur <davgur.ru@gmail.com>
2023-07-09 17:21:54 +0300

    Small fixes (#1037)
    * show collection image
    
    * show collection image

--- src/components/shared/Logo/UnitLogo.js
+++ src/components/shared/Logo/UnitLogo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 
-import { assetUrl, Requests } from '../../../helpers/Api';
+import { assetUrl, Requests, cLogoUrl } from '../../../helpers/Api';
 import FallbackImage from '../FallbackImage';
 
 import { selectors as sources } from '../../../redux/modules/sources';
@@ -15,40 +15,37 @@ const portraits = { bs: portraitBS, rb: portraitRB, ml: portraitML };
 
 const makeImaginary = (cuId, cId, width, height) => {
   if (cuId) {
-    return Requests.imaginary('thumbnail', {
-      url: assetUrl(`api/thumbnail/${cuId}`),
-      width,
-      stripmeta: true,
-    });
+    const src = Requests.imaginary('thumbnail', { url: assetUrl(`api/thumbnail/${cuId}`), width, stripmeta: true, });
+    return { src, fallbacks: [] };
   }
 
   if (cId) {
-    return Requests.imaginary('thumbnail', {
-      url: assetUrl(`logos/collections/${cId}.jpg`),
-      width,
-      height,
-      stripmeta: true,
-    });
+    const srcParams = { url: cLogoUrl(`${cId}.jpg`), width, height, stripmeta: true };
+    const src       = Requests.imaginary('thumbnail', srcParams);
+    srcParams.url   = assetUrl(`logos/collections/${cId}.jpg`);
+    const fallbacks = [Requests.imaginary('thumbnail', srcParams)];
+
+    return { src, fallbacks };
   }
 
-  return '/fake_image_url_for_show_fallback';
+  return { src: '/fake_image_url_for_show_fallback', fallbacks: [] };
 };
 
 const UnitLogo = props => {
   const {
-    unitId       = null,
-    collectionId = null,
-    sourceId     = null,
-    width        = 120,
-    height,
-    className    = '',
-    fallbackImg  = 'default',
-    ...rest
-  } = props;
+          unitId       = null,
+          collectionId = null,
+          sourceId     = null,
+          width        = 120,
+          height,
+          className    = '',
+          fallbackImg  = 'default',
+          ...rest
+        } = props;
 
   const sourcePath = useSelector(state => sources.getPathByID(state.sources)(sourceId));
 
-  const src = makeImaginary(unitId, collectionId, width, height);
+  const { src, fallbacks } = makeImaginary(unitId, collectionId, width, height);
 
   const fallback  = sourceId !== null && sourcePath && sourcePath.length ? portraits[sourcePath[0].id] : fallbackImg;
   const force16x9 = sourceId !== null ? 'true' : undefined;
@@ -60,7 +57,7 @@ const UnitLogo = props => {
       width={width}
       height={height}
       className={`unit-logo ${className} ui image`}
-      fallbackImage={[fallback]}
+      fallbackImage={[...fallbacks, fallback]}
       force16x9={force16x9}
     />
   );
