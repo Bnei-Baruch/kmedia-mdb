f626cc7ecc261bae1354ea7ecc278baebee18234
davgur <davgur.ru@gmail.com>
2023-07-31 10:11:26 +0300

    add CT filter likutim

--- src/components/FiltersAside/ContentTypeFilter/ContentType.js
+++ src/components/FiltersAside/ContentTypeFilter/ContentType.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectors } from '../../../redux/modules/filtersAside';
-import { withTranslation } from 'react-i18next';
 import {
   CT_LESSONS_SERIES,
   CT_SOURCE,
@@ -9,22 +8,24 @@ import {
   UNIT_EVENTS_TYPE,
   UNIT_LESSONS_TYPE,
   UNIT_PROGRAMS_TYPE,
-  UNIT_PUBLICATIONS_TYPE
+  UNIT_PUBLICATIONS_TYPE,
+  CT_LIKUTIM
 } from '../../../helpers/consts';
 import ContentTypeItem from './ContentTypeItem';
 import FilterHeader from '../FilterHeader';
 import ContentTypeItemGroup from './ContentTypeItemGroup';
 
 const groupByName = {
-  lessons: { cts: [CT_LESSONS_SERIES, ...UNIT_LESSONS_TYPE], key: 'lessons', order: 2 },
-  events: { cts: UNIT_EVENTS_TYPE, key: 'events', order: 3 },
-  programs: { cts: UNIT_PROGRAMS_TYPE, key: 'programs', order: 4 },
-  publications: { cts: UNIT_PUBLICATIONS_TYPE, key: 'publications', order: 5 },
   sources: { cts: [CT_SOURCE], key: 'sources', order: 1 },
+  likutim: { cts: [CT_LIKUTIM], key: 'likutim', order: 2 },
+  lessons: { cts: [CT_LESSONS_SERIES, ...UNIT_LESSONS_TYPE], key: 'lessons', order: 3 },
+  events: { cts: UNIT_EVENTS_TYPE, key: 'events', order: 4 },
+  programs: { cts: UNIT_PROGRAMS_TYPE, key: 'programs', order: 5 },
+  publications: { cts: UNIT_PUBLICATIONS_TYPE, key: 'publications', order: 6 },
 };
-const cts = Object.values(groupByName).flatMap(x => x.cts);
+const cts         = Object.values(groupByName).flatMap(x => x.cts);
 
-const ContentType = ({ namespace, t }) => {
+const ContentType = ({ namespace }) => {
   let items = useSelector(state => selectors.getTree(state.filtersAside, namespace, FN_CONTENT_TYPE));
   items     = items.filter(ct => !cts.includes(ct));
 
@@ -45,4 +46,4 @@ const ContentType = ({ namespace, t }) => {
   );
 };
 
-export default withTranslation()(ContentType);
+export default ContentType;
