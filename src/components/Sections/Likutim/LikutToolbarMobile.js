7594781220f9af0af11a41bc217add80676e9bc4
davgur <davgur.ru@gmail.com>
2024-01-18 09:49:18 +0200

    likut toolbar

--- src/components/Sections/Likutim/LikutToolbarMobile.js
+++ src/components/Sections/Likutim/LikutToolbarMobile.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import LanguageTextBtn from '../../Pages/WithText/Buttons/LanguageTextBtn';
 import TextSettings from '../../Pages/WithText/Buttons/TextSettings/TextSettings';
-import BookmarkBtn from '../../Pages/WithText/Buttons/BookmarkBtn';
 import SearchOnPageBtn from '../../Pages/WithText/Buttons/SearchOnPageBtn';
 import DownloadTextBtn from '../../Pages/WithText/Buttons/DownloadTextBtn';
 import ShareTextBtn from '../../Pages/WithText/Buttons/ShareTextBtn';
@@ -12,18 +11,18 @@ const LikutToolbarMobile = () => (
     <div className="text_toolbar__buttons">
       <LanguageTextBtn />
       <TextSettings />
+      <div className="divider" />
+      <ShareTextBtn />
+      <div className="divider" />
       <SearchOnPageBtn />
       <Dropdown
         item
         icon={null}
-        trigger={(<span className="material-symbols-outlined">more_vert</span>)}>
+        trigger={(<span className="material-symbols-outlined">more_vert</span>)}
+        pointing="top right"
+        className="text_toolbar__dropdown"
+      >
         <Dropdown.Menu>
-          <Dropdown.Item>
-            <BookmarkBtn />
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <ShareTextBtn />
-          </Dropdown.Item>
           <Dropdown.Item>
             <DownloadTextBtn />
           </Dropdown.Item>
--- src/components/Sections/Source/SourceToolbarMobile.js
+++ src/components/Sections/Source/SourceToolbarMobile.js
@@ -2,7 +2,6 @@ import React from 'react';
 import TocToggleBtn from './TOC/TocToggleBtn';
 import LanguageTextBtn from '../../Pages/WithText/Buttons/LanguageTextBtn';
 import TextSettings from '../../Pages/WithText/Buttons/TextSettings/TextSettings';
-import BookmarkBtn from '../../Pages/WithText/Buttons/BookmarkBtn';
 import SearchOnPageBtn from '../../Pages/WithText/Buttons/SearchOnPageBtn';
 import DownloadTextBtn from '../../Pages/WithText/Buttons/DownloadTextBtn';
 import ShareTextBtn from '../../Pages/WithText/Buttons/ShareTextBtn';
