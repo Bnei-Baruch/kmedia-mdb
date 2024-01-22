8ccfe23338ed69c042f49be22f1152171f377cfd
davgur <davgur.ru@gmail.com>
2024-01-22 14:03:10 +0200

    use modal for share on mobile

---
+++ src/components/Pages/WithText/Buttons/ShareTextModalBtn.js
@@ -0,0 +1,97 @@
+import React, { useContext, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import { Button, Message, Popup, Modal, } from 'semantic-ui-react';
+
+import ShareBar from '../../../Share/ShareBar';
+import { DeviceInfoContext } from '../../../../helpers/app-contexts';
+import useStateWithCallback from '../../../../helpers/use-state-with-callback';
+import { useSelector } from 'react-redux';
+import { textPageGetUrlInfoSelector } from '../../../../redux/selectors';
+
+const POPOVER_CONFIRMATION_TIMEOUT = 2500;
+
+const ShareTextModalBtn = () => {
+  const { t }                         = useTranslation();
+  const { isMobileDevice }            = useContext(DeviceInfoContext);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isCopyOpen, setIsCopyOpen]   = useStateWithCallback(false, isCopyOpen => {
+    if (isCopyOpen) {
+      timeout = setTimeout(() => setIsCopyOpen(false), POPOVER_CONFIRMATION_TIMEOUT);
+    }
+  });
+
+  const { select, search, url } = useSelector(textPageGetUrlInfoSelector);
+
+  const properties = { ...select, ...search };
+
+  const [urlWithParams, setUrlWithParams] = useState(url);
+
+  let timeout = undefined;
+
+  const handleCopied = () => {
+    clearPopupTimeout();
+    setIsCopyOpen(true);
+  };
+
+  const clearPopupTimeout = () => {
+    if (timeout) {
+      clearTimeout(timeout);
+      timeout = null;
+    }
+  };
+
+  const buttonSize = isMobileDevice ? 'tiny' : 'small';
+
+  return (
+    <Modal // share bar popup
+      className="share-bar"
+      on="click"
+      flowing
+      hideOnScroll
+      trigger={
+        <Button
+          icon={<span className="material-symbols-outlined">share</span>}
+        />
+      }
+      open={isPopupOpen}
+      onClose={() => setIsPopupOpen(false)}
+      onOpen={() => {
+        const _url = new URL(url);
+        for (const key in properties) {
+          _url.searchParams.set(key, properties[key]);
+        }
+
+        setUrlWithParams(_url.toString());
+        setIsPopupOpen(true);
+      }}
+    >
+      <Modal.Content>
+        <ShareBar
+          url={urlWithParams}
+          buttonSize={buttonSize}
+          messageTitle={t('sources-library.share-title')}
+        />
+        <Message
+          content={urlWithParams}
+          size="mini"
+          className="share-bar__message text_ellipsis"
+        />
+        <Popup // link was copied message popup
+          open={isCopyOpen}
+          content={t('messages.link-copied-to-clipboard')}
+          position={`bottom left`}
+          trigger={
+            (
+              <CopyToClipboard text={urlWithParams} onCopy={handleCopied}>
+                <Button compact size="small" content={t('buttons.copy')} />
+              </CopyToClipboard>
+            )
+          }
+        />
+      </Modal.Content>
+    </Modal>
+  );
+};
+
+export default ShareTextModalBtn;
--- src/components/Sections/Source/SourceToolbarMobile.js
+++ src/components/Sections/Source/SourceToolbarMobile.js
@@ -9,6 +9,7 @@ import DownloadTextBtn from '../../Pages/WithText/Buttons/DownloadTextBtn';
 import ShareTextBtn from '../../Pages/WithText/Buttons/ShareTextBtn';
 import LinkToLessonsBtn from '../../Pages/WithText/Buttons/LinkToLessonsBtn';
 import BookmarkBtn from '../../Pages/WithText/Buttons/BookmarkBtn';
+import ShareTextModalBtn from '../../Pages/WithText/Buttons/ShareTextModalBtn';
 
 const SourceToolbarMobile = () => (
   <div className="text_toolbar is-mobile">
@@ -35,7 +36,7 @@ const SourceToolbarMobile = () => (
             <DownloadTextBtn />
           </Dropdown.Item>
           <Dropdown.Item>
-            <ShareTextBtn />
+            <ShareTextModalBtn />
           </Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
