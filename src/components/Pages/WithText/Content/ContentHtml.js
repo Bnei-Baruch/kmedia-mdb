f0359f36f472f8ebd87a0b4f219f89dd2d4a58cf
davgur <davgur.ru@gmail.com>
2024-02-18 12:49:31 +0200

    scroll to selected

--- src/components/Pages/WithText/Content/ContentHtml.js
+++ src/components/Pages/WithText/Content/ContentHtml.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
@@ -8,8 +8,11 @@ import { getLanguageDirection } from '../../../../helpers/i18n-utils';
 import { useTextContent } from './useTextContent';
 import { textPageGetFileSelector, assetsGetDoc2htmlByIdSelector } from '../../../../redux/selectors';
 import { useNotes } from '../Notes/useNotes';
+import { SCROLL_SEARCH_ID } from '../../../../helpers/consts';
 
 const ContentHtml = ({ labels = [] }) => {
+  const ref = useRef();
+
   const { language, id } = useSelector(textPageGetFileSelector);
   const getDoc2htmlById  = useSelector(assetsGetDoc2htmlByIdSelector);
 
@@ -27,6 +30,12 @@ const ContentHtml = ({ labels = [] }) => {
       [...labels, ...notes]);
   }, [srchstart, srchend, getDoc2htmlById, id, labels, notes]);
 
+  const _needScroll = !!__html && !!srchstart && !!srchend;
+  useEffect(() => {
+    const el = document.getElementById(SCROLL_SEARCH_ID);
+    el && el.scrollIntoView({ behavior: 'smooth' });
+  }, [_needScroll, ref.current]);
+
   const wipErr = useTextContent();
 
   if (wipErr) return wipErr;
@@ -35,7 +44,7 @@ const ContentHtml = ({ labels = [] }) => {
 
   return (
     <div className={`${language}_styles`} dir={dir}>
-      <div id={DOM_ROOT_ID} dangerouslySetInnerHTML={{ __html }} />
+      <div id={DOM_ROOT_ID} dangerouslySetInnerHTML={{ __html }} ref={ref} />
     </div>
   );
 };
