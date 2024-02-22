208697be63fcffeba2b140391679d3d17b3ecbba
davgur <davgur.ru@gmail.com>
2024-02-22 14:39:45 +0200

    lint

--- src/components/Pages/WithText/Notes/NotesByPos.js
+++ src/components/Pages/WithText/Notes/NotesByPos.js
@@ -26,9 +26,9 @@ const NotesByPos = ({ pos, ids }) => {
   useClickOutside(handleClose, [ref]);
 
   const notes = useMemo(() => ids
-      .map(id => byId[id])
-      .map(n => ({ type: 'note', ...n }))
-    , [byId, ids]);
+    .map(id => byId[id])
+    .map(n => ({ type: 'note', ...n }))
+  , [byId, ids]);
 
   useEffect(() => {
     setOpen(expandAll);
