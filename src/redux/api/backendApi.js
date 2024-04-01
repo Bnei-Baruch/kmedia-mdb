565b25e6eb477be3990d433770031f1c5b7cd9cc
Gregory Shilin <gshilin@gmail.com>
2024-04-01 03:39:31 +0300

    Update backendApi.js

--- src/redux/api/backendApi.js
+++ src/redux/api/backendApi.js
@@ -3,7 +3,7 @@ import { API_BACKEND } from '../../helpers/Api';
 
 export const backendApi = createApi({
   reducerPath      : 'backendApi',
-  keepUnusedDataFor: 10,
+  keepUnusedDataFor: 10, // seconds; If user will click the "Back" button in 10 sec (or will return to prev parameters in any other way) - the result will still be in cache
   baseQuery        : fetchBaseQuery({
     baseUrl: API_BACKEND
   }),
