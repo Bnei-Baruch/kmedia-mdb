4299763bb307f3dc538b1108206853727637790f
gshilin <gshilin@gmail.com>
2024-04-15 05:02:27 +0300

    register chroniclesApi

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -35,6 +35,7 @@ import playlist from './modules/playlist';
 import fetchImage from './modules/fetchImage';
 import textPage from './modules/textPage';
 import { backendApi } from './api/backendApi';
+import { chroniclesApi } from './api/chronicles';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const verboseDebug = false;
@@ -62,8 +63,9 @@ const setupMiddleware = history => getDefaultMiddleware => {
 };
 
 const setupReducers = history => ({
-  router                  : connectRouter(history),
-  [backendApi.reducerPath]: backendApi.reducer,
+  router                     : connectRouter(history),
+  [backendApi.reducerPath]   : backendApi.reducer,
+  [chroniclesApi.reducerPath]: chroniclesApi.reducer,
 
   settings,
   preparePage,
