5bf44bae4d23b394efe3651b3fb2e6119f77b380
davgur <davgur.ru@gmail.com>
2026-05-26 14:32:49 +0300

    Refactor: Update Music component to use CollectionListTemplate; remove List component and unnecessary markup

--- src/components/Sections/Music/Music.js
+++ src/components/Sections/Music/Music.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import ResultsPageHeader from '../../Pagination/ResultsPageHeader';
 import SectionHeader from '../../shared/SectionHeader';
 import { getWipErr } from '../../shared/WipErr/WipErr';
-import List from './List';
+import CollectionListTemplate from '../../shared/ContentItem/CollectionListTemplate';
 import { useMusicQuery } from '../../../redux/api/music';
 import { settingsGetUILangSelector, settingsGetContentLanguagesSelector } from '../../../redux/selectors';
 import NotFound from '../../shared/NotFound';
@@ -32,15 +32,13 @@ const Music = () => {
   const content = wipErr || (
     <div className=" px-4 ">
       <ResultsPageHeader pageNo={1} pageSize={1000} total={music.length || 0} />
-      <hr className="m-0" />
-      <List items={music} />
+      {music.map(item => <CollectionListTemplate key={item.id} cID={item.id} size='small' />)}
     </div>
   );
 
   return (
     <>
       <SectionHeader section="music" />
-      <hr className="m-0" />
       {content}
     </>
   );
