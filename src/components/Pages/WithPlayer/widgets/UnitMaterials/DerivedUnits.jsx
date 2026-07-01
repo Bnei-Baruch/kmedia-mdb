ada8bb36aa762264475529bb5d3f4726a705f69f
davgur <davgur.ru@gmail.com>
2026-07-01 14:53:50 +0300

    refactor: simplify DerivedUnits component by removing unused code and enhancing rendering logic

--- src/components/Pages/WithPlayer/widgets/UnitMaterials/DerivedUnits.jsx
+++ src/components/Pages/WithPlayer/widgets/UnitMaterials/DerivedUnits.jsx
@@ -1,44 +1,14 @@
-import PropTypes from 'prop-types';
+import ContentItem from '../../../../shared/ContentItem/ContentItemContainer';
 
-import * as renderUnitHelper from '../../../../../helpers/renderUnitHelper';
-import * as shapes from '../../../../shapes';
+const DerivedUnits = ({ selectedUnits }) => (
+  <div className="m-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 cu_items">
+    {
+      selectedUnits
+        .filter(x => !!x)
+        .map((unit, i) => <ContentItem id={unit.id} key={unit.id} asList={true} />)
+    }
+  </div>
+);
 
-const renderUnit = (unit, t) => {
-  const {
-    clips,
-    relatedItems
-  } = renderUnitHelper.commonRenderUnitForClips(unit, t);
-
-  return (
-    <tr key={unit.id} className="align-top">
-      <td className="whitespace-nowrap">
-        { renderUnitHelper.renderUnitCollectionLogo(unit, 'clips', clips.length > 0 ? clips[0].id : null)}
-      </td>
-      <td>
-        { renderUnitHelper.renderUnitFilmDate(unit, t) }
-        { renderUnitHelper.renderUnitNameLink(unit) }
-        { renderUnitHelper.renderUnitDescription(unit) }
-        {
-          relatedItems &&
-          <ul className="index__collections list-none flex gap-2 divide-x text-xs p-0">
-            {relatedItems}
-          </ul>
-        }
-      </td>
-    </tr>
-  );
-};
-
-const DerivedUnits = ({ selectedUnits, t }) =>
-  <table className="index w-full">
-    <tbody>
-      {selectedUnits.map(u => renderUnit(u, t))}
-    </tbody>
-  </table>;
-
-DerivedUnits.propTypes = {
-  selectedUnits: PropTypes.arrayOf(shapes.ContentUnit).isRequired,
-  t: PropTypes.func.isRequired,
-};
 
 export default DerivedUnits;
