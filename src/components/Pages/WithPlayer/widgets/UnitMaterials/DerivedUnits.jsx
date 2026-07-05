e4ff861c4b070654579dc68080a513cf1fe282dc
davgur <davgur.ru@gmail.com>
2026-07-05 09:57:26 +0300

    refactor: update layout and enhance ContentItem rendering in DerivedUnits component

--- src/components/Pages/WithPlayer/widgets/UnitMaterials/DerivedUnits.jsx
+++ src/components/Pages/WithPlayer/widgets/UnitMaterials/DerivedUnits.jsx
@@ -1,11 +1,11 @@
 import ContentItem from '../../../../shared/ContentItem/ContentItemContainer';
 
 const DerivedUnits = ({ selectedUnits }) => (
-  <div className="m-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 cu_items">
+  <div className="m-4 flex flex-col gap-2">
     {
       selectedUnits
         .filter(x => !!x)
-        .map((unit, i) => <ContentItem id={unit.id} key={unit.id} asList={true} />)
+        .map((unit, i) => <ContentItem id={unit.id} key={unit.id} asList={true} withCCUInfo={true} size="sm" />)
     }
   </div>
 );
